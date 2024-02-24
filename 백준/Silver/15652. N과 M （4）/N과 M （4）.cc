92b61b64eadac575aebac302c5743dd780a0d70d
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-02-25 00:22:12 +0900

    [Silver III] Title: N과 M (4), Time: 8 ms, Memory: 1988 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include<vector>
+#include<utility>
+#include<set>
+using namespace std;
+
+void Printseqs(set<int> v1, int N, int M, vector<int> v2) {
+	if (M == 0) {
+		for (int i = 0; i < v2.size(); i++) {
+			cout << v2[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	for (int i = 1; i <= N; i++) {
+		if (v1.count(i) == 1) {
+			
+			v2.push_back(i);
+			Printseqs(v1, N, M - 1, v2);
+			
+			v2.pop_back();
+		}
+		v1.erase(i);
+	}
+}
+
+int main() {
+	int N, M;
+	cin >> N>> M;
+	set<int> v1;
+	for (int i = 1; i <= N; i++) {
+		v1.insert(i);
+	}
+	vector<int> v2; // 사용된 숫자
+	Printseqs(v1, N, M, v2);
+
+	
+}
