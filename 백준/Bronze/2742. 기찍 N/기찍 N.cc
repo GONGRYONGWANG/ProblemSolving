3ca7958195688451ab75deeda9f0ad40cc60979f
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-02-25 00:56:30 +0900

    [Bronze IV] Title: 기찍 N, Time: 12 ms, Memory: 1984 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+
+int main() {
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+	for (int i = 0; i <N; i++) {
+		cout << N - i << "\n";
+	}
+}
