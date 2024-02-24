25e79c805cef42f1f4630cd115c6134c73d80176
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-02-25 00:43:31 +0900

    [Bronze I] Title: A/B - 2, Time: 0 ms, Memory: 2020 KB, Score: 2000 point -BaekjoonHub

---
+++ 백준/Bronze/15792. A／B － 2/A／B － 2.cc
@@ -0,0 +1,28 @@
+//#pragma warning(disable:4996) 
+#include<bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+#define pq priority_queue
+typedef unsigned long long ull;
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+// ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+#define inf 1e9
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+    int A, B;
+    cin >> A >> B;
+    cout << A / B << ".";
+    for (int i = 0; i < 1000; i++) {
+        A %= B;
+        A *= 10;
+        cout << A / B;
+    }
+
+    return 0;
+}
