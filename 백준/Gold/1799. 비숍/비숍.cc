68a6b12b88bf0427ab8927650eee54a26ff988d2
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-02-24 23:54:02 +0900

    [Gold I] Title: 비숍, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1799. 비숍/비숍.cc
@@ -0,0 +1,74 @@
+#pragma warning(disable:4996) 
+#include<iostream>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<cstdlib>
+#include<ctime>
+#include<regex>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+#define pq priority_queue
+#define endl "\n"
+#define inf 1e9
+#define pb push_back
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
+//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+
+vector<int> E[20];
+int assign[20];
+bool done[20];
+int N;
+
+bool dfs(int x) {
+    for (int i = 0; i < E[x].size(); i++) {
+        int target = E[x][i];
+        if (done[target]) continue;
+        done[target] = 1;
+        if (assign[target] == 0 || dfs(assign[target])) {
+            assign[target] = x;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            bool x;
+            cin >> x;
+            int a = i + j - 1;
+            int b = N + i - j;
+            if (x) E[a].push_back(b);
+        }
+    }
+
+    int ans = 0;
+    for (int i = 1; i < 2 * N; i++) {
+        fill(done, done + 2 * N, false);
+        if (dfs(i)) ans += 1;
+    }
+
+    cout << ans;
+
+    return 0;
+}
