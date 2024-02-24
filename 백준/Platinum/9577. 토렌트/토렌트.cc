24fb67f922cdf9165d384cf9bb18ec308f8470ad
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-02-24 23:43:40 +0900

    [Platinum III] Title: 토렌트, Time: 20 ms, Memory: 2544 KB -BaekjoonHub

---
+++ 백준/Platinum/9577. 토렌트/토렌트.cc
@@ -0,0 +1,90 @@
+#pragma warning(disable:4996) 
+#include<iostream>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<cstdlib>
+#include<ctime>
+#include<regex>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+#define pq priority_queue
+#define endl "\n"
+#define inf 1e9
+#define pb push_back
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
+//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+
+
+unordered_map<int, vector<int>> E;
+int assign[101];
+bool done[101];
+
+bool dfs(int x) {
+    for (int i = 0; i < E[x].size(); i++) {
+        int target = E[x][i];
+        if (done[target]) continue;
+        done[target] = 1;
+        if (assign[target] == 0 || dfs(assign[target])) {
+            assign[target] = x;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+
+    int T;
+    cin >> T;
+    while (T--) {
+        int N, M;
+        cin >> N>>M;
+        E.clear();
+        for (int i = 0; i <M ; i++) {
+            int t1, t2;
+            cin >> t1 >> t2;
+            int a;
+            cin >> a;
+            for (int j = 0; j < a; j++) {
+                int q;
+                cin >> q;
+                for (int k = t1+1; k <= t2; k++) {
+                    E[k].push_back(q);
+                }
+            }
+        }
+        int cnt = 0;
+        fill(assign, assign + N + 1, 0);
+        for (int i = 1; i <= 100; i++) {
+            fill(done, done + N + 1, false);
+            if (dfs(i)) cnt += 1;
+            if (cnt == N) {
+                cout << i << endl;
+                break;
+            }
+        }
+        if (cnt != N) cout << -1<<endl;
+    }
+    
+    
+    return 0;
+}
