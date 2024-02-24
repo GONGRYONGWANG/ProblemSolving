b6256102882853962b54b483688f0367cdbb4e91
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-02-25 00:53:53 +0900

    [Bronze III] Title: 욱제는 건축왕이야!!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15923. 욱제는 건축왕이야！！/욱제는 건축왕이야！！.cc
@@ -0,0 +1,78 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
+//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+string debug = "ans: ";
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+
+int dist(pii a, pii b) {
+    return (a.first - b.first) * (a.first - b.first) + (a.second - b.second) * (a.second - b.second);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);  
+
+    int N;
+    cin >> N;
+    int xmn=inf, xmx=0, ymn=inf, ymx=0;
+    for (int i = 0; i < N; i++) {
+        int x, y;
+        cin >> x >> y;
+        xmn = min(xmn, x);
+        xmx = max(xmx, x);
+        ymn = min(ymn, y);
+        ymx = max(ymx, y);
+    }
+    cout << 2 * (xmx - xmn + ymx - ymn);
+
+    return 0;
+}
