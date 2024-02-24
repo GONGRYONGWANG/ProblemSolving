03dcf6d399ddf7070e67b6ad02863abccdfaacd0
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-02-25 00:23:07 +0900

    [Silver III] Title: 모든 순열, Time: 20 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/10974. 모든 순열/모든 순열.cc
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
+//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+int N;
+bool visited[9];
+vector<int> out;
+
+void bf(int n) {
+    if (n == 0) {
+        for (int i = 0; i < N; i++) {
+            cout << out[i] << " ";
+        }
+        cout << endl;
+        return;
+    }
+    for (int i = 1; i <= N; i++) {
+        if (visited[i]) continue;
+        out.push_back(i);
+        visited[i] = true;
+        bf(n - 1);
+        out.pop_back();
+        visited[i] = false;
+    }
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+    cin >> N;
+    bf(N);
+    return 0;
+}
