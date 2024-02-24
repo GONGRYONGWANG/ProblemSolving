40cb896fe5613c06d644d49696cab6e78ed2ecbf
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-02-24 23:45:47 +0900

    [Platinum IV] Title: 열혈강호 2, Time: 1256 ms, Memory: 10124 KB -BaekjoonHub

---
+++ 백준/Platinum/11376. 열혈강호 2/열혈강호 2.cc
@@ -0,0 +1,75 @@
+#pragma warning(disable:4996) 
+#include<iostream>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<cstdlib>
+#include<ctime>
+#include<regex>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+#define pq priority_queue
+#define endl "\n"
+#define inf 1e9
+#define pb push_back
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
+//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+
+int N, M;
+vector<int> E[2001];
+int assign[1001];
+bool done[1001];
+
+bool dfs(int x) {
+    for (int i = 0; i < E[x].size(); i++) {
+        int target = E[x][i];
+        if (done[target]) continue;
+        done[target] = 1;
+        if (assign[target] == 0 || dfs(assign[target])) {
+            assign[target] = x;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+    cin >> N >> M;
+    for (int i = 1; i <= N; i++) {
+        int n;
+        cin >> n;
+        for (int j = 0; j < n; j++) {
+            int job;
+            cin >> job;
+            E[i].push_back(job);
+            E[i + N].push_back(job);
+        }
+    }
+
+    int ans = 0;
+    for (int i = 1; i <= 2*N; i++) {
+        fill(done, done + M + 1, false);
+        if (dfs(i)) ans += 1;
+    }
+    cout << ans;
+
+
+    return 0;
+}
