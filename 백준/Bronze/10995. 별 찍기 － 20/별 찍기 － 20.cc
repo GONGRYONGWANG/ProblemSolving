eb516351bbaba4b7cfb228786ff0fb10ce8f62da
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-02-25 00:53:57 +0900

    [Bronze III] Title: 별 찍기 - 20, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10995. 별 찍기 － 20/별 찍기 － 20.cc
@@ -0,0 +1,56 @@
+#pragma warning(disable:4996) 
+#include<iostream>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<cstdlib>
+#include<ctime>
+#include<regex>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+#define pq priority_queue
+#define endl "\n"
+#define inf 1e9
+#define pb push_back
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
+//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        if (i % 2) {
+            for (int j = 0; j < N; j++) {
+                cout << " *";
+            }
+        }
+        else {
+            for (int j = 0; j < N; j++) {
+                cout << "* ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
