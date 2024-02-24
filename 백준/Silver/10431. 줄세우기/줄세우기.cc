198f25133ddf322655a452b55f243f94f0b084e3
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-02-25 00:33:04 +0900

    [Silver V] Title: 줄세우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/10431. 줄세우기/줄세우기.cc
@@ -0,0 +1,57 @@
+#pragma warning(disable:4996) 
+#include<iostream>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<cstdlib>
+#include<ctime>
+#include<regex>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+#define pq priority_queue
+#define endl "\n"
+#define inf 1e9
+#define pb push_back
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
+//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+    int T;
+    cin >> T;
+    while (T--) {
+        int t;
+        cin >> t;
+        int ans = 0;
+        vector<int> v;
+        for (int i = 0; i < 20; i++) {
+            int x;
+            cin >> x;
+            for (int j = 0; j < v.size(); j++) {
+                if (v[j] > x) ans += 1;
+            }
+            v.push_back(x);
+        }
+        cout << t << " " << ans<<endl;
+    }
+    return 0;
+}
