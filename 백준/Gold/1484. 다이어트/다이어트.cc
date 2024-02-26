2dc8208b447e98172ea51c94f376c529d739fece
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-02-26 22:42:19 +0900

    [Gold V] Title: 다이어트, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Gold/1484. 다이어트/다이어트.cc
@@ -0,0 +1,109 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
+//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+string debug = "ans: ";
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+
+int dist(pii a, pii b) {
+    return (a.first - b.first) * (a.first - b.first) + (a.second - b.second) * (a.second - b.second);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+void solve() {
+    
+    int G;
+    cin >> G;
+    vector<int> ans;
+    if (G % 2 != 0) {
+        for (int i = G; i > sqrt(G); i--) {
+            if (G % i != 0) continue;
+            ans.push_back((i + G / i) / 2);
+        }
+    }
+    else if (G % 4 == 0) {
+        G = G / 4;
+        for (int i = G; i > sqrt(G); i--) {
+            if (G % i != 0) continue;
+            ans.push_back(i + G / i);
+        }
+    }
+
+    if (ans.empty()) {
+        ans.push_back(-1);
+    }
+
+
+    sort(ans.begin(), ans.end());
+    for (int i = 0; i < ans.size(); i++) {
+        cout << ans[i] << endl;
+    }
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+    int T;
+    T = 1;
+    // cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
