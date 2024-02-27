9392689b0827bbe376395a07cf4d4b152757260d
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-02-27 13:25:12 +0900

    [Gold V] Title: 호텔, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1106. 호텔/호텔.cc
@@ -0,0 +1,99 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
+//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+string debug = "ans: ";
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+
+int dist(pii a, pii b) {
+    return (a.first - b.first) * (a.first - b.first) + (a.second - b.second) * (a.second - b.second);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+int arr[1100];
+
+void solve() {
+    int C, N;
+    cin >> C >> N;
+    fill(arr, arr + 1100, inf);
+    arr[0] = 0;
+    for (int i = 0; i < N; i++) {
+        int c, v;
+        cin >> c >> v;
+        for (int j = 0; j + v < 1100; j++) {
+            arr[j + v] = min(arr[j + v], arr[j] + c);
+        }
+    }
+
+    int ans = inf;
+    for (int i = C; i < 1100; i++) {
+        ans = min(ans, arr[i]);
+    }
+    cout << ans;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+    int T;
+    T = 1;
+    // cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
