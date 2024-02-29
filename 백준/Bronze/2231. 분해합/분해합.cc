40877d7fb0822ed8202b5c060394f81835154f5e
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-02-29 13:13:18 +0900

    [Bronze II] Title: 분해합, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.cc
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -1,26 +1,95 @@
-#include <iostream>
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
 using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
+//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+string debug = "output: ";
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+
+int dist(pii a, pii b) {
+    return (a.first - b.first) * (a.first - b.first) + (a.second - b.second) * (a.second - b.second);
+}
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+int construct(int x) {
+    int ret = x;
+    while (x != 0) {
+        ret += x % 10;
+        x /= 10;
+    }
+    return ret;
+}
+
+void solve() {
+    int N;
+    cin >> N;
+    for (int i = 1; i < N; i++) {
+        if (N == construct(i)) {
+            cout << i;
+            return;
+        }
+    }
+    cout << 0;
+}
+
 
 int main() {
-	int N;
-	cin >> N;
-
-	int result=0;
-	int cN;
-	int i = 0;
-
-	int tmp;
-	while (i<N) {
-
-		i++;
-		cN = N - i;
-
-		tmp = 0;
-		while (cN != 0) {
-			tmp += cN % 10;
-			cN /= 10;
-		}
-		if (tmp == i) { result = N - i; }
-	}
-	cout << result;
+    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+    int T;
+    // cin >> T;
+    T = 1; //
+    while (T--) {
+        solve();
+    }
+    return 0;
 }
