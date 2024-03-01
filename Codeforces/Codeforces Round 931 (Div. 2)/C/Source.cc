ecad30729a694f21bb96aeacf146b7423d666524
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-03-02 02:18:30 +0900

    Update Source.cc

--- Codeforces/Codeforces Round 931 (Div. 2)/C/Source.cc
+++ Codeforces/Codeforces Round 931 (Div. 2)/C/Source.cc
@@ -59,37 +59,90 @@ int dist(pii a, pii b) {
     return (a.first - b.first) * (a.first - b.first) + (a.second - b.second) * (a.second - b.second);
 }
 
+/*
+int COMB[100][100];
+int comb(int n, int r) {
+    if (r == 0) return 1;
+    if (n == r) return 1;
+    if (COMB[n][r]) return COMB[n][r];
+    return COMB[n][r] = comb(n - 1, r) + comb(n - 1, r - 1);
+} 
+*/
+
 //////////////////////////////////////////////////////////////////////////////////////
+
+
+// int arr[30] = { 0,1,2,1,2,3,1,2,3,2,1,2,2,2,3,1,2,3,2,3,2,2,3,3,3,2,3,3,3,4 };
+
 void solve() {
-    
-    vector<bool> mod(3, 0);
-    int N;
-    cin >> N;
+    int N, M;
+    cin >> N >> M;
+    int d1, d2, d3, d4;
+    cout << "? " << 1 << " " << 1 << endl << flush;
+    cin >> d1;
+    cout << "? " << 1 << " " << M << endl << flush;
+    cin >> d3;
+    cout << "? " << N << " " << 1 << endl << flush;
+    cin >> d4;
 
-    int total = 0;
-    for (int i = 0; i < N; i++) {
-        int x;
-        cin >> x;
-        total += x;
-        mod[x % 3] = 1;
-    }
-    if (total % 3 == 0) {
-        cout << 0 << endl;
-    }
-    else if (total % 3 == 1 && !mod[1]) {
-        cout << 2 << endl;
-    }
-    else cout << 1 << endl;
+    pii p1, p2;
+    p1 = { 0,0 };
+    p2 = { 0,0 };
 
+    int xpy = d1 + 2;
+    int xmy = d3 - M + 1;
+    int x = (xpy + xmy) / 2;
+    int y = (xpy - xmy) / 2;
+    if ((xpy + xmy) % 2 == 0 && x >= 1 && x <= N && y >= 1 && y <= M) {
+        p1 = { x,y };
+    }
+
+    xpy = d1 + 2;
+    xmy = N - 1 - d4;
+    x = (xpy + xmy) / 2;
+    y = (xpy - xmy) / 2;
+    if ((xpy + xmy) % 2 == 0 && x >= 1 && x <= N && y >= 1 && y <= M) {
+        p2 = { x,y };
+    }
+    if (p1.first == 0) {
+        cout << "! " << p2.first << " " << p2.second << endl << flush;
+        return;
+    }
+    if (p2.first == 0) {
+        cout << "! " << p1.first << " " << p1.second << endl << flush;
+        return;
+    }
+    if (p1 == p2) {
+        cout << "! " << p1.first << " " << p1.second << endl << flush;
+        return;
+    }
+
+    cout << "? " << p1.first << " " << p1.second << endl << flush;
+    int d;
+    cin >> d;
+    if (d == 0) {
+        cout << "! " << p1.first << " " << p1.second << endl << flush;
+        return;
+    }
+    else {
+        cout << "! " << p2.first << " " << p2.second << endl << flush;
+        return;
+    }
 
 
 }
+//d1 = x-1+y-1  // x+y = d1+2
+// d3 = x-1+M-y // x-y = d3-M+1
+//d4 = N-x+y-1  // x-y = N-1-d4
 
+//  x = (N+M-d2+d3+M-1)/2
 
 int main() {
     ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+
     int T;
     cin >> T;
+    // T = 1;
     while (T--) {
         solve();
     }
