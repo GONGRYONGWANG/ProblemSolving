11ecf4b9f90ca542c4a8eafc6241de92bb4228a7
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-03-03 22:37:44 +0900

    [Gold I] Title: 수들의 합 7, Time: 736 ms, Memory: 33272 KB -BaekjoonHub

---
+++ 백준/Gold/2268. 수들의 합 7/수들의 합 7.cc
@@ -0,0 +1,129 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
+//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+string debug = "output: ";
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+
+int dist(pii a, pii b) {
+    return (a.first - b.first) * (a.first - b.first) + (a.second - b.second) * (a.second - b.second);
+}
+
+/*
+int COMB[100][100];
+int comb(int n, int r) {
+    if (r == 0) return 1;
+    if (n == r) return 1;
+    if (COMB[n][r]) return COMB[n][r];
+    return COMB[n][r] = comb(n - 1, r) + comb(n - 1, r - 1);
+} 
+*/
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+ll tree[4000000];
+
+ll get(int node, int start, int end, int left, int right) {
+    if (left > end || right < start) return 0;
+    if (left <= start && right >= end) return tree[node];
+    int mid = (start + end) / 2;
+    return get(node * 2, start, mid, left, right) + get(node * 2 + 1, mid + 1, end, left, right);
+}
+
+void modify(int node, int start, int end, int target, ll val) {
+    if (start > target || end < target) return;
+    if (start == end) {
+        tree[node] = val;
+        return;
+    }
+    int mid = (start + end) / 2;
+    modify(node * 2, start, mid, target, val);
+    modify(node * 2 + 1, mid + 1, end, target, val);
+    tree[node] = tree[node * 2] + tree[node * 2 + 1];
+}
+
+
+
+void solve() {
+    int N, M;
+    cin >> N >> M;
+    while (M--) {
+        int i, j;
+        ll k;
+        bool x = 0;
+        cin >> x;
+        if (!x) {
+            cin >> i >> j;
+            if (i > j) swap(i, j);
+            cout << get(1, 1, N, i, j) << endl;
+        }
+        else {
+            cin >> i >> k;
+            modify(1, 1, N, i, k);
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
+
+    int T;
+    // cin >> T;
+    T = 1;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
