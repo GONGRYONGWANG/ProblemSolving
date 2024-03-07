cd44c0f813354ee7f137b6c43d479863a228e808
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-03-07 21:34:19 +0900

    [Gold II] Title: 트리의 높이와 너비, Time: 4 ms, Memory: 2216 KB -BaekjoonHub

---
+++ 백준/Gold/2250. 트리의 높이와 너비/트리의 높이와 너비.cc
@@ -0,0 +1,133 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
+//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+string debug = "output: ";
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+
+int dist(pii a, pii b) {
+    return (a.first - b.first) * (a.first - b.first) + (a.second - b.second) * (a.second - b.second);
+}
+
+/*
+int COMB[100][100];
+int comb(int n, int r) {
+    if (r == 0) return 1;
+    if (n == r) return 1;
+    if (COMB[n][r]) return COMB[n][r];
+    return COMB[n][r] = comb(n - 1, r) + comb(n - 1, r - 1);
+}
+*/
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+int parent[10001];
+pii child[10001];
+pii lr[10001];
+
+int cnt;
+void dfs(int x, int depth) {
+    if (x == -1) return;
+    dfs(child[x].first, depth + 1);
+    lr[depth].first = min(lr[depth].first, cnt);
+    lr[depth].second = max(lr[depth].second, cnt);
+    cnt += 1;
+    dfs(child[x].second, depth + 1);
+}
+
+void solve() {
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int p, l, r;
+        cin >> p >> l >> r;
+        if (l != -1) {
+            parent[l] = p;
+        }
+        if (r != -1) {
+            parent[r] = p;
+        }
+        child[p] = { l,r };
+    }
+    int root = 0;
+    for (int i = 1; i <= N; i++) {
+        lr[i] = { inf,-inf };
+        if (parent[i] == 0) {
+            root = i;
+        }
+    }
+    cnt = 1;
+    dfs(root, 1);
+    int lev = 1;
+    for (int i = 2; i <= N; i++) {
+        if (lr[i].second - lr[i].first + 1 > lr[lev].second - lr[lev].first + 1) {
+            lev = i;
+        }
+    }
+    cout << lev << " " << lr[lev].second - lr[lev].first + 1;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T;
+    // cin >> T;
+    T = 1;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
