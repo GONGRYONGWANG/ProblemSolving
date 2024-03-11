bf8ae5b5086b2c1c5b0fe4a10621eb8aaf1b205d
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-03-11 19:09:19 +0900

    [Gold II] Title: 세미나 배정, Time: 68 ms, Memory: 3716 KB -BaekjoonHub

---
+++ 백준/Gold/28305. 세미나 배정/세미나 배정.cc
@@ -0,0 +1,133 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
+//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+string debug = "output: ";
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+
+int dist(pii a, pii b) {
+    return (a.first - b.first) * (a.first - b.first) + (a.second - b.second) * (a.second - b.second);
+}
+
+/*
+int COMB[100][100];
+int comb(int n, int r) {
+    if (r == 0) return 1;
+    if (n == r) return 1;
+    if (COMB[n][r]) return COMB[n][r];
+    return COMB[n][r] = comb(n - 1, r) + comb(n - 1, r - 1);
+}
+*/
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+int N, T;
+int arr[200000];
+
+bool assign(int x) {
+    queue<int> q;
+    for (int i = 0; i < N; i++) {
+        while (!q.empty() && q.front() < arr[i] - T + 1) {
+            q.pop();
+        }
+
+        if (q.size() < x) {
+            q.push(max(T, arr[i]));
+        }
+        else if (q.front() < arr[i]) {
+            q.push(q.front() + T);
+            q.pop();
+        }
+        else {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+void solve() {
+
+    cin >> N >> T;
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + N);
+
+    int lo = 1;
+    int hi = N;
+
+    while(lo<hi){
+        int mid = (lo + hi) / 2;
+        if (assign(mid)) {
+            hi = mid;
+        }
+        else lo = mid + 1;
+    }
+    cout << hi;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T;
+    // cin >> T;
+    T = 1;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
