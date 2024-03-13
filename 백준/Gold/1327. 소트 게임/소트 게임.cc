2d161e694eb4682c2d286bbfc3c569e0903630da
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-03-13 21:57:37 +0900

    [Gold IV] Title: 소트 게임, Time: 32 ms, Memory: 5004 KB -BaekjoonHub

---
+++ 백준/Gold/1327. 소트 게임/소트 게임.cc
@@ -0,0 +1,121 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
+//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+string debug = "output: ";
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+
+int dist(pii a, pii b) {
+    return (a.first - b.first) * (a.first - b.first) + (a.second - b.second) * (a.second - b.second);
+}
+
+/*
+int COMB[100][100];
+int comb(int n, int r) {
+    if (r == 0) return 1;
+    if (n == r) return 1;
+    if (COMB[n][r]) return COMB[n][r];
+    return COMB[n][r] = comb(n - 1, r) + comb(n - 1, r - 1);
+}
+*/
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+unordered_map<string, int> visited;
+
+void solve() {
+
+    int N, K;
+    cin >> N >> K;
+    string s = "";
+    for (int i = 0; i < N; i++) {
+        char x;
+        cin >> x;
+        s += x;
+    }
+    visited[s] = 0;
+    queue<string> q;
+    q.push(s);
+    while (!q.empty()) {
+        string cur = q.front();
+        q.pop();
+        for (int i = 0; i + K <= N; i++) {
+            string nxt = cur;
+            for (int j = 0; j < K / 2; j++) {
+                swap(nxt[i + j], nxt[i + K - 1 - j]);
+            }
+            if (visited.count(nxt)) continue;
+            visited[nxt] = visited[cur] + 1;
+            q.push(nxt);
+        }
+    }
+    sort(s.begin(), s.end());
+    if (visited.count(s)) {
+        cout << visited[s];
+    }
+    else cout << -1;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T;
+    // cin >> T;
+    T = 1;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
