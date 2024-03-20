311c5ba77817c86e86b0492932b273e4704d6ba7
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-03-20 20:28:01 +0900

    [Gold III] Title: 어려운 스케줄링, Time: 16 ms, Memory: 3076 KB -BaekjoonHub

---
+++ 백준/Gold/26086. 어려운 스케줄링/어려운 스케줄링.cc
@@ -0,0 +1,120 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
+//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+string debug = "output: ";
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+
+
+
+/*
+int COMB[100][100];
+int comb(int n, int r) {
+    if (r == 0) return 1;
+    if (n == r) return 1;
+    if (COMB[n][r]) return COMB[n][r];
+    return COMB[n][r] = comb(n - 1, r) + comb(n - 1, r - 1);
+}
+*/
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+void solve() {
+    int N, M, K;
+    cin >> N >> M >> K;
+
+    vector<pii> query(M);
+    int lastsort = -1;
+    for (int i = 0; i < M; i++) {
+        cin >> query[i].first;
+        if (query[i].first == 0) {
+            cin >> query[i].second;
+        }
+        if (query[i].first == 1) lastsort = i;
+    }
+
+    deque<int> q;
+    bool dir = 0;
+    for (int i = 0; i < M; i++) {
+        if (query[i].first == 0) {
+            if (!dir) q.push_front(query[i].second);
+            else q.push_back(query[i].first);
+        }
+        else if (query[i].first == 1 && i == lastsort) {
+            sort(q.begin(), q.end());
+        }
+        else if (query[i].first == 2 && i > lastsort) {
+            dir = !dir;
+        }
+    }
+    if (!dir) {
+        cout << q[K - 1];
+    }
+    else cout << q[q.size() - K];
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T;
+    // cin >> T;
+    T = 1;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
