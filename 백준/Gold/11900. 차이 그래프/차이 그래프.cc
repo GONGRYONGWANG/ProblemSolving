80f562ab99ed999afc93d42037b8576771bae2c6
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-03-21 21:32:55 +0900

    [Gold I] Title: 차이 그래프, Time: 592 ms, Memory: 26744 KB -BaekjoonHub

---
+++ 백준/Gold/11900. 차이 그래프/차이 그래프.cc
@@ -0,0 +1,134 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
+//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+string debug = "output: ";
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll fastpow(ll x, ll n, ll mod = INF) {
+    if (n == 0) return 1;
+    if (n % 2) return x * fastpow(x, n - 1, mod) % mod;
+    ll half = fastpow(x, n / 2, mod);
+    return half * half % mod;
+}
+
+
+
+
+/*
+int COMB[100][100];
+int comb(int n, int r) {
+    if (r == 0) return 1;
+    if (n == r) return 1;
+    if (COMB[n][r]) return COMB[n][r];
+    return COMB[n][r] = comb(n - 1, r) + comb(n - 1, r - 1);
+}
+*/
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+int ans[2000];
+int arr[2000];
+bool visited[2000];
+struct cmp {
+    bool operator()(pii a, pii b) {
+        return a.second > b.second;
+    }
+};
+void solve() {
+    int N;
+    cin >> N;
+    for (int i = 1; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    pq<pii, vector<pii>, cmp> q;
+    q.push({ 0,0 });
+    while (!q.empty()) {
+        int cur = q.top().first;
+        int dist = q.top().second;
+        q.pop();
+        if (visited[cur]) continue;
+        visited[cur] = true;
+        ans[cur] = dist;
+        for (int i = 0; i < N; i++) {
+            if (visited[i] || arr[(cur - i + N) % N]==0) continue;
+            q.push({ i, dist + arr[(cur - i + N) % N] });
+        }
+    }
+
+    int M;
+    cin >> M;
+    for (int i = 0; i < M; i++) {
+        int a, b;
+        cin >> a >> b;
+        if (ans[(b + N - a) % N] == 0) cout << -1;
+        else  cout << ans[(b + N - a) % N];
+        cout << endl;
+    }
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T;
+    // cin >> T;
+    T = 1;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
+
