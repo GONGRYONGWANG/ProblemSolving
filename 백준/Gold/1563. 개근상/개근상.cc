a26e3543270246612e203179f2bd0b9ffb88fb22
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-03-25 15:57:26 +0900

    [Gold IV] Title: 개근상, Time: 0 ms, Memory: 2068 KB -BaekjoonHub

---
+++ 백준/Gold/1563. 개근상/개근상.cc
@@ -0,0 +1,116 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
+//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+string debug = "output: ";
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll fastpow(ll x, ll n, ll mod = INF) {
+    if (n == 0) return 1;
+    if (n % 2) return x * fastpow(x, n - 1, mod) % mod;
+    ll half = fastpow(x, n / 2, mod);
+    return half * half % mod;
+}
+
+
+
+
+/*
+int COMB[100][100];
+int comb(int n, int r) {
+    if (r == 0) return 1;
+    if (n == r) return 1;
+    if (COMB[n][r]) return COMB[n][r];
+    return COMB[n][r] = comb(n - 1, r) + comb(n - 1, r - 1);
+}
+*/
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+ll DP[1001][2][3];
+ll dp(int x, int l, int a) {
+    if (x == 0) return 1;
+    if (DP[x][l][a]) return DP[x][l][a];
+    DP[x][l][a] = dp(x - 1, l, 0);
+    if (l != 1) {
+        DP[x][l][a] += dp(x - 1, l + 1, 0);
+    }
+    if(a != 2){
+        DP[x][l][a] += dp(x - 1, l, a + 1);
+    }
+    DP[x][l][a] %= 1000000;
+    return DP[x][l][a];
+}
+
+void solve() {
+    int N;
+    cin >> N;
+    cout << dp(N, 0, 0);
+    
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T;
+    // cin >> T;
+    T = 1;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
+
