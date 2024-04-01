ac5fffab3a7461fd681355071f5d06198627f927
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-04-01 21:53:59 +0900

    [Gold IV] Title: 최대 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/25342. 최대 최소공배수/최대 최소공배수.cc
@@ -0,0 +1,124 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
+//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+string debug = "output: ";
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll xgcd(ll a, ll b) {
+    ll r1 = a;
+    ll r2 = b;
+    ll s1 = 1;
+    ll s2 = 0;
+    ll t1 = 0;
+    ll t2 = 1;
+    while (1) {
+        ll q = r1 / r2;
+        ll r = r1 - q * r2;
+        ll s = s1 - q * s2;
+        ll t = t1 - q * t2;
+        if (r == 0) return s2;
+        r1 = r2;
+        r2 = r;
+        s1 = s2;
+        s2 = s;
+        t1 = t2;
+        t2 = t;
+    }
+}
+
+ll fastpow(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * fastpow(x, n - 1, mod) % mod;
+    ll half = fastpow(x, n / 2, mod);
+    return half * half % mod;
+}
+
+ll modinv(ll x, ll mod) {
+    return fastpow(x, mod - 2, mod);
+}
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+void solve() {
+
+    ll N;
+    cin >> N;
+    if (N % 2) {
+        cout << N * (N - 1) * (N - 2) << endl;
+        return;
+    }
+    if (N % 3) {
+        cout << N * (N - 1) * (N - 3) << endl;
+        return;
+    }
+    cout << (N - 1) * (N - 2) * (N - 3) << endl;
+
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
