367434a4b1ac24cfca189310808a5e64bc5eb5d9
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-04-02 05:04:30 +0900

    [Gold III] Title: 수상 택시, Time: 100 ms, Memory: 16088 KB -BaekjoonHub

---
+++ 백준/Gold/2836. 수상 택시/수상 택시.cc
@@ -0,0 +1,111 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
+//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+string debug = "output: ";
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll fastpow(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * fastpow(x, n - 1, mod) % mod;
+    ll half = fastpow(x, n / 2, mod);
+    return half * half % mod;
+}
+
+ll modinv(ll x, ll mod) {
+    return fastpow(x, mod - 2, mod);
+}
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+void solve() {
+    int N, M;
+    cin >> N >> M;
+    vector<pll> arr(N);
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i].second >> arr[i].first;
+    }
+    sort(arr.begin(), arr.end());
+    arr.push_back({ M+1,M+2 });
+    ll ans = M;
+    ll l, r;
+    l = r = 0;
+    for (int i = 0; i <= N; i++) {
+        if (arr[i].first >= arr[i].second) continue;
+        if (arr[i].first <= r) {
+            r = max(r, arr[i].second);
+        }
+        else {
+            ans += 2*(r - l);
+            l = arr[i].first;
+            r = arr[i].second;
+        }
+    }
+    cout << ans;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    // cin >> T;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
