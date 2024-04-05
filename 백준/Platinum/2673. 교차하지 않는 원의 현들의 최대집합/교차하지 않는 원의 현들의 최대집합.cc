8881924d343dbbb516c6fbc2885e2b9ffcd50af3
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-04-05 23:44:21 +0900

    [Platinum IV] Title: 교차하지 않는 원의 현들의 최대집합, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Platinum/2673. 교차하지 않는 원의 현들의 최대집합/교차하지 않는 원의 현들의 최대집합.cc
@@ -0,0 +1,125 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
+//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+const string debug = "output: ";
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll fastpow(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * fastpow(x, n - 1, mod) % mod;
+    ll half = fastpow(x, n / 2, mod);
+    return half * half % mod;
+}
+
+ll modinv(ll x, ll mod) {
+    return fastpow(x, mod - 2, mod);
+}
+
+ll factorial(ll x) {
+    ll ret = 1;
+    // ret = F[x];
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    return factorial(n) * modinv(factorial(n - r), mod) % mod * modinv(factorial(r), mod) % mod;
+}
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+int arr[101];
+int DP[101][101];
+int dp(int x, int y) {
+    if (x >= y) return 0;
+    if (DP[x][y] != -1) return DP[x][y];
+    DP[x][y] = dp(x + 1, y);
+    if (arr[x] != 0 && arr[x] <= y && arr[x]>x) {
+        DP[x][y] = max(DP[x][y], 1 + dp(x + 1, arr[x] - 1) + dp(arr[x] + 1, y));
+    }
+    return DP[x][y];
+}
+
+void solve() {
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int a, b;
+        cin >> a >> b;
+        arr[a] = b;
+        arr[b] = a;
+    }
+    for (int i = 1; i <= 100; i++) {
+        for (int j = i + 1; j <= 100; j++) {
+            DP[i][j] = -1;
+        }
+    }
+    cout << dp(1, 100);
+
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    // cin >> T;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
