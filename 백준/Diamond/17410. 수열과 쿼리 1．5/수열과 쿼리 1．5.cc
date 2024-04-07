878dd06d21bab9eb51f553991177ed3dd517497b
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-04-07 17:12:10 +0900

    [Diamond V] Title: 수열과 쿼리 1.5, Time: 1272 ms, Memory: 3084 KB -BaekjoonHub

---
+++ 백준/Diamond/17410. 수열과 쿼리 1．5/수열과 쿼리 1．5.cc
@@ -0,0 +1,140 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
+//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+const string debug = "output: ";
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll fastpow(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * fastpow(x, n - 1, mod) % mod;
+    ll half = fastpow(x, n / 2, mod);
+    return half * half % mod;
+}
+
+ll modinv(ll x, ll mod) {
+    return fastpow(x, mod - 2, mod);
+}
+
+ll factorial(ll x) {
+    ll ret = 1;
+    // ret = F[x];
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    return factorial(n) * modinv(factorial(n - r), mod) % mod * modinv(factorial(r), mod) % mod;
+}
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+int arr[100001];
+vector<int> bucket[317];
+
+void solve() {
+    int N, M;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cin >> arr[i];
+        bucket[i / 316].push_back(arr[i]);
+    }
+    for (int i = 0; i < 317; i++) {
+        sort(bucket[i].begin(), bucket[i].end());
+    }
+
+    cin >> M;
+
+    while (M--) {
+        int a, i, j, v, k;
+        cin >> a;
+        if (a == 1) {
+            cin >> i >> v;
+            bucket[i / 316].erase(find(bucket[i / 316].begin(), bucket[i / 316].end(), arr[i]));
+            arr[i] = v;
+            bucket[i / 316].insert(lower_bound(bucket[i / 316].begin(), bucket[i / 316].end(), arr[i]), arr[i]);
+        }
+        else {
+            cin >> i >> j >> k;
+            int ret = 0;
+            for (int m = i; (m / 316 == i / 316) && (m <= j); m++) {
+                if (arr[m] > k)  ret += 1;
+            }
+            for (int m = i / 316 + 1; m < j / 316; m++) {
+                ret += bucket[m].end() - upper_bound(bucket[m].begin(), bucket[m].end(), k);
+            }
+            if (i / 316 != j / 316) {
+                for (int m = j / 316 * 316; m <= j; m++) {
+                    if (arr[m] > k) ret += 1;
+                }
+            }
+            cout << ret << endl;
+        }
+    }
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    // cin >> T;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
