c74fb75cfa4bc139dde3f8f23e8fba3e87accee8
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-04-07 17:51:57 +0900

    [Gold III] Title: 나만 안되는 연애, Time: 4 ms, Memory: 2340 KB -BaekjoonHub

---
+++ 백준/Gold/14621. 나만 안되는 연애/나만 안되는 연애.cc
@@ -0,0 +1,150 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
+//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+const string debug = "output: ";
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll fastpow(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * fastpow(x, n - 1, mod) % mod;
+    ll half = fastpow(x, n / 2, mod);
+    return half * half % mod;
+}
+
+ll modinv(ll x, ll mod) {
+    return fastpow(x, mod - 2, mod);
+}
+
+ll factorial(ll x) {
+    ll ret = 1;
+    // ret = F[x];
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    return factorial(n) * modinv(factorial(n - r), mod) % mod * modinv(factorial(r), mod) % mod;
+}
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+bool type[1001];
+vector<pii> E[1001];
+bool visited[1001];
+
+struct cmp {
+    bool operator()(pii a, pii b) {
+        return a.second > b.second;
+    }
+};
+
+void solve() {
+    int N, M;
+    cin >> N >> M;
+    for (int i = 1; i <= N; i++) {
+        char x;
+        cin >> x;
+        type[i] = (x == 'M');
+    }
+    for (int i = 0; i < M; i++) {
+        int u, v, d;
+        cin >> u >> v >> d;
+        if (type[u] == type[v]) continue;
+        E[u].push_back({ v,d });
+        E[v].push_back({ u,d });
+    }
+    
+    pq<pii, vector<pii>, cmp> q;
+
+    int ans = 0;
+    q.push({ 1,0 });
+    while (!q.empty()) {
+        int cur = q.top().first;
+        if (visited[cur]) {
+            q.pop();
+            continue;
+        }
+        ans += q.top().second;
+        q.pop();
+        visited[cur] = true;
+        for (int i = 0; i < E[cur].size(); i++) {
+            int nx = E[cur][i].first;
+            if (visited[nx]) continue;
+            q.push(E[cur][i]);
+        }
+    }
+
+    for (int i = 1; i <= N; i++) {
+        if (!visited[i]) {
+            cout << -1;
+            return;
+        }
+    }
+    cout << ans;
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    // cin >> T;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
