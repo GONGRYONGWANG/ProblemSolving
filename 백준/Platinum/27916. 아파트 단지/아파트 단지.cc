63413c2da7f74aa01027924faee91f3c71b9bb9c
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-04-09 19:07:17 +0900

    [Platinum V] Title: 아파트 단지, Time: 24 ms, Memory: 4364 KB, Score: 45 point -BaekjoonHub

---
+++ 백준/Platinum/27916. 아파트 단지/아파트 단지.cc
@@ -0,0 +1,150 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+int dx[4] = { 1,-1,0,0 };
+int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
+//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+const string debug = "output: ";
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll fastpow(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * fastpow(x, n - 1, mod) % mod;
+    ll half = fastpow(x, n / 2, mod);
+    return half * half % mod;
+}
+
+ll modinv(ll x, ll mod) {
+    return fastpow(x, mod - 2, mod);
+}
+
+ll factorial(ll x) {
+    ll ret = 1;
+    // ret = F[x];
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    return factorial(n) * modinv(factorial(n - r), mod) % mod * modinv(factorial(r), mod) % mod;
+}
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+
+int arr[300001];
+int DP[300001];
+
+void solve() {
+    int N, M, Q;
+    cin >> N >> M >> Q;
+    for (int i = 1; i <= N; i++) {
+        cin >> arr[i];
+    }
+    for (int i = 1; i < M; i++) {
+        DP[i] = inf + 1;
+    }
+    for (int i = M; i <= min(2 * M - 1, N); i++) {
+        DP[i] = arr[i] - arr[1];
+    }
+    for (int i = 2 * M; i <= N; i++) {
+        int l = i - 2 * M + 1;
+        int r = l / M * M + M - 1;
+
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (arr[i] - arr[m + 1] < DP[m]) {
+                r = m;
+            }
+            else l = m + 1;
+        }
+        DP[i] = min(max(DP[l], arr[i] - arr[l + 1]), max(DP[l - 1], arr[i] - arr[l]));
+
+
+        r = i - M;
+        l = r / M * M;
+
+        while (l < r) {
+            int m = (l + r) / 2;
+            if (arr[i] - arr[m + 1] < DP[m]) {
+                r = m;
+            }
+            else l = m + 1;
+        }
+        DP[i] = min(DP[i], min(max(DP[l], arr[i] - arr[l + 1]), max(DP[l - 1], arr[i] - arr[l])));
+    }
+
+    while(Q--) {
+        int x;
+        cin >> x;
+        if (DP[N] <= x) cout << 1;
+        else cout << 0;
+    }
+
+
+
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    // cin >> T;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
