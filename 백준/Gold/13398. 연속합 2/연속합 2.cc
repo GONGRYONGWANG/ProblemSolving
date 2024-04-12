7acc74b611b88ce8fb4f32338c9a849854c237d7
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-04-12 12:37:14 +0900

    [Gold V] Title: 연속합 2, Time: 8 ms, Memory: 2800 KB -BaekjoonHub

---
+++ 백준/Gold/13398. 연속합 2/연속합 2.cc
@@ -0,0 +1,122 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
+//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+const string debug = "output: ";
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll fastpow(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * fastpow(x, n - 1, mod) % mod;
+    ll half = fastpow(x, n / 2, mod);
+    return half * half % mod;
+}
+
+ll modinv(ll x, ll mod) {
+    return fastpow(x, mod - 2, mod);
+}
+
+ll factorial(ll x) {
+    ll ret = 1;
+    // ret = F[x];
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    return factorial(n) * modinv(factorial(n - r), mod) % mod * modinv(factorial(r), mod) % mod;
+}
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+int DP[100001][2];
+
+void solve() {
+    int N;
+    cin >> N;
+    int ans = 0;
+    int mx = -inf;
+
+    for (int i = 1; i <= N; i++) {
+        int x;
+        cin >> x;
+        DP[i][0] = max(DP[i - 1][0] + x, x);
+        DP[i][1] = max(DP[i - 1][0], DP[i - 1][1] + x);
+        ans = max(ans, DP[i][0]);
+        ans = max(ans, DP[i][1]);
+        mx = max(mx, x);
+    }
+    if (mx < 0) {
+        cout << mx;
+        return;
+    }
+    cout << ans;
+
+
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    // cin >> T;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
