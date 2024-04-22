000ca44dbcbaf20bca46ae718c41f48b613f3cf2
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-04-22 18:17:37 +0900

    [Gold V] Title: 택배 배송, Time: 28 ms, Memory: 5348 KB -BaekjoonHub

---
+++ 백준/Gold/5972. 택배 배송/택배 배송.cc
@@ -0,0 +1,133 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+const string debug = "output: ";
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll powmod(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * powmod(x, n - 1, mod) % mod;
+    ll half = powmod(x, n / 2, mod);
+    return half * half % mod;
+}
+
+ll modinv(ll x, ll mod) {
+    return powmod(x, mod - 2, mod);
+}
+
+ll factorial(ll x) {
+    ll ret = 1;
+    // ret = F[x];
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    return factorial(n) * modinv(factorial(n - r), mod) % mod * modinv(factorial(r), mod) % mod;
+}
+
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+//int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+vector<pii> E[50001];
+bitset<50001> visited;
+
+struct cmp {
+    bool operator()(pii a, pii b) {
+        return a.second > b.second;
+    }
+};
+
+void solve() {
+    int N, M;
+    cin >> N >> M;
+    for (int i = 0; i < M; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        E[u].push_back({ v,w });
+        E[v].push_back({ u,w });
+    }
+
+    pq<pii, vector<pii>, cmp> q;
+    q.push({ 1,0 });
+    while (!q.empty()) {
+        int cur = q.top().first;
+        int dist = q.top().second;
+        q.pop();
+        if (visited[cur]) continue;
+        visited[cur] = 1;
+        if (cur == N) {
+            cout << dist;
+            return;
+        }
+        for (int i = 0; i < E[cur].size(); i++) {
+            int nx = E[cur][i].first;
+            if (visited[nx]) continue;
+            q.push({ nx, E[cur][i].second + dist });
+        }
+    }
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    // cin >> T;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
