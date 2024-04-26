8bc6576857cd676fab7afc5e66f7434e04eeb561
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-04-26 23:37:29 +0900

    [Gold V] Title: 특별상, Time: 88 ms, Memory: 5152 KB -BaekjoonHub

---
+++ 백준/Gold/25391. 특별상/특별상.cc
@@ -0,0 +1,129 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+const string debug = "output: ";
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll powmod(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * powmod(x, n - 1, mod) % mod;
+    ll half = powmod(x, n / 2, mod);
+    return half * half % mod;
+}
+
+ll modinv(ll x, ll mod) {
+    return powmod(x, mod - 2, mod);
+}
+
+ll factorial(ll x) {
+    ll ret = 1;
+    // ret = F[x];
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    return factorial(n) * modinv(factorial(n - r), mod) % mod * modinv(factorial(r), mod) % mod;
+}
+
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+//int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+
+bool cmp1(pll a, pll b){
+    return a.second < b.second;
+}
+
+bool cmp2(pll a, pll b) {
+    return a.first > b.first;
+}
+
+void solve() {
+    ll N, M ,K;
+    cin >> N >> M >> K;
+
+    ll ans = 0;
+    vector<pll> arr(N);
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i].first >> arr[i].second;
+    }
+    
+    sort(arr.begin(), arr.end(), cmp1);
+
+    while(K--){
+        ans += arr.back().first;
+        arr.pop_back();
+    }
+
+    sort(arr.begin(), arr.end(), cmp2);
+
+    for (int i = 0; i < M; i++) {
+        ans += arr[i].first;
+    }
+
+    cout << ans;
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    // cin >> T;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
