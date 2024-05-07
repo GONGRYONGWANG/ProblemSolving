608e7c44fefac556b948215e45ff1f081cad34b6
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-05-08 02:55:19 +0900

    [Gold III] Title: 호반우와 리듬게임, Time: 16 ms, Memory: 9872 KB -BaekjoonHub

---
+++ 백준/Gold/20120. 호반우와 리듬게임/호반우와 리듬게임.cc
@@ -0,0 +1,124 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+const string debug = "output: ";
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll powmod(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * powmod(x, n - 1, mod) % mod;
+    ll half = powmod(x, n / 2, mod);
+    return half * half % mod;
+}
+
+ll modinv(ll x, ll mod) {
+    return powmod(x, mod - 2, mod);
+}
+
+ll factorial(ll x) {
+    ll ret = 1;
+    // ret = F[x];
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    return factorial(n) * modinv(factorial(n - r), mod) % mod * modinv(factorial(r), mod) % mod;
+}
+
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+//int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+ll arr[1000];
+ll DP[1000][1004];
+int N;
+ll dp(int x, int combo) {
+    if (combo == 1003) return -INF;
+    if (x == N) return 0;
+    if (DP[x][combo] != -INF) return DP[x][combo];
+
+    if (combo > 1000) {
+        DP[x][combo] = max(dp(x + 1, combo + 1), dp(x + 1, 1) + arr[x]);
+    }
+    else {
+        DP[x][combo] = max(dp(x + 1, combo + 1) + arr[x] * (combo + 1), dp(x + 1, 1001));
+    }
+
+    return DP[x][combo];
+}
+
+void solve() {
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < 1004; j++) {
+            DP[i][j] = -INF;
+        }
+        cin >> arr[i];
+    }
+
+    cout << max((ll)0, dp(0, 0));
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    // cin >> T;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
