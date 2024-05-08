b75229b9fc6b7105f50b0f7651cb18655f102ea3
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-05-08 17:22:36 +0900

    [Silver III] Title: 카드 놓기, Time: 220 ms, Memory: 10212 KB -BaekjoonHub

---
+++ 백준/Silver/18115. 카드 놓기/카드 놓기.cc
@@ -0,0 +1,131 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+const string debug = "output: ";
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll powmod(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * powmod(x, n - 1, mod) % mod;
+    ll half = powmod(x, n / 2, mod);
+    return half * half % mod;
+}
+
+ll modinv(ll x, ll mod) {
+    return powmod(x, mod - 2, mod);
+}
+
+ll factorial(ll x) {
+    ll ret = 1;
+    // ret = F[x];
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    return factorial(n) * modinv(factorial(n - r), mod) % mod * modinv(factorial(r), mod) % mod;
+}
+
+int dx[4] = { 0,1,0,-1};
+int dy[4] = { 1,0,-1,0 };
+//int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+//int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+
+void solve() {
+
+    int N;
+    cin >> N;
+    vector<int> arr(N);
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    deque<int> q;
+
+    for (int i = 1; i <= N; i++) {
+        if (arr[N-i] == 1) {
+            q.push_front(i);
+        }
+        else if (arr[N-i] == 3) {
+            q.push_back(i);
+        }
+        else {
+            int tmp = q.front();
+            q.pop_front();
+            q.push_front(i);
+            q.push_front(tmp);
+        }
+
+
+    }
+
+    for (int i = 0; i < N; i++) {
+        cout << q[i] << " ";
+    }
+
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    // cin >> T;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
