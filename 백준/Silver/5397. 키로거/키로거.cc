febc63d991f4ec877c3b46146d8ec13012014f0f
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-05-10 15:28:11 +0900

    [Silver II] Title: 키로거, Time: 112 ms, Memory: 5708 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.cc
@@ -0,0 +1,132 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+const string debug = "output: ";
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll powmod(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * powmod(x, n - 1, mod) % mod;
+    ll half = powmod(x, n / 2, mod);
+    return half * half % mod;
+}
+
+ll modinv(ll x, ll mod) {
+    return powmod(x, mod - 2, mod);
+}
+
+ll factorial(ll x) {
+    ll ret = 1;
+    // ret = F[x];
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    return factorial(n) * modinv(factorial(n - r), mod) % mod * modinv(factorial(r), mod) % mod;
+}
+
+//int dx[4] = { 0,1,0,-1};
+//int dy[4] = { 1,0,-1,0 };
+//int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+//int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+void solve() {
+    deque<char> q1;
+    deque<char> q2;
+    string s;
+    cin >> s;
+    for (char x : s) {
+        if (x == '<') {
+            if (q1.empty()) continue;
+            q2.push_front(q1.back());
+            q1.pop_back();
+        }
+        else if (x == '>') {
+            if (q2.empty()) continue;
+            q1.push_back(q2.front());
+            q2.pop_front();
+        }
+        else if (x == '-') {
+            if (q1.empty()) continue;
+            q1.pop_back();
+        }
+        else {
+            q1.push_back(x);
+        }
+    }
+
+    for (int i = 0; i < q1.size(); i++) {
+        cout << q1[i];
+    }
+    for (int i = 0; i < q2.size(); i++) {
+        cout << q2[i];
+    }
+    cout << endl;
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
+
