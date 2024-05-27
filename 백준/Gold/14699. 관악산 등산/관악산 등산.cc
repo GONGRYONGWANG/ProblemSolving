1c85b14ae9556b7a69ac14a20ebe75e81056c48e
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-05-27 15:35:02 +0900

    [Gold IV] Title: 관악산 등산, Time: 20 ms, Memory: 3096 KB -BaekjoonHub

---
+++ 백준/Gold/14699. 관악산 등산/관악산 등산.cc
@@ -0,0 +1,147 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+const string debug = "output: ";
+
+struct VectorHasher {
+    int operator()(const vector<int>& V) const {
+        int hash = V.size();
+        for (auto& i : V) {
+            hash ^= i + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+        }
+        return hash;
+    }
+};
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll powmod(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * powmod(x, n - 1, mod) % mod;
+    ll half = powmod(x, n / 2, mod);
+    return half * half % mod;
+}
+
+ll modinv(ll x, ll mod) {
+    return powmod(x, mod - 2, mod);
+}
+
+ll factorial(ll x) {
+    ll ret = 1;
+    // ret = F[x];
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    return factorial(n) * modinv(factorial(n - r), mod) % mod * modinv(factorial(r), mod) % mod;
+}
+
+//int dx[4] = { 0,1,0,-1};
+//int dy[4] = { 1,0,-1,0 };
+//int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+//int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+
+int height[5001];
+vector<int> E[5001];
+int cnt[5001];
+int ans[5001];
+
+void solve() {
+    int N, M;
+    cin >> N >> M;
+    for (int i = 1; i <= N; i++) {
+        cin >> height[i];
+        ans[i] = 1;
+    }
+
+    for (int i = 0; i < M; i++) {
+        int u, v;
+        cin >> u >> v;
+        if (height[u] < height[v]) swap(u, v);
+        E[u].push_back(v);
+        cnt[v] += 1;
+    }
+
+    queue<int> q;
+    for (int i = 1; i <= N; i++) {
+        if (cnt[i] == 0) q.push(i);
+    }
+
+    while(!q.empty()){
+        int cur = q.front();
+        q.pop();
+        for (int e : E[cur]) {
+            ans[e] = max(ans[e], ans[cur] + 1);
+            cnt[e] -= 1;
+            if (cnt[e] == 0) q.push(e);
+        }
+    }
+
+    for (int i = 1; i <= N; i++) {
+        cout << ans[i] << endl;
+    }
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
