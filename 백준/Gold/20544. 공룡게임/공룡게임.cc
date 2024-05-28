23f9331fcb6c8fa774f92055b1231a6b92299a68
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-05-28 16:42:39 +0900

    [Gold IV] Title: 공룡게임, Time: 0 ms, Memory: 2068 KB -BaekjoonHub

---
+++ 백준/Gold/20544. 공룡게임/공룡게임.cc
@@ -0,0 +1,134 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+const string debug = "output: ";
+
+struct VectorHasher {
+    int operator()(const vector<int>& V) const {
+        int hash = V.size();
+        for (auto& i : V) {
+            hash ^= i + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+        }
+        return hash;
+    }
+};
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll powmod(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * powmod(x, n - 1, mod) % mod;
+    ll half = powmod(x, n / 2, mod);
+    return half * half % mod;
+}
+
+ll modinv(ll x, ll mod) {
+    return powmod(x, mod - 2, mod);
+}
+
+ll factorial(ll x) {
+    ll ret = 1;
+    // ret = F[x];
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    return factorial(n) * modinv(factorial(n - r), mod) % mod * modinv(factorial(r), mod) % mod;
+}
+
+int ccw(pll a, pll b, pll c) {
+    return (b.first - a.first) * (c.second - a.second) - (c.first - a.first) * (b.second - a.second);
+}
+
+//int dx[4] = { 0,1,0,-1};
+//int dy[4] = { 1,0,-1,0 };
+//int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+//int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+int N;
+ll mod = 1e9 + 7;
+ll DP[1001][3][2];
+ll dp(int x, int p, int k) {
+    if (x >= N) {
+        if (k) return 1;
+        else return 0;
+    }
+    if (DP[x][p][k] != -1) return DP[x][p][k];
+    if (p == 0) return DP[x][p][k] = (dp(x + 1, 2, 1) + dp(x + 1, 1, k) + dp(x + 1, 0, k)) % mod;
+    if (p == 1) return DP[x][p][k] = (dp(x + 2, 0, 1) + dp(x + 2, 0, k) + dp(x + 1, 0, k)) % mod;
+    if (p == 2) return DP[x][p][k] = (dp(x + 2, 0, k) + dp(x + 1, 0, k)) % mod;
+}
+
+void solve() {
+    cin >> N;
+    for (int i = 0; i <= N; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 2; k++) {
+                DP[i][j][k] = -1;
+            }
+        }
+    }
+
+    cout << dp(1, 0, 0);
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
