fe0ce30607e496920f57eee7d6a5c8c285707f9b
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-06-18 16:46:44 +0900

    [Gold V] Title: 병약한 윤호, Time: 4 ms, Memory: 28396 KB -BaekjoonHub

---
+++ 백준/Gold/14677. 병약한 윤호/병약한 윤호.cc
@@ -0,0 +1,148 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+const string debug = "output: ";
+
+struct VectorHasher {
+    int operator()(const vector<int>& V) const {
+        int hash = V.size();
+        for (auto& i : V) {
+            hash ^= i + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+        }
+        return hash;
+    }
+};
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll powmod(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * powmod(x, n - 1, mod) % mod;
+    ll half = powmod(x, n / 2, mod);
+    return half * half % mod;
+}
+
+ll modinv(ll x, ll mod) {
+    return powmod(x, mod - 2, mod);
+}
+
+ll factorial(ll x) {
+    ll ret = 1;
+    // ret = F[x];
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    return factorial(n) * modinv(factorial(n - r), mod) % mod * modinv(factorial(r), mod) % mod;
+}
+
+int ccw(pll a, pll b, pll c) {
+    return (b.first - a.first) * (c.second - a.second) - (c.first - a.first) * (b.second - a.second);
+}
+
+//int dx[4] = { 0,1,0,-1};
+//int dy[4] = { 1,0,-1,0 };
+//int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+//int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+int visited[1500][1500][3];
+int arr[1500];
+
+void solve() {
+    int N;
+    cin >> N;
+    for (int i = 0; i < 3*N; i++) {
+        char x;
+        cin >> x;
+        if (x == 'B') arr[i] = 0;
+        else if (x == 'L') arr[i] = 1;
+        else arr[i] = 2;
+    }
+
+    int ans = 0;
+
+    queue<vector<int>> q;
+    q.push({ 0,3 * N - 1,0 });
+    visited[0][3 * N - 1][0] = true;
+    while (!q.empty()) {
+        int l = q.front()[0];
+        int r = q.front()[1];
+        int t = q.front()[2];
+        q.pop();
+        ans = max(l + 3 * N - r - 1, ans);
+        if (l > r) continue;
+        if (arr[l] == t && !visited[l+1][r][(t+1)%3]) {
+            q.push({ l + 1,r,(t + 1) % 3 });
+            visited[l + 1][r][(t + 1) % 3] = true;
+        }
+        if (arr[r] == t && !visited[l][r-1][(t + 1) % 3]) {
+            q.push({ l,r-1,(t + 1) % 3 });
+            visited[l][r-1][(t + 1) % 3] = true;
+        }
+    }
+
+    cout << ans;
+
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
