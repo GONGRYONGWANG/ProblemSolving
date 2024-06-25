bfb15779100e254bf27ce840772c538bc54eb08c
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-06-25 16:30:37 +0900

    [Gold III] Title: 연예인은 힘들어, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Gold/17270. 연예인은 힘들어/연예인은 힘들어.cc
@@ -0,0 +1,182 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+const string debug = "output: ";
+
+struct VectorHasher {
+    int operator()(const vector<int>& V) const {
+        int hash = V.size();
+        for (auto& i : V) {
+            hash ^= i + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+        }
+        return hash;
+    }
+};
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll powmod(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * powmod(x, n - 1, mod) % mod;
+    ll half = powmod(x, n / 2, mod);
+    return half * half % mod;
+}
+
+ll modinv(ll x, ll mod) {
+    return powmod(x, mod - 2, mod);
+}
+
+
+// F[];
+ll factorial(ll x) {
+    ll ret = 1;
+    //ret = F[x];
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    return factorial(n) * modinv(factorial(n - r), mod) % mod * modinv(factorial(r), mod) % mod;
+}
+
+int ccw(pll a, pll b, pll c) {
+    return (b.first - a.first) * (c.second - a.second) - (c.first - a.first) * (b.second - a.second);
+}
+
+int dx[4] = { 0,1,0,-1};
+int dy[4] = { 1,0,-1,0 };
+//int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+//int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+vector<pii> E[101];
+int distj[101];
+int dists[101];
+
+struct cmp {
+    bool operator()(pii a, pii b) {
+        return a.second > b.second;
+    }
+};
+
+void solve() {
+    int N, M;
+    cin >> N >> M;
+
+    for (int i = 0; i < M; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        E[a].push_back({ b,c });
+        E[b].push_back({ a,c });
+    }
+
+    int j, s;
+    cin >> j >> s;
+
+    pq<pii, vector<pii>, cmp> q;
+    q.push({ j,1 });
+    while (!q.empty()) {
+        int cur = q.top().first;
+        int d = q.top().second;
+        q.pop();
+        if (distj[cur]) continue;
+        distj[cur] = d;
+        for (pii nx : E[cur]) {
+            if (distj[nx.first]) continue;
+            q.push({ nx.first, nx.second + d });
+        }
+    }
+
+    q.push({ s,1 });
+    while(!q.empty()){
+        int cur = q.top().first;
+        int d = q.top().second;
+        q.pop();
+        if (dists[cur]) continue;
+        dists[cur] = d;
+        for (pii nx : E[cur]) {
+            if (dists[nx.first]) continue;
+            q.push({ nx.first, nx.second + d });
+        }
+    }
+
+    int mindist = inf;
+    int ans = 0;
+    distj[0] = inf;
+    dists[0] = inf;
+
+    for (int i = 1; i <= N; i++) {
+
+        if (i == j || i == s) continue;
+        if (distj[i] == 0 || dists[i] == 0) continue;
+        mindist = min(mindist, distj[i] + dists[i]);
+        if (distj[i] + dists[i] != mindist) continue;
+        if (distj[i] > dists[i]) continue;
+
+        if (distj[ans] + dists[ans] != mindist) ans = i;
+        else if (distj[i] < distj[ans]) ans = i;
+    }
+
+    if (mindist != distj[ans] + dists[ans]) cout << -1;
+    else cout << ans;
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
