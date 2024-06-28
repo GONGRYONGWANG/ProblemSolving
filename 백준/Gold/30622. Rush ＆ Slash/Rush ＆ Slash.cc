5938b2e81c2a24acb040f3593bf5c615b1bce9f8
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-06-28 18:54:09 +0900

    [Gold II] Title: Rush & Slash, Time: 96 ms, Memory: 10136 KB -BaekjoonHub

---
+++ 백준/Gold/30622. Rush ＆ Slash/Rush ＆ Slash.cc
@@ -0,0 +1,160 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+const string debug = "output: ";
+
+struct VectorHasher {
+    int operator()(const vector<int>& V) const {
+        int hash = V.size();
+        for (auto& i : V) {
+            hash ^= i + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+        }
+        return hash;
+    }
+};
+
+struct PiiHasher {
+    size_t operator()(const pair<int, int>& x) const {
+        return hash<long long>()(((long long)x.first) ^
+            (((long long)x.second) << 32));
+    }
+};
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll powmod(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * powmod(x, n - 1, mod) % mod;
+    ll half = powmod(x, n / 2, mod);
+    return half * half % mod;
+}
+
+ll modinv(ll x, ll mod) {
+    return powmod(x, mod - 2, mod);
+}
+
+
+// F[];
+ll factorial(ll x) {
+    ll ret = 1;
+    //ret = F[x];
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    return factorial(n) * modinv(factorial(n - r), mod) % mod * modinv(factorial(r), mod) % mod;
+}
+
+int ccw(pll a, pll b, pll c) {
+    return (b.first - a.first) * (c.second - a.second) - (c.first - a.first) * (b.second - a.second);
+}
+
+//int dx[4] = { 0,1,0,-1};
+//int dy[4] = { 1,0,-1,0 };
+int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+
+int parent[100001];
+ll dist[100001];
+int findp(int x) {
+    if (parent[x] == x) return x;
+    return parent[x] = findp(parent[x]);
+}
+
+void solve() {
+    int N;
+    cin >> N;
+
+    unordered_map<pii, int, PiiHasher> m;
+
+    for (int i = 1; i <= N; i++) {
+        parent[i] = i;
+        int x, y;
+        cin >> x >> y;
+        dist[i] = abs(x) + abs(y);
+        m[{x, y}] = i;
+        for (int dir = 0; dir < 8; dir++) {
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+            if (m.count({ nx,ny })) {
+                int a = findp(i);
+                int b = findp(m[{nx, ny}]);
+                if (dist[a] < dist[b]) parent[b] = a;
+                else parent[a] = b;
+            }
+        }
+    }
+
+    ll mx = 0;
+    ll total = 0;
+    for (int i = 1; i <= N; i++) {
+        if (findp(i) != i) continue;
+        mx = max(dist[i], mx);
+        total += dist[i];
+    }
+
+    cout << total * 2 - mx;
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
