8afb41ca7c12584fdeee577eb483c3fad6cc628e
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-07-03 15:23:39 +0900

    [Gold II] Title: 평균값 수열, Time: 712 ms, Memory: 139520 KB -BaekjoonHub

---
+++ 백준/Gold/5485. 평균값 수열/평균값 수열.cc
@@ -0,0 +1,178 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+const string debug = "output: ";
+
+struct VectorHasher {
+    size_t operator()(const vector<int>& V) const {
+        size_t hash = V.size();
+        for (auto& i : V) {
+            hash ^= i + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+        }
+        return hash;
+    }
+};
+
+struct PiiHasher {
+    size_t operator()(const pii& x) const {
+        return hash<long long>()(((long long)x.first) ^ (((long long)x.second) << 32));
+    }
+};
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll powmod(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * powmod(x, n - 1, mod) % mod;
+    ll half = powmod(x, n / 2, mod);
+    return half * half % mod;
+}
+
+ll modinv(ll x, ll mod) {
+    return powmod(x, mod - 2, mod);
+}
+
+
+// F[];
+ll factorial(ll x) {
+    ll ret = 1;
+    //ret = F[x];
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    return factorial(n) * modinv(factorial(n - r), mod) % mod * modinv(factorial(r), mod) % mod;
+}
+
+ll ccw(pll a, pll b, pll c) {
+    return (b.first - a.first) * (c.second - a.second) - (c.first - a.first) * (b.second - a.second);
+}
+
+bool intersect(pll A, pll B, pll C, pll D) { // 선분 교차 판정 A-B , C-D
+    if (A > B) swap(A, B);
+    if (C > D) swap(C, D);
+
+    ll l1 = ccw(A, B, C) * ccw(A, B, D);
+    ll l2 = ccw(C, D, A) * ccw(C, D, B);
+
+    if (l1 == 0 && l2 == 0) {
+        return A <= D && C <= B;
+    }
+    return l1 <= 0 && l2 <= 0;
+}
+
+
+
+//int dx[4] = { 0,1,0,-1};
+//int dy[4] = { 1,0,-1,0 };
+//int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+//int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+void solve() {
+    int N;
+    cin >> N;
+    vector<ll> arr(N);
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+        arr[i] *= 2;
+    }
+    vector<ll> arr2 = { 0,0 };
+    for (int i = 0; i < N - 1; i++) {
+        arr2.push_back(arr[i + 1] - arr[i]);
+        if (arr[i + 1] - arr[i] < 0) {
+            cout << 0;
+            return;
+        }
+    }
+    for (int i = 2; i <= N; i++) {
+        arr2[i] += arr2[i - 2];
+    }
+
+
+    ll l = arr2[0];
+    ll r = arr2[2];
+
+    for (int i = 1; i < arr2.size(); i += 2) {
+        l = max(l, arr2[i - 1] - arr2[i]);
+        if (i != arr2.size() - 1) {
+            r = min(r, arr2[i + 1] - arr2[i]);
+        }
+    }
+
+
+    if (l > r) {
+        cout << 0;
+        return;
+    }
+    ll ans = (r - l + 1) / 2;
+    if (l % 2 == 0 && r % 2 == 0) ans += 1;
+    cout << ans;
+
+
+
+
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
