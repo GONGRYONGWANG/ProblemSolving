09cc07dcc98fba86ce85eb0465fbf584c9cf5956
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-07-03 22:31:24 +0900

    [Diamond V] Title: Confuzzle, Time: 852 ms, Memory: 17208 KB -BaekjoonHub

---
+++ 백준/Diamond/20297. Confuzzle/Confuzzle.cc
@@ -0,0 +1,223 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+const string debug = "output: ";
+
+struct VectorHasher {
+    size_t operator()(const vector<int>& V) const {
+        size_t hash = V.size();
+        for (auto& i : V) {
+            hash ^= i + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+        }
+        return hash;
+    }
+};
+
+struct PiiHasher {
+    size_t operator()(const pii& x) const {
+        return hash<long long>()(((long long)x.first) ^ (((long long)x.second) << 32));
+    }
+};
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll powmod(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * powmod(x, n - 1, mod) % mod;
+    ll half = powmod(x, n / 2, mod);
+    return half * half % mod;
+}
+
+ll modinv(ll x, ll mod) {
+    return powmod(x, mod - 2, mod);
+}
+
+
+// F[];
+ll factorial(ll x) {
+    ll ret = 1;
+    //ret = F[x];
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    return factorial(n) * modinv(factorial(n - r), mod) % mod * modinv(factorial(r), mod) % mod;
+}
+
+ll ccw(pll a, pll b, pll c) {
+    return (b.first - a.first) * (c.second - a.second) - (c.first - a.first) * (b.second - a.second);
+}
+
+bool intersect(pll A, pll B, pll C, pll D) { // 선분 교차 판정 A-B , C-D
+    if (A > B) swap(A, B);
+    if (C > D) swap(C, D);
+
+    ll l1 = ccw(A, B, C) * ccw(A, B, D);
+    ll l2 = ccw(C, D, A) * ccw(C, D, B);
+
+    if (l1 == 0 && l2 == 0) {
+        return A <= D && C <= B;
+    }
+    return l1 <= 0 && l2 <= 0;
+}
+
+
+
+//int dx[4] = { 0,1,0,-1};
+//int dy[4] = { 1,0,-1,0 };
+//int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+//int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+int arr[100001];
+vector<int> E[100001];
+
+int ans;
+
+bool visited[100001];
+int sz[100001];
+void getsz(int cur, int prv) {
+    sz[cur] = 1;
+    for (int nx : E[cur]) {
+        if (nx == prv || visited[nx]) continue;
+        getsz(nx, cur);
+        sz[cur] += sz[nx];
+    }
+}
+
+int getcent(int cur, int prv, int size) {
+    for (int nx : E[cur]) {
+        if (nx == prv || visited[nx]) continue;
+        if (sz[nx] > size / 2) return getcent(nx, cur, size);
+    }
+    return cur;
+}
+
+map<int, int> m;
+
+void dnq(int x) {
+    getsz(x, -1);
+    int cent = getcent(x, -1, sz[x]);
+    visited[cent] = true;
+    m.clear();
+    m[arr[cent]] = 0;
+    for (int nx : E[cent]) {
+        if (visited[nx]) continue;
+        queue<vector<int>> q;
+        q.push({ nx,cent,1 }); // cur, prv, depth
+        while (!q.empty()) {
+            int cur = q.front()[0];
+            int prv = q.front()[1];
+            int depth = q.front()[2];
+            q.pop();
+            if (m.count(arr[cur])) {
+                ans = min(ans, m[arr[cur]] + depth);
+            }
+            for (int nxt : E[cur]) {
+                if (nxt == prv || visited[nxt]) continue;
+                q.push({ nxt, cur, depth + 1 });
+            }
+        }
+        q.push({ nx,cent,1 }); // cur, prv, depth
+        while (!q.empty()) {
+            int cur = q.front()[0];
+            int prv = q.front()[1];
+            int depth = q.front()[2];
+            q.pop();
+            if (!m.count(arr[cur])) {
+                m[arr[cur]] = depth;
+            }
+            else m[arr[cur]] = min(m[arr[cur]], depth);
+            for (int nxt : E[cur]) {
+                if (nxt == prv || visited[nxt]) continue;
+                q.push({ nxt, cur, depth + 1 });
+            }
+        }
+    }
+
+    for (int nx : E[cent]) {
+        if (visited[nx]) continue;
+        dnq(nx);
+    }
+
+
+}
+
+void solve() {
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < N - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        E[u].push_back(v);
+        E[v].push_back(u);
+    }
+    ans = inf;
+    dnq(1);
+    cout << ans;
+
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
