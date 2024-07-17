31eb0e7dfc513972d2cf9879eb92f066ef9ea21e
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-07-17 16:14:59 +0900

    [Platinum IV] Title: 시계 사진들, Time: 72 ms, Memory: 5272 KB -BaekjoonHub

---
+++ 백준/Platinum/10266. 시계 사진들/시계 사진들.cc
@@ -0,0 +1,213 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+const string debug = "output: ";
+
+struct VectorHasher {
+    size_t operator()(const vector<int>& V) const {
+        size_t hash = V.size();
+        for (auto& i : V) {
+            hash ^= i + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+        }
+        return hash;
+    }
+};
+
+struct PiiHasher {
+    size_t operator()(const pii& x) const {
+        return hash<long long>()(((long long)x.first) ^ (((long long)x.second) << 32));
+    }
+};
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll powmod(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * powmod(x, n - 1, mod) % mod;
+    ll half = powmod(x, n / 2, mod);
+    return half * half % mod;
+}
+
+
+
+ll modinv(ll x, ll mod) {
+    return powmod(x, mod - 2, mod);
+}
+
+
+//const ll Fsize = 1;
+//ll F[Fsize];
+ll factorial(ll x) {
+    ll ret = 1;
+    //ret = F[x]; //
+    return ret;
+}
+
+//ll modI[Fsize];  ll modIF[Fsize];
+ll modinvfactorial(ll x) {
+    ll ret = 1;
+    //ret = modIF[x]; //
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    if (r == 0 || n == r) return 1;
+    return factorial(n) * modinvfactorial(n - r) % mod * modinvfactorial(r) % mod;
+}
+
+ll ccw(pll a, pll b, pll c) {
+    ll ret = (b.first - a.first) * (c.second - a.second) - (c.first - a.first) * (b.second - a.second);
+    if (ret > 0) return 1;
+    else if (ret == 0) return 0;
+    else return -1;
+}
+
+bool intersect(pll A, pll B, pll C, pll D) { // 선분 교차 판정 A-B , C-D
+    if (A > B) swap(A, B);
+    if (C > D) swap(C, D);
+
+    ll l1 = ccw(A, B, C) * ccw(A, B, D);
+    ll l2 = ccw(C, D, A) * ccw(C, D, B);
+
+    if (l1 == 0 && l2 == 0) {
+        return A <= D && C <= B;
+    }
+    return l1 <= 0 && l2 <= 0;
+}
+
+
+
+//int dx[4] = { 0,1,0,-1};
+//int dy[4] = { 1,0,-1,0 };
+//int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+//int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+void solve() {
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    vector<int> B(N);
+
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+    for (int i = 0; i < N; i++) {
+        cin >> B[i];
+    }
+
+    sort(A.begin(), A.end());
+    sort(B.begin(), B.end());
+    int A0 = A[0];
+    int B0 = B[0];
+
+    for (int i = 0; i < N - 1; i++) {
+        A[i] = A[i + 1] - A[i];
+        B[i] = B[i + 1] - B[i];
+    }
+
+    A[N - 1] = A0 + 360000 - A[N - 1];
+    B[N - 1] = B0 + 360000 - B[N - 1];
+
+    for (int i = 0; i < N; i++) {
+        A.push_back(A[i]);
+    }
+
+    vector<int> lps(N, 0);
+    int len = 0;
+    lps[0] = 0;
+    int idx = 1;
+    while (idx < N) {
+        if (B[idx] == B[len]) {
+            len += 1;
+            lps[idx] = len;
+            idx += 1;
+        }
+        else {
+            if (len != 0)len = lps[len - 1];
+            else {
+                lps[idx] = 0;
+                idx += 1;
+            }
+        }
+    }
+
+    int i = 0; int j = 0;
+    while (i < 2 * N) {
+        if (B[j] == A[i]) { i += 1; j += 1; }
+        if (j == N) {
+            cout << "possible";
+            return;
+        }
+        else if (i < 2 * N && B[j] != A[i]) {
+            if (j != 0) j = lps[j - 1];
+            else i += 1;
+        }
+    }
+
+    cout << "impossible";
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
