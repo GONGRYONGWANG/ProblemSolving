da31dc52492c2d7c9bc58ba3126b528821b90cc1
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-07-29 16:31:31 +0900

    [Gold III] Title: 출퇴근, Time: 728 ms, Memory: 51428 KB, Score: 140 point -BaekjoonHub

---
+++ 백준/Gold/20313. 출퇴근/출퇴근.cc
@@ -0,0 +1,220 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+const string debug = "output: ";
+
+struct VectorHasher {
+    size_t operator()(const vector<int>& V) const {
+        size_t hash = V.size();
+for (auto& i : V) {
+    hash ^= i + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+}
+return hash;
+    }
+};
+
+struct PiiHasher {
+    size_t operator()(const pii& x) const {
+        return hash<long long>()(((long long)x.first) ^ (((long long)x.second) << 32));
+    }
+};
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll powmod(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * powmod(x, n - 1, mod) % mod;
+    ll half = powmod(x, n / 2, mod);
+    return half * half % mod;
+}
+
+
+
+ll modinv(ll x, ll mod) {
+    return powmod(x, mod - 2, mod);
+}
+
+
+//const ll Fsize = 1;
+//ll F[Fsize];
+ll factorial(ll x) {
+    ll ret = 1;
+    //ret = F[x]; //
+    return ret;
+}
+
+//ll modI[Fsize];  ll modIF[Fsize];
+ll modinvfactorial(ll x) {
+    ll ret = 1;
+    //ret = modIF[x]; //
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    if (r == 0 || n == r) return 1;
+    return factorial(n) * modinvfactorial(n - r) % mod * modinvfactorial(r) % mod;
+}
+
+ll ccw(pll a, pll b, pll c) {
+    ll ret = (b.first - a.first) * (c.second - a.second) - (c.first - a.first) * (b.second - a.second);
+    if (ret > 0) return 1;
+    else if (ret == 0) return 0;
+    else return -1;
+}
+
+bool intersect(pll A, pll B, pll C, pll D) { // 선분 교차 판정 A-B , C-D
+    if (A > B) swap(A, B);
+    if (C > D) swap(C, D);
+
+    ll l1 = ccw(A, B, C) * ccw(A, B, D);
+    ll l2 = ccw(C, D, A) * ccw(C, D, B);
+
+    if (l1 == 0 && l2 == 0) {
+        return A <= D && C <= B;
+    }
+    return l1 <= 0 && l2 <= 0;
+}
+
+
+
+//int dx[4] = { 0,1,0,-1};
+//int dy[4] = { 1,0,-1,0 };
+//int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+//int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+pii edge[2000];
+
+vector<vector<ll>> E[1001][101];
+
+ll visited[1001][101];
+
+struct cmp {
+    bool operator()(vector<ll> a, vector<ll> b) {
+        return a[2] > b[2];
+    }
+};
+
+void solve() {
+    int N, M, A, B;
+    cin >> N >> M >> A >> B;
+
+    for (int i = 0; i < M; i++) {
+        int u, v;
+        ll T;
+        cin >> u >> v >> T;
+        edge[i] = { u,v };
+        E[u][0].push_back({ v,0,T });
+        E[v][0].push_back({ u,0,T });
+    }
+
+    int K;
+    cin >> K;
+    for (int i = 1; i <= K; i++) {
+        for (int j = 0; j < M; j++) {
+            int T;
+            cin >> T;
+            int u, v;
+            u = edge[j].first; v = edge[j].second;
+            E[u][i].push_back({ v,i,T });
+            E[v][i].push_back({ u,i,T });
+        }
+        for (int j = 1; j <= N; j++) {
+            E[j][i - 1].push_back({ j,i,0 });
+        }
+    }
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 0; j <= K; j++ ) {
+            visited[i][j] = -1;
+        }
+    }
+
+    pq<vector<ll>, vector<vector<ll>>, cmp> q;
+    q.push({ A,0,0 });
+    while (!q.empty()) {
+        int cur = q.top()[0];
+        int k = q.top()[1];
+        ll d = q.top()[2];
+        q.pop();
+        if (visited[cur][k] != -1) continue;
+        visited[cur][k] = d;
+        for (vector<ll> nx : E[cur][k]) {
+            int nxt = nx[0];
+            int nk = nx[1];
+            ll nd = d + nx[2];
+            if (visited[nxt][nk] != -1) continue;
+            q.push({ nxt,nk,nd });
+        }
+    }
+
+    ll ans = INF;
+    for (int i = 0; i <= K; i++) {
+        ans = min(ans, visited[B][i]);
+    }
+    cout << ans;
+
+
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
