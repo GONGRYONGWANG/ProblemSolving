c6ab01b87c5b4bb0ae09fed6806985bf4c00480e
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-08-01 20:53:18 +0900

    [Platinum I] Title: 소가 길을 건너간 이유 2020, Time: 488 ms, Memory: 143328 KB -BaekjoonHub

---
+++ 백준/Platinum/19545. 소가 길을 건너간 이유 2020/소가 길을 건너간 이유 2020.cc
@@ -0,0 +1,184 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+const string debug = "output: ";
+
+struct VectorHasher {
+    size_t operator()(const vector<int>& V) const {
+        size_t hash = V.size();
+for (auto& i : V) {
+    hash ^= i + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+}
+return hash;
+    }
+};
+
+struct PiiHasher {
+    size_t operator()(const pii& x) const {
+        return hash<long long>()(((long long)x.first) ^ (((long long)x.second) << 32));
+    }
+};
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll powmod(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * powmod(x, n - 1, mod) % mod;
+    ll half = powmod(x, n / 2, mod);
+    return half * half % mod;
+}
+
+
+
+ll modinv(ll x, ll mod) {
+    return powmod(x, mod - 2, mod);
+}
+
+
+//const ll Fsize = 1;
+//ll F[Fsize];
+ll factorial(ll x) {
+    ll ret = 1;
+    //ret = F[x]; //
+    return ret;
+}
+
+//ll modI[Fsize];  ll modIF[Fsize];
+ll modinvfactorial(ll x) {
+    ll ret = 1;
+    //ret = modIF[x]; //
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    if (r == 0 || n == r) return 1;
+    return factorial(n) * modinvfactorial(n - r) % mod * modinvfactorial(r) % mod;
+}
+
+ll ccw(pll a, pll b, pll c) {
+    ll ret = (b.first - a.first) * (c.second - a.second) - (c.first - a.first) * (b.second - a.second);
+    if (ret > 0) return 1;
+    else if (ret == 0) return 0;
+    else return -1;
+}
+
+bool intersect(pll A, pll B, pll C, pll D) { // 선분 교차 판정 A-B , C-D
+    if (A > B) swap(A, B);
+    if (C > D) swap(C, D);
+
+    ll l1 = ccw(A, B, C) * ccw(A, B, D);
+    ll l2 = ccw(C, D, A) * ccw(C, D, B);
+
+    if (l1 == 0 && l2 == 0) {
+        return A <= D && C <= B;
+    }
+    return l1 <= 0 && l2 <= 0;
+}
+
+
+
+//int dx[4] = { 0,1,0,-1};
+//int dy[4] = { 1,0,-1,0 };
+//int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+//int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+ll N, M, L;
+ll DP[3001][3001][2];
+vector<ll> A, B;
+ll dist(int a, int b) {
+    return L * L + (A[a] - B[b]) * (A[a] - B[b]);
+}
+ll dp(int a, int b, int x) {
+    if (a == N && b == M) return 0;
+    if (DP[a][b][x]) return DP[a][b][x];
+    if (!x) {
+        if (b == M) return INF;
+        return DP[a][b][x] = min(dp(a, b + 1, x) + dist(a - 1, b) * (N + M - 1), dp(a, b + 1, !x) + dist(a - 1, b) * (a + b) * (N + M - a - b));
+
+    }
+    else {
+        if (a == N) return INF;
+        return DP[a][b][x] = min(dp(a + 1, b, x) + dist(a, b - 1) * (N + M - 1), dp(a + 1, b, !x) + dist(a, b - 1) * (a + b) * (N + M - a - b));
+    }
+}
+
+
+void solve() {
+    cin >> N >> M >> L;
+    A.resize(N); B.resize(M);
+    for (int i = 0; i < N; i++) {
+        cin >> A[i];
+    }
+    sort(A.begin(), A.end());
+    for (int i = 0; i < M; i++) {
+        cin >> B[i];
+    }
+    sort(B.begin(), B.end());
+
+    cout << min(dp(1, 0, 0), dp(0, 1, 1));
+
+
+
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
