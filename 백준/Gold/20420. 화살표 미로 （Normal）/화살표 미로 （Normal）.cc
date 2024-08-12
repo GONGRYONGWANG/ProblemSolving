49cf350c7474fa173a751511d85705960538d4ff
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-08-12 14:47:38 +0900

    [Gold I] Title: 화살표 미로 (Normal), Time: 2464 ms, Memory: 86460 KB -BaekjoonHub

---
+++ 백준/Gold/20420. 화살표 미로 （Normal）/화살표 미로 （Normal）.cc
@@ -0,0 +1,208 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+const string debug = "output: ";
+
+struct VectorHasher {
+    size_t operator()(const vector<int>& V) const {
+        size_t hash = V.size();
+        for (auto& i : V) {
+            hash ^= i + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+        }
+        return hash;
+    }
+};
+
+struct PiiHasher {
+    size_t operator()(const pii& x) const {
+        return hash<long long>()(((long long)x.first) ^ (((long long)x.second) << 32));
+    }
+};
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll powmod(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * powmod(x, n - 1, mod) % mod;
+    ll half = powmod(x, n / 2, mod);
+    return half * half % mod;
+}
+
+
+
+ll modinv(ll x, ll mod) { // mod is primenum
+    return powmod(x, mod - 2, mod);
+}
+
+
+//const ll Fsize = 1;
+//ll F[Fsize];
+ll factorial(ll x) {
+    ll ret = 1;
+    //ret = F[x]; //
+    return ret;
+}
+
+//ll modI[Fsize];  ll modIF[Fsize];
+ll modinvfactorial(ll x) {
+    ll ret = 1;
+    //ret = modIF[x]; //
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    if (r == 0 || n == r) return 1;
+    return factorial(n) * modinvfactorial(n - r) % mod * modinvfactorial(r) % mod;
+}
+
+ll ccw(pll a, pll b, pll c) {
+    ll ret = (b.first - a.first) * (c.second - a.second) - (c.first - a.first) * (b.second - a.second);
+    if (ret > 0) return 1;
+    else if (ret == 0) return 0;
+    else return -1;
+}
+
+bool intersect(pll A, pll B, pll C, pll D) { // 선분 교차 판정 A-B , C-D
+    if (A > B) swap(A, B);
+    if (C > D) swap(C, D);
+
+    ll l1 = ccw(A, B, C) * ccw(A, B, D);
+    ll l2 = ccw(C, D, A) * ccw(C, D, B);
+
+    if (l1 == 0 && l2 == 0) {
+        return A <= D && C <= B;
+    }
+    return l1 <= 0 && l2 <= 0;
+}
+
+
+
+int dx[4] = { 1,0,-1,0};
+int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+//int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+int board[50][50];
+bool visited[50][50][151][151];
+
+void solve() {
+
+
+    int N, M, K;
+    cin >> N >> M >> K;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            char x;
+            cin >> x;
+            int dir;
+            if (x == 'D') dir = 0;
+            else if (x == 'R') dir = 1;
+            else if (x == 'U') dir = 2;
+            else dir = 3;
+            board[i][j] = dir;
+        }
+    }
+
+    queue<vector<int>> q;
+    q.push({ 0,0,K,K });
+    while (!q.empty()) {
+        int x = q.front()[0];
+        int y = q.front()[1];
+        int l = q.front()[2];
+        int r = q.front()[3];
+        q.pop();
+
+        int dir = board[x][y];
+
+        if (x == N - 1 && y == M - 1) {
+            cout << "Yes";
+            return;
+        }
+
+
+        for (int i = 0; i <= min(3, l); i++) {
+            int nx = x + dx[(dir + i) % 4];
+            int ny = y + dy[(dir + i) % 4];
+            if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+            if (visited[nx][ny][l - i][r]) continue;
+            visited[nx][ny][l - i][r] = true;
+            q.push({ nx,ny,l - i,r });
+        }
+
+        for (int i = 0; i <= min(3, r); i++) {
+            int nx = x + dx[(dir - i + 4) % 4];
+            int ny = y + dy[(dir - i + 4) % 4];
+            if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+            if (visited[nx][ny][l][r-i]) continue;
+            visited[nx][ny][l][r-i] = true;
+            q.push({ nx,ny,l,r - i });
+        }
+
+
+    }
+
+    cout << "No";
+
+
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
