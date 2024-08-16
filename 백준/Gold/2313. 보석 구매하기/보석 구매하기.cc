f0d087a244c191c0b10c97fadd43d5e721e2568b
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-08-16 18:30:14 +0900

    [Gold V] Title: 보석 구매하기, Time: 76 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Gold/2313. 보석 구매하기/보석 구매하기.cc
@@ -0,0 +1,196 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+const string debug = "output: ";
+
+struct VectorHasher {
+    size_t operator()(const vector<int>& V) const {
+        size_t hash = V.size();
+        for (auto& i : V) {
+            hash ^= i + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+        }
+        return hash;
+    }
+};
+
+struct PiiHasher {
+    size_t operator()(const pii& x) const {
+        return hash<long long>()(((long long)x.first) ^ (((long long)x.second) << 32));
+    }
+};
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll xgcd(ll a, ll b) {
+    ll r1 = a; ll r2 = b;
+    ll s1 = 1; ll s2 = 0;
+    ll t1 = 0; ll t2 = 1;
+    while (1) {
+        ll q = r1 / r2;
+        ll r = r1 - q * r2;
+        ll s = s1 - q * s2;
+        ll t = t1 - q * t2;
+        if (r == 0) return s2;
+        r1 = r2; r2 = r;
+        s1 = s2; s2 = s;
+        t1 = t2; t2 = t;
+    }
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll powmod(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * powmod(x, n - 1, mod) % mod;
+    ll half = powmod(x, n / 2, mod);
+    return half * half % mod;
+}
+
+
+
+ll modinv(ll x, ll mod) { // mod is primenum
+    return powmod(x, mod - 2, mod);
+}
+
+
+//const ll Fsize = 1;
+//ll F[Fsize];
+ll factorial(ll x) {
+    ll ret = 1;
+    //ret = F[x]; //
+    return ret;
+}
+
+//ll modI[Fsize];  ll modIF[Fsize];
+ll modinvfactorial(ll x) {
+    ll ret = 1;
+    //ret = modIF[x]; //
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    if (r == 0 || n == r) return 1;
+    return factorial(n) * modinvfactorial(n - r) % mod * modinvfactorial(r) % mod;
+}
+
+ll ccw(pll a, pll b, pll c) {
+    ll ret = (b.first - a.first) * (c.second - a.second) - (c.first - a.first) * (b.second - a.second);
+    if (ret > 0) return 1;
+    else if (ret == 0) return 0;
+    else return -1;
+}
+
+bool intersect(pll A, pll B, pll C, pll D) { // 선분 교차 판정 A-B , C-D
+    if (A > B) swap(A, B);
+    if (C > D) swap(C, D);
+
+    ll l1 = ccw(A, B, C) * ccw(A, B, D);
+    ll l2 = ccw(C, D, A) * ccw(C, D, B);
+
+    if (l1 == 0 && l2 == 0) {
+        return A <= D && C <= B;
+    }
+    return l1 <= 0 && l2 <= 0;
+}
+
+
+
+//int dx[4] = { 1,0,-1,0 }; // DRUL
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+//int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+void solve() {
+    int N;
+    cin >> N;
+    ll total = 0;
+    vector<pii> ans;
+    for (int i = 0; i < N; i++) {
+        pii ret = { 0,1 };
+        int mn = 0;
+        int k;
+        cin >> k;
+        vector<ll> psum(k + 1, 0);
+        for (int j = 1; j <= k; j++) {
+            cin >> psum[j];
+            psum[j] += psum[j - 1];
+            if (psum[j] - psum[mn] > psum[ret.second] - psum[ret.first]) {
+                ret = { mn,j };
+            }
+            else if (psum[j] - psum[mn] == psum[ret.second] - psum[ret.first] && j - mn < ret.second - ret.first) {
+                ret = { mn,j };
+            }
+
+            if (psum[j] <= psum[mn]) mn = j;
+        }
+        ans.push_back(ret);
+        total += psum[ret.second] - psum[ret.first];
+    }
+    cout << total << endl;
+    for (pii x : ans) {
+        cout << x.first + 1 << " " << x.second << endl;
+    }
+
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
