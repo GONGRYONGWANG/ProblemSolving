15df89933b80c147953d57cf385adab7b318bde4
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-08-19 16:41:04 +0900

    [Platinum IV] Title: 증가 수열의 개수, Time: 556 ms, Memory: 49020 KB -BaekjoonHub

---
+++ 백준/Platinum/17409. 증가 수열의 개수/증가 수열의 개수.cc
@@ -0,0 +1,213 @@
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+const string debug = "output: ";
+
+struct VectorHasher {
+    size_t operator()(const vector<int>& V) const {
+        size_t hash = V.size();
+        for (auto& i : V) {
+            hash ^= i + 0x9e3779b9 + (hash << 6) + (hash >> 2);
+        }
+        return hash;
+    }
+};
+
+struct PiiHasher {
+    size_t operator()(const pii& x) const {
+        return hash<long long>()(((long long)x.first) ^ (((long long)x.second) << 32));
+    }
+};
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll xgcd(ll a, ll b) {
+    ll r1 = a; ll r2 = b;
+    ll s1 = 1; ll s2 = 0;
+    ll t1 = 0; ll t2 = 1;
+    while (1) {
+        ll q = r1 / r2;
+        ll r = r1 - q * r2;
+        ll s = s1 - q * s2;
+        ll t = t1 - q * t2;
+        if (r == 0) return s2;
+        r1 = r2; r2 = r;
+        s1 = s2; s2 = s;
+        t1 = t2; t2 = t;
+    }
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll powmod(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * powmod(x, n - 1, mod) % mod;
+    ll half = powmod(x, n / 2, mod);
+    return half * half % mod;
+}
+
+
+
+ll modinv(ll x, ll mod) { // mod is primenum
+    return powmod(x, mod - 2, mod);
+}
+
+
+//const ll Fsize = 1;
+//ll F[Fsize];
+ll factorial(ll x) {
+    ll ret = 1;
+    //ret = F[x]; //
+    return ret;
+}
+
+//ll modI[Fsize];  ll modIF[Fsize];
+ll modinvfactorial(ll x) {
+    ll ret = 1;
+    //ret = modIF[x]; //
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    if (r == 0 || n == r) return 1;
+    return factorial(n) * modinvfactorial(n - r) % mod * modinvfactorial(r) % mod;
+}
+
+ll ccw(pll a, pll b, pll c) {
+    ll ret = (b.first - a.first) * (c.second - a.second) - (c.first - a.first) * (b.second - a.second);
+    if (ret > 0) return 1;
+    else if (ret == 0) return 0;
+    else return -1;
+}
+
+bool intersect(pll A, pll B, pll C, pll D) { // 선분 교차 판정 A-B , C-D
+    if (A > B) swap(A, B);
+    if (C > D) swap(C, D);
+
+    ll l1 = ccw(A, B, C) * ccw(A, B, D);
+    ll l2 = ccw(C, D, A) * ccw(C, D, B);
+
+    if (l1 == 0 && l2 == 0) {
+        return A <= D && C <= B;
+    }
+    return l1 <= 0 && l2 <= 0;
+}
+
+
+
+//int dx[4] = { 1,0,-1,0 }; // DRUL
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+//int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+vector<ll> emptyvec = { 0,0,0,0,0,0,0,0,0,0,0 };
+vector<ll> tree[400000];
+ll mod = 1e9 + 7;
+vector<ll> get(int node, int start, int end, int left, int right) {
+    if (left > end || right < start) return emptyvec;
+    if (left <= start && right >= end) return tree[node];
+    int mid = (start + end) / 2;
+    vector<ll> v1 = get(node * 2, start, mid, left, right);
+    vector<ll> v2 = get(node * 2 + 1, mid + 1, end, left, right);
+    for (int i = 0; i <= 10; i++) {
+        v1[i] += v2[i];
+        v1[i] %= mod;
+    }
+    return v1;
+}
+
+void update(int node, int start, int end, int target, vector<ll> v) {
+    if (target > end || target < start) return;
+    if (start == end) {
+        tree[node] = v;
+        return;
+    }
+    int mid = (start + end) / 2;
+    update(node * 2, start, mid, target, v);
+    update(node * 2 + 1, mid + 1, end, target, v);
+    for (int i = 0; i <= 10; i++) {
+        tree[node][i] = tree[node * 2][i] + tree[node * 2 + 1][i];
+        tree[node][i] %= mod;
+    }
+
+}
+
+void solve() {
+    int N, K;
+    cin >> N >> K;
+    for (int i = 0; i < 400000; i++) {
+        tree[i] = emptyvec;
+    }
+    for (int i = 0; i < N; i++) {
+        int x;
+        cin >> x;
+        vector<ll> v = get(1, 1, 100000, 1, x);
+        for (int i = 10; i >= 2; i--) {
+            v[i] = v[i - 1];
+        }
+        v[1] = 1;
+        update(1, 1, 100000, x, v);
+    }
+
+    cout << get(1, 1, 100000, 1, 100000)[K];
+
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    while (T--) {
+        solve();
+    }
+    return 0;
+}
