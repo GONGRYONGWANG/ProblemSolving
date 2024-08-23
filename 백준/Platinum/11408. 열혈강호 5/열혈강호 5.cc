6dfc96124b757d8d90ed467e5a51c1430743e413
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-08-23 14:14:25 +0900

    [Platinum III] Title: 열혈강호 5, Time: 232 ms, Memory: 7928 KB -BaekjoonHub

--- 백준/Platinum/11408. 열혈강호 5/열혈강호 5.cc
+++ 백준/Platinum/11408. 열혈강호 5/열혈강호 5.cc
@@ -1,122 +1,205 @@
-#include<iostream>
-#include<iomanip>
-#include<cstdio>
-#include<string>
-#include<vector>
-#include<utility>
-#include<list>
-#include<queue>
-#include<stack>
-#include<deque>
-#include<set>
-#include<unordered_set>
-#include<map>
-#include<unordered_map>
-#include<cmath>
-#include<algorithm>
-#include<cstdlib>
-#include<ctime> // srand(time(0))
-#include<regex> // 정규표현식
-#include<random> // rand
-#include<complex> // complex
-#include<numeric>
+#include<bits/stdc++.h>
+#include<ext/rope>
+/*
+string s;
+s.c_str() -> 스트링을 char arr로
+rope<char> rp(s.c_str());
+rp.insert(idx, s.c_str());
+rp.insert(idx, some_char);'
+cout<<rp.substr(idx, length);
+*/
+
 using namespace std;
+using namespace __gnu_cxx;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+typedef tree<int,null_type,less_equal<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_multiset;
+
+
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 typedef unsigned long long ull;
 typedef unsigned int uint;
-typedef complex<double> cpx;
+
 #define pq priority_queue
 #define endl "\n"
-#define inf 1e9
-//int dx[4] = { 1,-1,0,0 };
-//int dy[4] = { 0,0,1,-1 };
-//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
-//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
+
+const ll INF = 1e18;
+const int inf = 1e9;
 const double pi = 3.14159265358979323846;
 
-const int sz = 803;
-int capacity[sz][sz];
-int flow[sz][sz];
-int dist[sz][sz];
-vector<int> E[sz];
+const string debug = "output: ";
 
-int parent[sz];
-int d[sz];
-bool inq[sz];
-
-int main() {
-    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
-    int N, M;
-    cin >> N >> M;
-    int src = sz-1;
-    int sink =  sz-2;
-    for (int i = 1; i <= N; i++) {
-        E[src].push_back(i);
-        E[i].push_back(src);
-        capacity[src][i] = 1;
-        int a;
-        cin >> a;
-        while (a--) {
-            int x;
-            cin >> x;
-            x += 400;
-            E[i].push_back(x);
-            E[x].push_back(i);
-            capacity[i][x] = 1;
-            cin>>dist[i][x];
-            dist[x][i] = -dist[i][x];
-        }
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
     }
-    for (int i = 401; i <= 400 + M; i++) {
-        E[i].push_back(sink);
-        E[sink].push_back(i);
-        capacity[i][sink] = 1;
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll powmod(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * powmod(x, n - 1, mod) % mod;
+    ll half = powmod(x, n / 2, mod);
+    return half * half % mod;
+}
+
+ll modinv(ll x, ll mod) {
+    return powmod(x, mod - 2, mod);
+}
+
+ll factorial(ll x) {
+    ll ret = 1;
+    // ret = F[x];
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    return factorial(n) * modinv(factorial(n - r), mod) % mod * modinv(factorial(r), mod) % mod;
+}
+
+//int dx[4] = { 1,-1,0,0 };
+//int dy[4] = { 0,0,1,-1 };
+//int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+//int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+
+
+///////////////////////////////////////////////
+struct MCMF {
+    struct Edge {
+        int to, rev;
+        long long cap, flow, cost;
+        Edge(int to, int rev, long long cap, long long cost) : to(to), rev(rev), cap(cap), flow(0), cost(cost) {}
+    };
+
+    int n;
+    vector<vector<Edge>> adj;
+    vector<long long> dist;
+    vector<int> parent, parentEdge;
+    vector<bool> inQueue;
+    int source, sink;
+
+    MCMF(int n) : n(n), adj(n), dist(n), parent(n), parentEdge(n), inQueue(n), source(-1), sink(-1) {}
+
+    void setSource(int s) {
+        source = s;
     }
 
-    int ans1 = 0;
-    int ans2 = 0;
+    void setSink(int t) {
+        sink = t;
+    }
 
-    while (1) {
-        fill(d, d + sz, inf);
-        fill(parent, parent + sz, 0);
-        fill(inq, inq + sz, false);
-        d[src] = 0;
-        inq[src] = true;
-        parent[src] = src;
+    void addEdge(int u, int v, long long cap, long long cost) {
+        adj[u].emplace_back(v, adj[v].size(), cap, cost);
+        adj[v].emplace_back(u, adj[u].size() - 1, 0, -cost); // 역방향 간선
+    }
+
+    bool spfa() {
+        fill(dist.begin(), dist.end(), LLONG_MAX);
+        fill(inQueue.begin(), inQueue.end(), false);
         queue<int> q;
-        q.push(src);
+        dist[source] = 0;
+        inQueue[source] = true;
+        q.push(source);
 
         while (!q.empty()) {
-            int cur = q.front();
+            int u = q.front();
             q.pop();
-            inq[cur] = false;
-            for (int i = 0; i < E[cur].size(); i++) {
-                int next = E[cur][i];
-                if (capacity[cur][next] - flow[cur][next] <= 0 || d[next]<=d[cur]+dist[cur][next]) continue;
-                d[next] = d[cur] + dist[cur][next];
-                parent[next] = cur;
-                if (!inq[next]) {
-                    q.push(next);
-                    inq[next] = true;
+            inQueue[u] = false;
+
+            for (int i = 0; i < adj[u].size(); ++i) {
+                Edge &e = adj[u][i];
+                if (e.flow < e.cap && dist[u] + e.cost < dist[e.to]) {
+                    dist[e.to] = dist[u] + e.cost;
+                    parent[e.to] = u;
+                    parentEdge[e.to] = i;
+
+                    if (!inQueue[e.to]) {
+                        inQueue[e.to] = true;
+                        q.push(e.to);
+                    }
                 }
             }
         }
-        if (!parent[sink]) break;
-        int minflow = inf;
-        for (int i = sink; i != src; i = parent[i]) {
-            minflow = min(minflow, capacity[parent[i]][i] - flow[parent[i]][i]);
-        }
 
-        for (int i = sink; i != src; i = parent[i]) {
-            ans2 += minflow * dist[parent[i]][i];
-            flow[parent[i]][i] += minflow;
-            flow[i][parent[i]] -= minflow;
-        }
-        ans1 += minflow;
+        return dist[sink] != LLONG_MAX;
     }
-    cout << ans1 << endl << ans2;
 
+    pair<long long, long long> mcmf() {
+        long long maxFlow = 0, minCost = 0;
+
+        while (spfa()) {
+            long long flow = LLONG_MAX;
+
+            for (int u = sink; u != source; u = parent[u]) {
+                Edge &e = adj[parent[u]][parentEdge[u]];
+                flow = min(flow, e.cap - e.flow);
+            }
+
+            for (int u = sink; u != source; u = parent[u]) {
+                Edge &e = adj[parent[u]][parentEdge[u]];
+                e.flow += flow;
+                adj[u][e.rev].flow -= flow;
+                minCost += flow * e.cost;
+            }
+
+            maxFlow += flow;
+        }
+
+        return {maxFlow, minCost};
+    }
+};
+
+void solve() {
+
+    int N,M;
+    cin>>N>>M;
+    MCMF flow(N+M+3);
+    int src= N+M+1; int sink = N+M+2;
+    flow.setSource(src);
+    flow.setSink(sink);
+
+    for(int i=1; i<=N;i++){
+        flow.addEdge(src,i,1,0);
+        int k;
+        cin>>k;
+        while(k--){
+            int v,cost;
+            cin>>v>>cost;
+            flow.addEdge(i,v+N,1,cost);
+        }
+    }
+
+    for(int i=N+1;i<=N+M;i++){
+        flow.addEdge(i,sink,1,0);
+    }
+
+    pair<ll,ll> ret = flow.mcmf();
+
+    cout<<ret.first<<endl<<ret.second;
+
+}
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    while (T--) {
+        solve();
+    }
     return 0;
 }
