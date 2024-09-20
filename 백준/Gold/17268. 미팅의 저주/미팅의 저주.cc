d66d3ca8dd3a34f7db38ff068c297861f4ef05ef
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2024-09-20 16:37:53 +0900

    [Gold III] Title: 미팅의 저주, Time: 96 ms, Memory: 2176 KB -BaekjoonHub

---
+++ 백준/Gold/17268. 미팅의 저주/미팅의 저주.cc
@@ -0,0 +1,82 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+//int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+ll mod = 987654321;
+int DP[5001];
+ll dp(int x) {
+    if (x == 1 || x == 0) return 1;
+    if (DP[x]) return DP[x];
+    ll ret = 0;
+    for (int i = 0; i < x; i++) {
+        ret += dp(i) * dp(x - 1 - i) % mod;
+        ret %= mod;
+    }
+    return DP[x] = ret;
+}
+
+void solve() {
+
+    int N;
+    cin >> N;
+    N /= 2;
+    cout << dp(N);
+
+
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    while (T--) {
+        solve();
+    }
+
+    return 0;
+}
