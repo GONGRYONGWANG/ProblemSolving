543123553b2e79800cd731789b724d40ec5b6fc5
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-01-19 15:41:36 +0900

    [Platinum I] Title: 수열과 쿼리 6, Time: 204 ms, Memory: 6044 KB -BaekjoonHub

--- 백준/Platinum/13548. 수열과 쿼리 6/수열과 쿼리 6.cc
+++ 백준/Platinum/13548. 수열과 쿼리 6/수열과 쿼리 6.cc
@@ -1,122 +1,83 @@
-#pragma warning(disable:4996) 
 #include<iostream>
-#include<iomanip>
-#include<cstdio>
-#include<string>
 #include<vector>
-#include<utility>
-#include<list>
-#include<queue>
-#include<stack>
-#include<deque>
-#include<set>
-#include<unordered_set>
-#include<map>
-#include<unordered_map>
+#include<algorithm>     
 #include<cmath>
-#include<algorithm>
-#include<cstdlib>
-#include<ctime> // srand(time(0))
-#include<regex> // 정규표현식
-#include<random> // rand
-#include<complex> // complex
-#include<numeric>
+
 using namespace std;
-typedef long long ll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-typedef unsigned long long ull;
-typedef unsigned int uint;
-typedef complex<double> cpx;
-#define pq priority_queue
-#define endl "\n"
-#define inf 1e9
-//int dx[4] = { 1,-1,0,0 };
-//int dy[4] = { 0,0,1,-1 };
-//int dx[8] = { 1,1,1,-1,-1,-1,0,0 };
-//int dy[8] = { 1,0,-1,1,0,-1,1,-1 };
-const double pi = 3.14159265358979323846;
+
+int SQRTN;
+int max_freq = 0;
+
+int freq[101010];
+int freq_cnt[101010];
+vector <int> vec;
+int ans[101010];
+
+struct Query{
+    int idx,left,right;
+    bool operator < (const Query &x){
+        if(left/SQRTN!=x.left/SQRTN) return left/SQRTN < x.left/SQRTN;
+        return right<x.right;
+    }
+};
 
 
-int N, M;
-bool cmp(vector<int> a, vector<int> b) {
-    int sqrtN = (int)sqrt(N);
-    if (a[0] / sqrtN == b[0]/ sqrtN) return a[1] < b[1];
-    return a[0] / sqrtN < b[0] / sqrtN;
+void add(int x){
+    if(freq[x]) freq_cnt[freq[x]]--;
+    freq[x]++;
+    freq_cnt[freq[x]]++;
+    max_freq = max(max_freq,freq[x]);
 }
 
-int A[100001];
-int cnt[100000];
-int cntnum[100000];
-int maxcnt;
-int ans[100000];
-int main() {
-    ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
-    cin >> N;
-    for (int i = 0; i < N; i++) {
-        cin >> A[i];
-    }
-    cin >> M;
-    vector<vector<int>> Q;
-    for (int i = 0; i < M; i++) {
-        int a, b;
-        cin >> a >> b;
-        Q.push_back({ a - 1,b - 1, i });
-    }
-    sort(Q.begin(), Q.end(), cmp);
+void remove(int x){
+    freq_cnt[freq[x]]--;
+    if(max_freq == freq[x]&&!freq_cnt[freq[x]]) max_freq--;
+        freq[x]--;
+        freq_cnt[freq[x]]++;
+}
 
-    maxcnt = 0;
-    int s, e , idx;
-    s = Q[0][0];
-    e = Q[0][1];
-    idx = Q[0][2];
-    for (int i = s; i <= e; i++) {
-        cntnum[cnt[A[i]]] -= 1;
-        cnt[A[i]] += 1;
-        cntnum[cnt[A[i]]] += 1;
-        maxcnt = max(maxcnt, cnt[A[i]]);
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    cin>>n;
+
+    SQRTN = sqrt(n);    
+    vec.resize(n+1);
+    for(int i = 1;i<=n;i++){
+        cin>>vec[i];
     }
-    ans[idx] = maxcnt;
-    for (int i = 1; i < M; i++) {
-        int l = Q[i][0];
-        int r = Q[i][1];
-        idx = Q[i][2];
-        while (l < s) {
-            s -= 1;
-            cntnum[cnt[A[s]]] -= 1;
-            cnt[A[s]] += 1;
-            cntnum[cnt[A[s]]] += 1;
-            maxcnt = max(maxcnt, cnt[A[s]]);
-        }
-        while (s < l) {
-            if (maxcnt == cnt[A[s]] && cntnum[cnt[A[s]]] == 1) {
-                maxcnt -= 1;
-            }
-            cntnum[cnt[A[s]]] -= 1;
-            cnt[A[s]] -= 1;
-            cntnum[cnt[A[s]]] += 1;
-            s += 1;
-        }
-        while (e < r) {
-            e += 1;
-            cntnum[cnt[A[e]]] -= 1;
-            cnt[A[e]] += 1;
-            cntnum[cnt[A[e]]] += 1;
-            maxcnt = max(maxcnt, cnt[A[e]]);
-        }
-        while (r < e) {
-            if (maxcnt == cnt[A[e]] && cntnum[cnt[A[e]]] == 1) {
-                maxcnt -= 1;
-            }
-            cntnum[cnt[A[e]]] -= 1;
-            cnt[A[e]] -= 1;
-            cntnum[cnt[A[e]]] += 1;
-            e -= 1;
-        }
-        ans[idx] = maxcnt;
+    int m;
+    cin>>m;
+    vector <Query> q;
+    for(int i = 0;i<m;i++){
+        int a,b;
+        cin>>a>>b;
+        q.push_back({i,a,b});
     }
-    for (int i = 0; i < M; i++) {
-        cout << ans[i] << endl;
+    
+    sort(q.begin(),q.end());
+
+    int l = q[0].left, r = q[0].right;
+    max_freq = 0;
+    for(int i = l;i<=r;i++){
+        if(freq_cnt[freq[vec[i]]]) freq_cnt[freq[vec[i]]]--;
+        freq[vec[i]]++;
+        freq_cnt[freq[vec[i]]]++;
+        max_freq = max(max_freq,freq[vec[i]]);
+    }
+    ans[q[0].idx] = max_freq;
+
+    for(int i = 1;i<m;i++){
+        while(l>q[i].left) add(vec[--l]);
+        while(r<q[i].right) add(vec[++r]);
+        while(l<q[i].left) remove(vec[l++]);
+        while(r>q[i].right) remove(vec[r--]);
+        ans[q[i].idx] = max_freq;
+    }
+
+    for(int i = 0;i<m;i++){
+        cout<<ans[i]<<'\n';
     }
 
     return 0;
