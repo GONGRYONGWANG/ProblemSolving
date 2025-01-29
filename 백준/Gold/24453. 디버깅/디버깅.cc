573461a3ce0ce218321d68325af66a59aff2adb8
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-01-29 18:50:29 +0900

    [Gold III] Title: 디버깅, Time: 120 ms, Memory: 4464 KB -BaekjoonHub

---
+++ 백준/Gold/24453. 디버깅/디버깅.cc
@@ -0,0 +1,99 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+void solve(int tc) {
+    int N, M;
+    cin >> N >> M;
+    
+    vector<bool> arr(N + 1, false);
+    for (int i = 0; i < M; i++) {
+        int x;
+        cin >> x;
+        arr[x] = true;
+    }
+
+    int X, Y;
+    cin >> X >> Y;
+
+
+
+    int cnt = 0;
+    for (int i = 1; i <= X; i++) {
+        cnt += arr[i];
+    }
+
+    int ans = 0;
+
+    ans = max(ans, M - cnt);
+    for (int i = X + 1; i <= N; i++) {
+        cnt -= arr[i - X];
+        cnt += arr[i];
+        ans = max(ans, M - cnt);
+    }
+
+    cout << min(ans, M - Y);
+
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
