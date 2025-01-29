8af6b02d08d96a246eb89dae86fb98017268c152
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-01-29 14:52:56 +0900

    [Gold I] Title: 대결, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1489. 대결/대결.cc
@@ -0,0 +1,103 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+
+void solve(int tc) {
+    int N;
+    cin >> N;
+    vector<int> A(N), B(N);
+    for (int i = 0; i < N; i++) cin >> A[i];
+    for (int i = 0; i < N; i++) cin >> B[i];
+    sort(A.begin(), A.end()); sort(B.begin(), B.end());
+
+    int win = 0; 
+    queue<int> draw;
+    int bidx = 0;
+    for (int i = 0; i < N; i++) {
+        if (A[i] < B[bidx]) {
+            if (!draw.empty()&& draw.front()<A[i]) {
+                draw.pop();
+                win += 1;
+            }
+        }
+        else if (A[i] == B[bidx]) {
+            if (!draw.empty() && draw.front() < A[i]) {
+                draw.pop();
+                win += 1;
+            }
+            else {
+                draw.push(A[i]);
+                bidx += 1;
+            }
+        }
+        else {
+            win += 1;
+            bidx += 1;
+        }
+    }
+
+    cout << draw.size() + 2 * win;
+    
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
