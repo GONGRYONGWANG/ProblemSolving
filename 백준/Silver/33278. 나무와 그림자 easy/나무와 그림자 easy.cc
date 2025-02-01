3bab7a6c978b45b89bf1409f964dd3452fab044f
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-02-02 02:12:59 +0900

    [Silver III] Title: 나무와 그림자 easy, Time: 96 ms, Memory: 6712 KB -BaekjoonHub

---
+++ 백준/Silver/33278. 나무와 그림자 easy/나무와 그림자 easy.cc
@@ -0,0 +1,91 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+
+
+void solve(int tc) {
+
+    ll N, t;
+    cin >> N >> t;
+    vector<pll> arr(N);
+    for (int i = 0; i < N; i++) cin >> arr[i].first >> arr[i].second;
+    sort(arr.begin(), arr.end());
+
+    ll e = -INF;
+
+    ll ans = 0;
+
+    for (int i = 0; i < N; i++) {
+        ll x = arr[i].first; ll h = arr[i].second;
+        ans += max(0ll, min(h, e - t * x));
+        e = max(e, t * x + h);
+    }
+
+    cout << ans;
+
+
+    
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
