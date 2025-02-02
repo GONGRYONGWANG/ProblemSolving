6b283e20d7b37a17aec3e289c36730017f3a05a7
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-02-02 22:46:33 +0900

    [Gold V] Title: Baba is Rabbit, Time: 156 ms, Memory: 27540 KB -BaekjoonHub

---
+++ 백준/Gold/17839. Baba is Rabbit/Baba is Rabbit.cc
@@ -0,0 +1,98 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+    unordered_map<string, vector<string>> E;
+    for (int i = 0; i < N; i++) {
+        string a, b, is;
+        cin >> a >> is >> b;
+        E[a].push_back(b);
+    }
+
+    unordered_map<string, bool> visited;
+    queue<string> q;
+    q.push("Baba");
+    visited["Baba"] = true;
+    vector<string> ret;
+    while (!q.empty()) {
+        string x = q.front();
+        q.pop();
+        for (string& nx : E[x]) {
+            if (visited[nx]) continue;
+            visited[nx] = true;
+            ret.push_back(nx);
+            q.push(nx);
+        }
+    }
+    sort(ret.begin(), ret.end());
+    for (string& s : ret) {
+        cout << s << endl;
+    }
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
