9a5743537f18e79921624197f4b835d1a3c7def3
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-02-02 15:12:43 +0900

    [Gold IV] Title: 특식 배분, Time: 16 ms, Memory: 10100 KB -BaekjoonHub

---
+++ 백준/Gold/33279. 특식 배분/특식 배분.cc
@@ -0,0 +1,96 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+
+int arr[100001];
+
+ld Psum[100001];
+ld dp(int x);
+
+ld psum(int x) {
+    if (x <= 0) return 0;
+    if (Psum[x]) return Psum[x];
+    return Psum[x] = psum(x - 1) + dp(x);
+}
+
+ld DP[100001];
+ld dp(int x) {
+    if (DP[x]) return DP[x];
+    return DP[x] = (psum(x - 1) - psum(x - arr[x] - 1)) / arr[x] + 1;
+}
+
+void solve(int tc) {
+    
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++) cin >> arr[i];
+    cout << fixed;
+    cout.precision(9);
+    cout << dp(N);
+
+
+
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
