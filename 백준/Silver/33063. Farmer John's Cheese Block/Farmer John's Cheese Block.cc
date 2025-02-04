cbd0bf302a1a19b0e927aff17c3a462079460974
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-02-04 13:47:05 +0900

    [Silver V] Title: Farmer John's Cheese Block, Time: 76 ms, Memory: 13740 KB -BaekjoonHub

---
+++ 백준/Silver/33063. Farmer John's Cheese Block/Farmer John's Cheese Block.cc
@@ -0,0 +1,86 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+int XY[1000][1000];
+int YZ[1000][1000];
+int ZX[1000][1000];
+
+void solve(int tc) {
+     
+    int N, Q;
+    cin >> N >> Q;
+    int ret = 0;
+    while (Q--) {
+        int x, y, z;
+        cin >> x >> y >> z;
+        XY[x][y] += 1;
+        if (XY[x][y] == N) ret += 1;
+        YZ[y][z] += 1;
+        if (YZ[y][z] == N) ret += 1;
+        ZX[z][x] += 1;
+        if (ZX[z][x] == N) ret += 1;
+        cout << ret << endl;
+    }
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
