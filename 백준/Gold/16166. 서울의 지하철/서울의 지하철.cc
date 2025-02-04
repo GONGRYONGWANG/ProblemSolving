a502266a87ce3c7349cbe2dce665ee828c704c24
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-02-04 14:53:47 +0900

    [Gold IV] Title: 서울의 지하철, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Gold/16166. 서울의 지하철/서울의 지하철.cc
@@ -0,0 +1,105 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+unordered_map<int, vector<int>> E;
+unordered_map<int, int> dist;
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int K;
+        cin >> K;
+        vector<int> v(K);
+        for (int j = 0; j < K; j++) {
+            cin >> v[j];
+            for (int k = 0; k < j; k++) {
+                E[v[j]].push_back(v[k]);
+                E[v[k]].push_back(v[j]);
+            }
+        }
+    }
+
+    queue<int> q;
+    q.push(0);
+    dist[0] = 1;
+    while (!q.empty()) {
+        int x = q.front();
+        q.pop();
+        for (int nx : E[x]) {
+            if (dist[nx]) continue;
+            dist[nx] = dist[x] + 1;
+            q.push(nx);
+        }
+    }
+
+    int e;
+    cin >> e;
+    if (e == 0) cout << 0;
+    else if (dist[e] == 0) cout << -1;
+    else cout << dist[e] - 2;
+    
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
