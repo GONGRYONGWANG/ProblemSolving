9c5ee712f4705ebbad2759e400eb702b82bb03f8
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-02-04 15:07:26 +0900

    [Gold II] Title: 재활용 캠페인, Time: 52 ms, Memory: 7792 KB -BaekjoonHub

---
+++ 백준/Gold/22988. 재활용 캠페인/재활용 캠페인.cc
@@ -0,0 +1,97 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+    ll X;
+    cin >> X;
+    int ans = 0;
+    vector<ll> arr;
+    for (int i = 0; i < N; i++) {
+        ll x;
+        cin >> x;
+        if (x == X) ans += 1;
+        else arr.push_back(x);
+    }
+    sort(arr.begin(), arr.end());
+    multiset<ll> st;
+    for (ll x : arr) {
+        auto it = st.lower_bound(X - X / 2 - x);
+        if (it != st.end()) {
+            st.erase(it);
+            ans += 1;
+        }
+        else st.insert(x);
+    }
+
+    cout << ans + st.size() / 3;
+
+
+
+
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
