9a3c59c44ecc3dd442e638a20422d6d0bac5108c
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-02-06 03:01:12 +0900

    [Gold IV] Title: 거짓말, Time: 96 ms, Memory: 3976 KB -BaekjoonHub

---
+++ 백준/Gold/31091. 거짓말/거짓말.cc
@@ -0,0 +1,96 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+    vector<int> cnt(N + 1, 0);
+    for (int i = 0; i < N; i++) {
+        int x;
+        cin >> x;
+        if (x > 0) {
+            cnt[0] += 1;
+            cnt[x] -= 1;
+        }
+        else {
+            cnt[-x + 1] += 1;
+        }
+    }
+
+    for (int i = 1; i <= N; i++) cnt[i] += cnt[i - 1];
+
+    int ret = 0;
+    for (int i = 0; i <= N; i++) {
+        if (cnt[i] == i) ret += 1;
+    }
+
+    cout << ret << endl;
+    for (int i = 0; i <= N; i++) {
+        if (cnt[i] == i) cout << i << " ";
+    }
+
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
