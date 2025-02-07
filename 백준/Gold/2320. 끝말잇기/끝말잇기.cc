595b03e83404166a1a4f97b04cb9b97b61c99453
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-02-07 14:12:35 +0900

    [Gold I] Title: 끝말잇기, Time: 8 ms, Memory: 8680 KB -BaekjoonHub

---
+++ 백준/Gold/2320. 끝말잇기/끝말잇기.cc
@@ -0,0 +1,86 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+int DP[(1 << 16)][26];
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+    vector<string> arr(N);
+    for (string& s : arr) cin >> s;
+    int ans = 0;
+    for (int bit = 0; bit < (1 << N); bit++) {
+        for (int i = 0; i < N; i++) {
+            if (bit & (1 << i)) {
+                DP[bit][arr[i].front() - 'A'] = max(DP[bit][arr[i].front() - 'A'], DP[bit ^ (1 << i)][arr[i].back() - 'A'] + int(arr[i].length()));
+                ans = max(ans, DP[bit][arr[i].front() - 'A']);
+            }
+        }
+    }
+
+    cout << ans;
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
