5cab0ff9c0ad17caa44eb2d9f95e4f0b4252c3ec
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-02-10 17:51:28 +0900

    [Gold III] Title: Višekratnici, Time: 404 ms, Memory: 5940 KB -BaekjoonHub

---
+++ 백준/Gold/14136. Višekratnici/README.md
@@ -0,0 +1,30 @@
+# [Gold III] Višekratnici - 14136 
+
+[문제 링크](https://www.acmicpc.net/problem/14136) 
+
+### 성능 요약
+
+메모리: 5940 KB, 시간: 404 ms
+
+### 분류
+
+다이나믹 프로그래밍, 수학, 정수론
+
+### 제출 일자
+
+2025년 2월 10일 17:51:11
+
+### 문제 설명
+
+<p>Zadan je niz prirodnih brojeva. Pronañite duljinu najduljeg rastućeg podniza u kojem je svaki broj višekratnik prethodnog broja. </p>
+
+### 입력 
+
+ <p>U prvom redu nalazi se cijeli broj N (1 ≤ N ≤ 100000), duljina niza.</p>
+
+<p>U svakom od sljedećih N redova nalazi se po jedan prirodni broj manji od 1000000, redom brojevi promatranog niza.</p>
+
+### 출력 
+
+ <p>U prvi i jedini red ispišite duljinu najduljeg rastućeg podniza u kojem je svaki broj višekratnik prethodnog broja. </p>
+
---
+++ 백준/Gold/14136. Višekratnici/Višekratnici.cc
@@ -0,0 +1,101 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+int DP[1000001];
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+
+    int ans = 0;
+
+    for (int i = 0; i < N; i++) {
+        int x;
+        cin >> x;
+
+        int ret = 1;
+        for (int j = 1; j <= sqrt(x); j++) {
+            if (x % j) continue;
+            if (j != x) ret = max(ret, DP[j] + 1);
+            if (x / j != x) ret = max(ret, DP[x / j] + 1);
+        }
+        
+        DP[x] = ret;
+        ans = max(ans, ret);
+    }
+
+    cout << ans;
+
+
+
+
+    
+
+
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
