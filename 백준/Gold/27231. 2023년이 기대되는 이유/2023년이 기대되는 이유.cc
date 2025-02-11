ab8166330174e807c420a894c01d28fe9dd99da0
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-02-12 01:07:23 +0900

    [Gold V] Title: 2023년이 기대되는 이유, Time: 24 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Gold/27231. 2023년이 기대되는 이유/2023년이 기대되는 이유.cc
@@ -0,0 +1,123 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+void solve(int tc) {
+
+    string s;
+    cin >> s;
+    bool b = false;
+    for (char x : s) {
+        if (x != '1' && x != '0') b = true;
+    }
+
+    if (!b) {
+        cout << "Hello, BOJ 2023!" << endl;
+        return;
+    }
+
+    unordered_set<int> st;
+    int N = s.length();
+    for (int bit = 0; bit < (1 << (N - 1)); bit++) {
+        int total = 0;
+        int ret = 0;
+        for (int i = 0; i < N - 1; i++) {
+            ret *= 10;
+            ret += s[i] - '0';
+            if (bit & (1 << i)) {
+                total += ret;
+                ret = 0;
+            }
+        }
+        ret *= 10;
+        ret += s.back() - '0';
+        total += ret;
+
+        st.insert(total);
+    }
+
+    vector<ll> v(N);
+    for (int i = 0; i < N; i++) {
+        v[i] = s[i] - '0';
+    }
+
+    int ans = 0;
+    while (1) {
+        ll ret = 0;
+        for (int i = 0; i < N; i++) {
+            ret += v[i];
+            v[i] *= s[i] - '0';
+        }
+        if (ret > stoi(s)) break;
+        if (st.count(ret)) ans += 1;
+    }
+        
+    cout << ans << endl;
+
+
+
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
