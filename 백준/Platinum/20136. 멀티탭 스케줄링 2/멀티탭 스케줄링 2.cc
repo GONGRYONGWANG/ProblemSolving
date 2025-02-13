07a6f122e2da425530bce33ec40dfefc32bb7d6d
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-02-13 13:39:21 +0900

    [Platinum IV] Title: 멀티탭 스케줄링 2, Time: 176 ms, Memory: 19484 KB -BaekjoonHub

---
+++ 백준/Platinum/20136. 멀티탭 스케줄링 2/멀티탭 스케줄링 2.cc
@@ -0,0 +1,94 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+void solve(int tc) {
+
+    int N, K;
+    cin >> N >> K;
+
+    vector<int> idx(K + 1, 0);
+    vector<int> nx(K + 1, inf);
+    vector<int> arr(K + 1);
+    for (int i = 1; i <= K; i++) {
+        cin >> arr[i];
+        if (idx[arr[i]]) nx[idx[arr[i]]] = i;
+        idx[arr[i]] = i;
+    }
+
+    int ans = 0;
+    multiset<int> st;
+    for (int i = 1; i <= K; i++) {
+        if (st.size() == N && *st.begin() != i) {
+            st.erase(prev(st.end()));
+            ans += 1;
+        }
+        if (!st.empty() && *st.begin() == i) st.erase(st.begin());
+        st.insert(nx[i]);
+    }
+
+    cout << ans;
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
