ed4ea9de85ce4bbcdb1b42d3071aa9cd1907080a
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-02-19 22:52:11 +0900

    [Gold I] Title: 수집합, Time: 308 ms, Memory: 44640 KB -BaekjoonHub

---
+++ 백준/Gold/4373. 수집합/수집합.cc
@@ -0,0 +1,106 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+void solve(int tc) {
+
+    while (1) {
+        int N;
+        cin >> N;
+        if (N == 0) break;
+        vector<ll> arr(N);
+        for (int i = 0; i < N; i++) cin >> arr[i];
+        sort(arr.begin(), arr.end());
+
+        unordered_map<ll, set<ll>> m;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                m[arr[j] - arr[i]].insert(arr[j]);
+            }
+        }
+
+        ll ans = -INF;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                ll x = arr[i] + arr[j];
+                if (!m.count(x)) continue;
+                auto it = prev(m[x].end());
+                if (*it != arr[i] && *it != arr[j] && *it - x != arr[i] && *it != x != arr[j]) {
+                    ans = max(ans, *it);
+                    continue;
+                }
+                if (it == m[x].begin()) continue;
+                it--;
+                if (*it != arr[i] && *it != arr[j] && *it - x != arr[i] && *it != x != arr[j]) {
+                    ans = max(ans, *it);
+                }
+            }
+        }
+
+        if (ans == -INF) cout << "no solution" << endl;
+        else cout << ans << endl;
+    }
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
