c1c3784485b8c44a50cb033e0c1db2f14c426500
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-02-25 12:42:08 +0900

    [Gold III] Title: 백양로 브레이크, Time: 20 ms, Memory: 2268 KB -BaekjoonHub

---
+++ 백준/Gold/11562. 백양로 브레이크/백양로 브레이크.cc
@@ -0,0 +1,110 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+int adj[251][251];
+
+void solve(int tc) {
+
+    int N, M;
+    cin >> N >> M;
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            adj[i][j] = inf;
+        }
+        adj[i][i] = 0;
+    }
+
+    for (int i = 0; i < M; i++) {
+        int u, v, b;
+        cin >> u >> v >> b;
+        if (b) adj[u][v] = adj[v][u] = 0;
+        else {
+            adj[u][v] = 0;
+            adj[v][u] = 1;
+        }
+    }
+
+    for (int k = 1; k <= N; k++) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]);
+            }
+        }
+    }
+
+
+    int K;
+    cin >> K;
+    while (K--) {
+        int s, e;
+        cin >> s >> e;
+        cout << adj[s][e] << endl;
+    }
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
