1a51c8bfac79d96d0eade3ec9e1ee22d640ca0fb
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-02-26 15:04:32 +0900

    [Platinum IV] Title: Byteland, Time: 132 ms, Memory: 10316 KB -BaekjoonHub

---
+++ 백준/Platinum/8632. Byteland/Byteland.cc
@@ -0,0 +1,113 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+int parent[7001];
+int findp(int x) {
+    if (parent[x] == x) return x;
+    return parent[x] = findp(parent[x]);
+}
+
+void solve(int tc) {
+
+    int N, M;
+    cin >> N >> M;
+    
+    for (int i = 1; i <= N; i++) parent[i] = i;
+
+    vector<pii> E(M);
+    vector<vector<int>> Es(100001);
+
+    for (int i = 0; i < M; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        E[i] = { u,v };
+        Es[w].push_back(i);
+    }
+
+    vector<bool> ans(M, false);
+
+    for (int i = 1; i <= 100000; i++) {
+        for (int x : Es[i]) {
+            auto [u, v] = E[x];
+            if (findp(u) != findp(v)) ans[x] = true;
+        }
+        for (int x : Es[i]) {
+            auto [u, v] = E[x];
+            parent[findp(u)] = findp(v);
+        }
+    }
+
+    for (int i = 0; i < M; i++) {
+        if (ans[i]) cout << "TAK";
+        else cout << "NIE";
+        cout << endl;
+    }
+
+
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
