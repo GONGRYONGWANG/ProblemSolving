f9460dde9289d93eef44c7697ef114ddaa28646c
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-03-05 13:51:11 +0900

    [Gold II] Title: 핸들 뭘로 하지, Time: 324 ms, Memory: 33576 KB -BaekjoonHub

---
+++ 백준/Gold/25498. 핸들 뭘로 하지/핸들 뭘로 하지.cc
@@ -0,0 +1,104 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+char arr[500001];
+vector<int> E[500001];
+bool visited[500001];
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < N - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        E[u].push_back(v);
+        E[v].push_back(u);
+    }
+    queue<int> q;
+    q.push(1);
+    while (!q.empty()) {
+        queue<int> nxq;
+        cout << arr[q.front()];
+        while (!q.empty()) {
+            int x = q.front();
+            q.pop();
+            visited[x] = true;
+            for (int nx : E[x]) {
+                if (visited[nx]) continue;
+                if (nxq.empty() || arr[nx] > arr[nxq.front()]) nxq = {};
+                if (nxq.empty() || arr[nx] == arr[nxq.front()]) nxq.push(nx);
+            }
+        }
+        q = nxq;
+    }
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
