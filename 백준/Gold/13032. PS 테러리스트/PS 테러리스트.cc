177e2b02a356e1cf71ce9aab3dc01d8e9177329e
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-03-06 02:31:29 +0900

    [Gold IV] Title: PS 테러리스트, Time: 36 ms, Memory: 4096 KB -BaekjoonHub

---
+++ 백준/Gold/13032. PS 테러리스트/PS 테러리스트.cc
@@ -0,0 +1,92 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+int DP[100000];
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+
+    vector<pii> arr(N);
+    for (int i = 0; i < N; i++) cin >> arr[i].first >> arr[i].second;
+    sort(arr.begin(), arr.end());
+
+    int ans = 0;
+    vector<int> v;
+    for (int i = 0; i < N; i++) {
+        auto [x, p] = arr[i];
+        int idx = lower_bound(v.begin(), v.end(), x - p) - v.begin();
+        if (idx == 0) DP[i] = 1;
+        else DP[i] = DP[idx - 1] + 1;
+        ans = max(ans, DP[i]);
+        v.push_back(x);
+    }
+
+    cout << N - ans;
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
