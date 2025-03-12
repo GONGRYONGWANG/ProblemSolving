0805857455794db8df739eac62c01a55e023e0e5
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-03-12 20:59:12 +0900

    [Gold IV] Title: 공격, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1430. 공격/공격.cc
@@ -0,0 +1,104 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+int dist[51];
+
+void solve(int tc) {
+
+    int N, R, D, X, Y;
+    cin >> N >> R >> D >> X >> Y;
+
+    vector<pii> arr(N + 1);
+    arr[0] = { X,Y };
+    for (int i = 1; i <= N; i++)  cin >> arr[i].first >> arr[i].second;
+
+    ld ans = 0;
+
+    queue<int> q;
+    q.push(0);
+    dist[0] = 1;
+    while (!q.empty()) {
+        int x = q.front();
+        q.pop();
+        for (int i = 1; i <= N; i++) {
+            if (dist[i]) continue;
+            auto [x1, y1] = arr[x];
+            auto [x2, y2] = arr[i];
+            if ((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) > R * R) continue;
+            dist[i] = dist[x] + 1;
+            q.push(i);
+            ans += ld(D) / (1ll << (dist[x] - 1));
+        }
+    }
+
+    cout << fixed;
+    cout.precision(9);
+    cout << ans;
+
+}
+
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
