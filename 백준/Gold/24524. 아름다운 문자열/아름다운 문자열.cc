00132516ec78182dc3575c0c13b867cddf2fdfd3
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-03-13 16:47:25 +0900

    [Gold V] Title: 아름다운 문자열, Time: 32 ms, Memory: 3684 KB -BaekjoonHub

---
+++ 백준/Gold/24524. 아름다운 문자열/아름다운 문자열.cc
@@ -0,0 +1,94 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+
+void solve(int tc) {
+    string S, T;
+    cin >> S >> T;
+
+    unordered_map<char, int> m;
+    for (int i = 0; i < T.length(); i++) m[T[i]] = i;
+
+    vector<int> DP(T.length(), 0);
+    for (char c : S) {
+        if (!m.count(c)) continue;
+        int x = m[c];
+        if (x == 0) DP[0] += 1;
+        else if (DP[x - 1] != 0) {
+            DP[x - 1] -= 1;
+            DP[x] += 1;
+        }
+    }
+
+    cout << DP[T.length() - 1];
+
+
+
+}
+
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
