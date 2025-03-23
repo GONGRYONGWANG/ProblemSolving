b9b96003a9ae87c45e8a88b4cc3211210545834e
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-03-23 22:43:18 +0900

    [Silver I] Title: 3교시: 수학, Time: 184 ms, Memory: 17868 KB -BaekjoonHub

---
+++ 백준/Silver/33633. 3교시： 수학/3교시： 수학.cc
@@ -0,0 +1,105 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+
+void solve(int tc) {
+    int N;
+    cin >> N;
+    vector<ll> v;
+    v.push_back(1);
+    N -= 1;
+    while (N--) {
+        vector<ll> nv;
+        unordered_map<ll, bool> visited;
+        for (ll x : v) {
+            if (!visited[x * 2]) {
+                visited[x * 2] = true;
+                nv.push_back(x * 2);
+            }
+            if ((x - 1) % 3 == 0 && (x - 1) / 3 != 1 && (x - 1) / 3 % 2 && !visited[(x - 1) / 3]) {
+                visited[(x - 1) / 3] = true;
+                nv.push_back((x - 1) / 3);
+            }
+        }
+        v = nv;
+    }
+
+    sort(v.begin(), v.end());
+    
+    cout << v.size() << endl;
+    for (ll x : v) cout << x << endl;
+
+
+
+
+
+
+
+}
+
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
