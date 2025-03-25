cbff7f75223c1b356f70116467bc25793f78442c
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-03-25 17:39:51 +0900

    [Gold V] Title: 생일 멘션이 너무 많아, Time: 192 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Gold/33666. 생일 멘션이 너무 많아/생일 멘션이 너무 많아.cc
@@ -0,0 +1,114 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+void solve(int tc) {
+
+    ll N, M;
+    cin >> N >> M;
+
+    ll total = 0;
+    ll cnt = 0;
+
+    vector<ll> arr(N);
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+        if (arr[i] >= 2) {
+            total += arr[i];
+            cnt += 1;
+        }
+    }
+
+    vector<ll> ret(M + 2, 0);
+
+    for (int i = 0; i < N; i++) {
+        ll x = arr[i];
+        if (x * cnt > total) {
+            ret[1] += 1;
+            ret[2] -= 1;
+            continue;
+        }
+        if (x > M) {
+            cout << -1;
+            return;
+        }
+        ret[1] += 1;
+        ret[x + 1] -= 1;
+    }
+
+    for (int i = 1; i <= M; i++) {
+        ret[i] += ret[i - 1];
+        cout << ret[i] << " ";
+    }
+
+
+
+
+
+}
+
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
