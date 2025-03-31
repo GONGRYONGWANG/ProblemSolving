8dc3675e5db391f8571ffc68f12405f201c146f3
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-03-31 18:23:12 +0900

    [Gold III] Title: 가디언 엔젤 슬라임, Time: 980 ms, Memory: 64592 KB -BaekjoonHub

---
+++ 백준/Gold/33694. 가디언 엔젤 슬라임/가디언 엔젤 슬라임.cc
@@ -0,0 +1,113 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+
+void solve(int tc) {
+
+    ll N, X;
+    cin >> N >> X;
+
+    map<ll, int> m;
+
+    for (int i = 0; i < N; i++) {
+        ll a, b, c, t;
+        cin >> a >> b >> c >> t;
+
+        ll apex = (c + a - 1) / a + t;
+        if ((apex - t) * a < X) continue;
+
+
+        ll l = (X + a - 1) / a + t;
+        ll r = ((apex - t) * a - X + b) / b + apex;
+
+        m[l] += 1;
+        m[r] -= 1;
+
+    }
+
+    int psum = 0;
+
+    ll ans = 0;
+
+    for (auto it = m.begin(); it != m.end(); it++) {
+        psum += it->second;
+        if (psum >= 3) {
+            ans += next(it)->first - it->first;
+        }
+    }
+
+    cout << ans;
+
+
+
+
+
+
+}
+
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
