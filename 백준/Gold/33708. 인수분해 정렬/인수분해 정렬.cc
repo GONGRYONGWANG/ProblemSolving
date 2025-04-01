07e48db106bc7482c84add0aa39f7342d9a48c4d
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-04-01 16:43:40 +0900

    [Gold II] Title: 인수분해 정렬, Time: 24 ms, Memory: 3784 KB -BaekjoonHub

---
+++ 백준/Gold/33708. 인수분해 정렬/인수분해 정렬.cc
@@ -0,0 +1,106 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+const ll INF = 1e18 + 7;
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+bool isnprime[1000001];
+vector<int> prime;
+
+void solve(int tc) {
+    for (int i = 2; i <= 1000; i++) {
+        if (isnprime[i]) continue;
+        prime.push_back(i);
+        for (int j = i * i; j <= 1000000; j += i) {
+            isnprime[j] = true;
+        }
+    }
+
+    int N;
+    cin >> N;
+    vector<int> arr(N);
+    for (int i = 0; i < N; i++) cin >> arr[i];
+
+    bool flag = true;
+    for (int i = 0; i < N - 1; i++) {
+        flag &= (arr[i] <= arr[i + 1]);
+    }
+
+    for (int i = 0; i < N - 1; i++) {
+        flag |= isnprime[arr[i]];
+        flag |= (arr[i] != 1 && arr[i + 1] != 1);
+    }
+    flag |= isnprime[arr[N - 1]];
+
+    cout << (flag ? "YES" : "NO");
+
+
+
+
+
+
+
+}
+
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
