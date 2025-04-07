a4bd43dfa6a2c625dfc444bd811807d55edbe446
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-04-08 03:27:43 +0900

    [Platinum IV] Title: Snow Boots, Time: 144 ms, Memory: 12184 KB -BaekjoonHub

---
+++ 백준/Platinum/15745. Snow Boots/Snow Boots.cc
@@ -0,0 +1,115 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(1e18+7)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+void solve(int tc) {
+
+    int N, B;
+    cin >> N >> B;
+
+    vector<pii> f(N);
+    for (int i = 0; i < N; i++) {
+        cin >> f[i].first; f[i].second = i+1;
+    }
+
+    sort(f.begin(), f.end());
+
+    map<int, int> m; m[N - 1] = 0;
+
+    set<int> st;
+    st.insert(1); st.insert(N);
+    multiset<int> line; line.insert(N - 1);
+
+    for (int i = 0; i < N; i++) {
+        auto [h, x] = f[i];
+        if (x == 1 || x == N) continue;
+
+        int l = *prev(st.upper_bound(x));
+        int r = *st.upper_bound(x);
+        st.insert(x);
+
+        line.erase(line.find(r - l));
+        line.insert(x - l); line.insert(r - x);
+
+        if (!m.count(*line.rbegin())) m[*line.rbegin()] = h;
+
+    }
+
+
+    while (B--) {
+        int s, d;
+        cin >> s >> d;
+        if (s >= prev(m.upper_bound(d))->second) cout << 1;
+        else cout << 0;
+        cout << endl;
+    }
+
+
+
+}
+
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
