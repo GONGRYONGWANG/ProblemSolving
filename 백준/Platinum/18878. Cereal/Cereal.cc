980158f45114450e64a7f1db357f92c1e38d13c2
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-04-09 17:04:34 +0900

    [Platinum V] Title: Cereal, Time: 28 ms, Memory: 3584 KB -BaekjoonHub

---
+++ 백준/Platinum/18878. Cereal/Cereal.cc
@@ -0,0 +1,107 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(1e18+7)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+pii arr[100001];
+int alot[100001];
+bool assign(int n, int m) {
+    int nx = alot[m];
+    if (nx != 0 && nx < n) return false;
+    alot[m] = n;
+    if (nx == 0) return true;
+    if (m == arr[nx].second) return false;
+    return assign(nx, arr[nx].second);
+}
+
+void solve(int tc) {
+
+    int N, M;
+    cin >> N >> M;
+
+    for (int i = 1; i <= N; i++) {
+        cin >> arr[i].first >> arr[i].second;
+    }
+
+    vector<int> ans(N + 1);
+
+    int ret = 0;
+    for (int i = N; i >= 1; i--) {
+        if (assign(i, arr[i].first)) ret += 1;
+        ans[i] = ret;
+    }
+
+    for (int i = 1; i <= N; i++) cout << ans[i] << endl;
+
+
+
+
+
+
+
+}
+
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
