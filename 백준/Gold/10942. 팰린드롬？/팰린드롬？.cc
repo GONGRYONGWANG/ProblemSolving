83aa0e996ac7824ea3e8e88847028df4996ed413
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-04-14 16:59:01 +0900

    [Gold IV] Title: 팰린드롬?, Time: 208 ms, Memory: 5936 KB -BaekjoonHub

---
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -0,0 +1,98 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(9e18)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+int arr[2000];
+bool DP[2000][2000];
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++) cin >> arr[i];
+    for (int l = 1; l <= N; l++) {
+        for (int i = 0; i + l - 1 < N; i++) {
+            if (l == 1) DP[i][i + l - 1] = true;
+            else if (arr[i] == arr[i + l - 1] && (l == 2 || DP[i + 1][i + l - 2])) DP[i][i + l - 1] = true;
+        }
+    }
+
+    int M;
+    cin >> M;
+    while (M--) {
+        int s, e;
+        cin >> s >> e;
+        s -= 1; e -= 1;
+        cout << DP[s][e] << endl;
+    }
+
+
+
+
+
+}
+    
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
