297b2050152d60b73e0eb000e1550d96b463bd45
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-04-29 02:09:41 +0900

    [Platinum IV] Title: SSB Tournament, Time: 216 ms, Memory: 9836 KB -BaekjoonHub

---
+++ 백준/Platinum/20723. SSB Tournament/SSB Tournament.cc
@@ -0,0 +1,96 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(4e18)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+ll deg[1000001];
+
+void solve(int tc) {
+
+    ll N, K;
+    cin >> N >> K;
+
+    while (K--) {
+        int u, v;
+        cin >> u >> v;
+        deg[u] += 1; deg[v] += 1;
+    }
+
+    ll ret = 0;
+
+    for (int i = 1; i <= N; i++) {
+        ret += deg[i] * (N - 1 - deg[i]);
+    }
+
+    ret /= 2;
+
+    cout << N * (N - 1) * (N - 2) / 6 - ret;
+
+
+
+}
+    
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
