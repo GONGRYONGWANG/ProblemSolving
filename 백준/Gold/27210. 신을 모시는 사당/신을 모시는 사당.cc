4655c483b774880b8b16ccc2058061f096944150
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-05-01 15:45:27 +0900

    [Gold V] Title: 신을 모시는 사당, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/27210. 신을 모시는 사당/신을 모시는 사당.cc
@@ -0,0 +1,98 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(4e18)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+void solve(int tc) {
+    
+    int N;
+    cin >> N;
+
+    int mx = 0;
+    int mn = 0;
+
+    int ans = 0;
+
+    int psum = 0;
+    for (int i = 0; i < N; i++) {
+        int x;
+        cin >> x;
+        if (x == 1) psum += 1;
+        else psum -= 1;
+        mx = max(mx, psum);
+        mn = min(mn, psum);
+        ans = max(ans, max(psum - mn, mx - psum));
+    }
+    cout << ans;
+
+
+
+
+
+
+}
+    
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
