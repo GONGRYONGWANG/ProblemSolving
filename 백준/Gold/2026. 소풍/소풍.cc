7eb120ef587001fb4225f7961b1329f2d59718b4
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-05-08 19:48:34 +0900

    [Gold II] Title: 소풍, Time: 84 ms, Memory: 2816 KB -BaekjoonHub

---
+++ 백준/Gold/2026. 소풍/소풍.cc
@@ -0,0 +1,112 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(4e18)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+int N, K;
+
+bool adj[901][901];
+
+
+vector<int> v;
+bool bt(int x) {
+    if (v.size() == K) {
+        for (int u : v) {
+            cout << u << endl;
+        }
+        return true;
+    }
+    if (x == N + 1) return false;
+    bool flag = true;
+    for (int u : v) {
+        if (!adj[u][x]) flag = false;
+    }
+    if (flag) {
+        v.push_back(x);
+        if (bt(x + 1)) return true;
+        v.pop_back();
+    }
+    if (bt(x + 1)) return true;
+    return false;
+}
+
+
+void solve(int tc) {
+
+    int F;
+    cin >> K >> N >> F;
+
+    while (F--) {
+        int u, v;
+        cin >> u >> v;
+        adj[u][v] = adj[v][u] = true;
+    }
+
+    if (!bt(1)) cout << -1;
+
+
+}
+    
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
