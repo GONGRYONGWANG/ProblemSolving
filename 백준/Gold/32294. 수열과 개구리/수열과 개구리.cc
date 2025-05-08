143060ed4d2c1a3d0ca4098b49989e86678bba86
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-05-08 14:37:29 +0900

    [Gold II] Title: 수열과 개구리, Time: 212 ms, Memory: 29352 KB -BaekjoonHub

---
+++ 백준/Gold/32294. 수열과 개구리/수열과 개구리.cc
@@ -0,0 +1,121 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(4e18)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+ll A[200001];
+ll B[200001];
+
+vector<pll> E[200001];
+
+struct cmp {
+    bool operator()(pll& a, pll& b) {
+        return a.second > b.second;
+    }
+};
+
+bool visited[200001];
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+
+    for (int i = 1; i <= N; i++) cin >> A[i];
+    for (int i = 1; i <= N; i++) cin >> B[i];
+
+    for (int i = 1; i <= N; i++) {
+        E[max(0ll, i - A[i])].push_back({ i,B[i] });
+        if (i + A[i] > N) E[0].push_back({ i,B[i] });
+        else E[A[i] + i].push_back({ i,B[i] });
+    }
+
+    pq<pll, vector<pll>, cmp> q;
+    q.push({ 0,0 });
+
+    vector<ll> ret(N + 1);
+    while (!q.empty()) {
+        auto [x, d] = q.top();
+        q.pop();
+        if (visited[x]) continue;
+        visited[x] = true;
+        ret[x] = d;
+        for (pll& e : E[x]) {
+            if (visited[e.first]) continue;
+            q.push({ e.first,e.second + d });
+        }
+    }
+    
+    for (int i = 1; i <= N; i++) {
+        cout << ret[i] << " ";
+    }
+
+
+
+
+
+}
+    
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
