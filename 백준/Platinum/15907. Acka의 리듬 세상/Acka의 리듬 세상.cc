d975a82aeaad48ffdd87964b3436d1430da5626a
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-05-09 16:35:45 +0900

    [Platinum V] Title: Acka의 리듬 세상, Time: 60 ms, Memory: 5648 KB -BaekjoonHub

---
+++ 백준/Platinum/15907. Acka의 리듬 세상/Acka의 리듬 세상.cc
@@ -0,0 +1,99 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(4e18)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+bool isnprime[2000001];
+vector<int> prime;
+
+void solve(int tc) {
+
+    for (ll i = 2; i <= 2000000; i++) {
+        if (isnprime[i]) continue;
+        prime.push_back(i);
+        for (ll j = i * i; j <= 2000000; j += i) isnprime[j] = true;
+    }
+
+
+    int N;
+    cin >> N;
+    vector<int> arr(N);
+    for (int i = 0; i < N; i++) cin >> arr[i];
+
+    int ans = 0;
+    for (int p : prime) {
+        if ((2000000 - 1) / p + 1 <= (N + 1) / 2) break;
+        unordered_map<int, int> cnt;
+        for (int x : arr) {
+            cnt[x % p] += 1;
+            ans = max(ans, cnt[x % p]);
+        }
+    }
+
+    cout << ans;
+
+}
+    
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
