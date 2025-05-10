d26940a3285f31a383b491b5c0bfa9432d050e89
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-05-10 21:54:39 +0900

    [Silver I] Title: 점프왕 쩰리 (Large), Time: 0 ms, Memory: 2040 KB -BaekjoonHub

---
+++ 백준/Silver/16174. 점프왕 쩰리 （Large）/점프왕 쩰리 （Large）.cc
@@ -0,0 +1,112 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(4e18)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+int board[64][64];
+bool visited[64][64];
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> board[i][j];
+        }
+    }
+
+    queue<pii> q;
+    q.push({ 0,0 });
+    visited[0][0] = true;
+    while (!q.empty()) {
+        auto [x, y] = q.front();
+        q.pop();
+        int k = board[x][y];
+        if (k == -1) {
+            cout << "HaruHaru";
+            return;
+        }
+        if (x + k < N && !visited[x + k][y]) {
+            q.push({ x + k,y });
+            visited[x + k][y] = true;
+        }
+        if (y + k < N && !visited[x][y + k]) {
+            q.push({ x,y + k });
+            visited[x][y + k] = true;
+        }
+    }
+
+    cout << "Hing";
+
+
+
+
+
+
+}
+    
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
