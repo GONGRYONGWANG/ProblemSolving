9a49411f5b7f6c0532bd8d805b28abf48bfa3e57
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-05-11 16:24:54 +0900

    [Platinum V] Title: 수강신청, Time: 20 ms, Memory: 2920 KB -BaekjoonHub

---
+++ 백준/Platinum/31721. 수강신청/수강신청.cc
@@ -0,0 +1,99 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(4e18)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+void solve(int tc) {
+
+    ll N, M;
+    cin >> N >> M;
+    int ans = 0;
+    vector<ll> arr;
+    for (int i = 0; i < N; i++) {
+        int a, b;
+        cin >> a >> b;
+        if (a >= b) ans += 1;
+        else arr.push_back(a);
+    }
+
+    sort(arr.begin(), arr.end());
+
+    ll total = 0;
+
+    for (ll x : arr) {
+        if (x > total) {
+            ans += 1;
+            total += M - 1;
+        }
+        else total -= x;
+    }
+
+    cout << ans;
+
+
+}
+    
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
