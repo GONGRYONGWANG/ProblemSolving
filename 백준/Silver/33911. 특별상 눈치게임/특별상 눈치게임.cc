14e222be216cbcf6e9d673410701337a28b62184
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-05-21 20:35:28 +0900

    [Silver III] Title: 특별상 눈치게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/33911. 특별상 눈치게임/특별상 눈치게임.cc
@@ -0,0 +1,104 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(4e18)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+    vector<int> cnt(101, 0);
+    while (N--) {
+        for (int i = 0; i < 3; i++) {
+            int x; cin >> x;
+            cnt[x] += 1; 
+        }
+    }
+
+    int ans = 0;
+
+    int prvmx = 0;
+    int overlap = 0;
+    for (int i = 100; i >= 1; i--) {
+        if (cnt[i] == 0) {
+            if (prvmx == 0) ans += (i - 1 + overlap) * (i - 2 + overlap) / 2;
+            else if (prvmx == 1) ans += i - 1 + overlap;
+            else ans += 1;
+        }
+        else if (cnt[i] == 1) prvmx += 1;
+        else overlap += 1;
+
+        if (prvmx == 3) break;
+    }
+    cout << ans;
+
+
+
+
+
+}
+    
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
