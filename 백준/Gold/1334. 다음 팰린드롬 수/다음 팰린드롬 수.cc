c5933547ec912b56a162a74aea61ac610a8e46df
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-05-23 01:47:15 +0900

    [Gold V] Title: 다음 팰린드롬 수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1334. 다음 팰린드롬 수/다음 팰린드롬 수.cc
@@ -0,0 +1,110 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(4e18)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+void solve(int tc) {
+
+    string s;
+    cin >> s;
+
+    int N = s.length();
+    string l = s.substr(0, (N + 1) / 2);
+    string r = s.substr(0, N / 2);
+    reverse(r.begin(),r.end());
+    if (l + r > s) {
+        cout << l << r;
+        return;
+    }
+
+    for (int i = l.length() - 1; i >= 0; i--) {
+        if (l[i] != '9') {
+            l[i] = char(l[i] + 1);
+            break;
+        }
+        else l[i] = '0';
+    }
+
+    if (l.front() == '0') {
+        cout << 1;
+        for (int i = 0; i < N - 1; i++) cout << 0;
+        cout << 1;
+        return;
+    }
+
+    r = l.substr(0, N / 2);
+    reverse(r.begin(), r.end());
+    cout << l << r;
+   
+
+
+
+
+
+
+}
+    
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
