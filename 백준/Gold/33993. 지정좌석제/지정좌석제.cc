9ccba16ca519fe1982b525ee96d9120de85b8b3d
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-05-25 21:47:41 +0900

    [Gold V] Title: 지정좌석제, Time: 192 ms, Memory: 124188 KB -BaekjoonHub

---
+++ 백준/Gold/33993. 지정좌석제/지정좌석제.cc
@@ -0,0 +1,112 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(4e18)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+int cnt[5002][5002];
+bool occupy[5002][5002];
+
+void solve(int tc) {
+
+    int N, R, C, W;
+    cin >> N >> R >> C >> W;
+    W /= 2;
+    
+
+
+    while (N--) {
+        int x, y;
+        cin >> x >> y;
+        for (int i = max(1, x - W); i <= min(R, x + W); i++) {
+            cnt[i][max(1, y - W)] += 1;
+            cnt[i][min(C + 1, y + W + 1)] -= 1;
+        }
+        occupy[x][y] = true;
+    }
+
+    int x = 0, y = 0;
+
+    for (int i = 1; i <= R; i++) {
+        for (int j = 1; j <= C; j++) {
+            cnt[i][j] += cnt[i][j - 1];
+            if (occupy[i][j]) continue;
+            if (cnt[i][j] > cnt[x][y]) x = i, y = j;
+        }
+    }
+
+    cout << cnt[x][y] << endl;
+    cout << x << " " << y;
+
+
+
+
+
+
+
+}
+    
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
