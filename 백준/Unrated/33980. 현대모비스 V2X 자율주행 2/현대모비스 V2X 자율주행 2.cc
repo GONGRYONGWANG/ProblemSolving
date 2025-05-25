56b1937ce60c90ea1ca1426f253a605012868484
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-05-25 15:38:36 +0900

    [Unrated] Title: 현대모비스 V2X 자율주행 2, Time: 4 ms, Memory: 3512 KB -BaekjoonHub

---
+++ 백준/Unrated/33980. 현대모비스 V2X 자율주행 2/현대모비스 V2X 자율주행 2.cc
@@ -0,0 +1,115 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(4e18)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+int N;
+int solve2(string A, string B) {
+
+    int ret = 0;
+    int d = 1;
+    if (A[0] != 'U') {
+        ret += 1;
+        for (int i = 2 * N - 1; i >= 0; i--) {
+            if (A[i] == 'U') {
+                A[i] = 'R';
+                break;
+            }
+        }
+    }
+    if (B[0] != 'R') {
+        ret += 1;
+        for (int i = 2 * N - 1; i >= 0; i--) {
+            if (B[i] == 'R') {
+                B[i] = 'U';
+                break;
+            }
+        }
+    }
+    for (int i = 1; i < 2 * N - 1; i++) {
+        if (A[i] == 'R' && B[i] == 'U') {
+            if (d == 1) ret += 1;
+            else d -= 1;
+        }
+        if (A[i] == 'U' && B[i] == 'R') d += 1;
+    }
+    return ret;
+}
+
+void solve(int tc) {
+    
+    cin >> N;
+    string A, B;
+    cin >> A >> B;
+
+    cout << min(solve2(A, B), solve2(B, A));
+
+
+
+
+
+}
+
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
