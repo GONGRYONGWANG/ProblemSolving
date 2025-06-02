28ea47ed5e6573cd59d97b56a1d786e7eba545b7
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-06-02 23:22:49 +0900

    [Gold IV] Title: 꽁꽁 얼어붙은 트리, Time: 636 ms, Memory: 65536 KB -BaekjoonHub

---
+++ 백준/Gold/34011. 꽁꽁 얼어붙은 트리/꽁꽁 얼어붙은 트리.cc
@@ -0,0 +1,116 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(4e18)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+int cnt[1000001];
+int depth[1000001];
+
+vector<int> E[1000001];
+
+bool isnprime[1000001];
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+
+    for (int i = 2; i <= N; i++) {
+        int p;
+        cin >> p;
+        E[p].push_back(i);
+    }
+
+    queue<int> q;
+    q.push(1);
+    depth[1] = 0;
+    while (!q.empty()) {
+        int x = q.front();
+        q.pop();
+        cnt[depth[x]] += 1;
+        for (int nx : E[x]) {
+            depth[nx] = depth[x] + 1;
+            q.push(nx);
+        }
+    }
+
+    int ans = 0;
+
+    for (int i = 2; i <= N; i++) {
+        int ret = 1;
+        for (int j = i; j <= N; j += i) {
+            ret += cnt[j];
+        }
+        ans = max(ans, ret);
+    }
+
+    cout << ans;
+
+
+}
+    
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
