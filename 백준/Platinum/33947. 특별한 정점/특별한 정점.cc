45391cfbc13738b031358a2d9a162d603980c5c4
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-06-04 04:52:04 +0900

    [Platinum IV] Title: 특별한 정점, Time: 108 ms, Memory: 22912 KB -BaekjoonHub

---
+++ 백준/Platinum/33947. 특별한 정점/특별한 정점.cc
@@ -0,0 +1,114 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(4e18)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+vector<int> E[200001];
+int special[200001];
+int parent[200001];
+
+int ans = 0;
+
+int dfs(int x) {
+    int cnt = 0;
+    for (int nx : E[x]) {
+        if (nx == parent[x]) continue;
+        parent[nx] = x;
+        cnt += dfs(nx);
+    }
+    if (cnt >= 2) {
+        ans += cnt - 1;
+        return 0;
+    }
+    if (cnt == 0 && !special[x]) return 0;
+    return 1;
+}
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+    
+    for (int i = 0; i < N - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        E[u].push_back(v);
+        E[v].push_back(u);
+    }
+
+    for (int i = 1; i <= N; i++)  cin >> special[i];
+
+    if (dfs(1)) ans += 1;
+
+    cout << max(0, ans - 1);
+
+
+
+
+}
+    
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
