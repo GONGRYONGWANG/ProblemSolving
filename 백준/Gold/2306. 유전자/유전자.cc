eead3f609a589adc385df1c7c5f03752d506d72a
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-06-05 20:31:40 +0900

    [Gold III] Title: 유전자, Time: 64 ms, Memory: 3004 KB -BaekjoonHub

---
+++ 백준/Gold/2306. 유전자/유전자.cc
@@ -0,0 +1,99 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(4e18)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+string s;
+int DP[500][500];
+int dp(int l, int r) {
+    if (r <= l) return 0;
+    if (DP[l][r] != -1) return DP[l][r];
+    int ret = 0;
+    if ((s[l] == 'a' && s[r] == 't') || (s[l] == 'g' && s[r] == 'c')) ret = 1 + dp(l + 1, r - 1);
+    for (int i = l; i < r; i++) {
+        ret = max(ret, dp(l, i) + dp(i + 1, r));
+    }
+    return DP[l][r] = ret;
+}
+
+void solve(int tc) {
+
+    cin >> s;
+
+    for (int i = 0; i < s.length(); i++) {
+        for (int j = i; j < s.length(); j++) {
+            DP[i][j] = -1;
+        }
+    }
+
+    cout << dp(0, s.length() - 1) * 2;
+
+
+
+
+}
+    
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
