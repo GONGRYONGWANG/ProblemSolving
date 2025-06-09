fd09b66925672ca0e40d574fbedeac9d9ef3eb3d
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-06-09 15:55:10 +0900

    [Gold IV] Title: CTP 왕국은 한솔 왕국을 이길 수 있을까?, Time: 44 ms, Memory: 2944 KB -BaekjoonHub

---
+++ 백준/Gold/15789. CTP 왕국은 한솔 왕국을 이길 수 있을까？/CTP 왕국은 한솔 왕국을 이길 수 있을까？.cc
@@ -0,0 +1,123 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(4e18)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+int parent[100001];
+int sz[100001];
+int findp(int x) {
+    if (parent[x] == x) return x;
+    return parent[x] = findp(parent[x]);
+}
+
+void solve(int tc) {
+
+    int N, M;
+    cin >> N >> M;
+
+    for (int i = 1; i <= N; i++) parent[i] = i, sz[i] = 1;
+
+    while (M--) {
+        int u, v;
+        cin >> u >> v;
+        u = findp(u); v = findp(v);
+        if (u == v) continue;
+        sz[u] += sz[v];
+        parent[v] = u;
+    }
+
+    int C, H, K;
+    cin >> C >> H >> K;
+    C = findp(C); H = findp(H);
+
+    vector<int> v;
+    for (int i = 1; i <= N; i++) {
+        int u = findp(i);
+        if (u != i) continue;
+        if (u == C || u == H) continue;
+        v.push_back(sz[u]);
+    }
+
+    sort(v.rbegin(), v.rend());
+
+    int ans = sz[C];
+    for (int i = 0; i < min(K, int(v.size())); i++) {
+        ans += v[i];
+    }
+
+    cout << ans;
+
+
+
+
+
+
+
+
+}
+    
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
