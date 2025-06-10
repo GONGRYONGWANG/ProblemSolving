64110c8a823ceb31e96ef911ea16452001cec9fd
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-06-10 22:38:15 +0900

    [Platinum IV] Title: 트리와 경로의 길이, Time: 0 ms, Memory: 2220 KB -BaekjoonHub

---
+++ 백준/Platinum/12928. 트리와 경로의 길이/트리와 경로의 길이.cc
@@ -0,0 +1,103 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(4e18)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+
+int DP[51][1001];
+bool dp(int x, int s) {
+    if (DP[x][s] != -1) return DP[x][s];
+    bool ret = false;
+    for (int i = 1; i <= x; i++) {
+        if (i * (i + 1) / 2 > s) continue;
+        ret |= dp(x - i, s - i * (i + 1) / 2);
+    }
+    return DP[x][s] = ret;
+}
+
+void solve(int tc) {
+
+    int N, S;
+    cin >> N >> S;
+
+    if (N == 1) {
+        cout << (S == 0);
+        return;
+    }
+
+    for (int i = 1; i <= N - 2; i++) {
+        for (int j = 0; j <= S; j++) {
+            DP[i][j] = -1;
+        }
+    }
+    DP[0][0] = 1;
+
+    cout << dp(N - 2, S);
+
+}
+    
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
