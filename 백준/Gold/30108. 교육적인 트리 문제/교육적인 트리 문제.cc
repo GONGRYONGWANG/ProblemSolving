4b311743a5d7d7b04313519ecdb3d0cbe7721418
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-06-11 15:41:01 +0900

    [Gold IV] Title: 교육적인 트리 문제, Time: 252 ms, Memory: 25868 KB -BaekjoonHub

---
+++ 백준/Gold/30108. 교육적인 트리 문제/교육적인 트리 문제.cc
@@ -0,0 +1,108 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(4e18)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+vector<int> E[300001];
+ll arr[300001];
+
+struct cmp {
+    bool operator()(pll& a, pll& b) {
+        return a.second < b.second;
+    }
+};
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+
+    for (int i = 2; i <= N; i++) {
+        int p; cin >> p;
+        E[p].push_back(i);
+    }
+
+    for (int i = 1; i <= N; i++) cin >> arr[i];
+
+    ll ret = 0;
+
+    pq<pll, vector<pll>, cmp> q;
+    q.push({ 1,arr[1] });
+    while (!q.empty()) {
+        auto [x, d] = q.top();
+        q.pop();
+        ret += d;
+        cout << ret << endl;
+        for (int nx : E[x]) {
+            q.push({ nx,arr[nx] });
+        }
+    }
+
+
+
+}
+    
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
