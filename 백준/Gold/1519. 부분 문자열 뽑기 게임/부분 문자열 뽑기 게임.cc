6bfd56ca3838f12c72548808ae047dc1ad763967
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-06-12 21:45:39 +0900

    [Gold II] Title: 부분 문자열 뽑기 게임, Time: 528 ms, Memory: 54920 KB -BaekjoonHub

---
+++ 백준/Gold/1519. 부분 문자열 뽑기 게임/부분 문자열 뽑기 게임.cc
@@ -0,0 +1,109 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(4e18)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+int DP[1000001];
+int dp(int x) {
+    if (DP[x] != -1) return DP[x];
+
+    DP[x] = inf;
+
+    string s = to_string(x);
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == '0') continue;
+        string nx;
+        for (int j = i; j < s.length(); j++) {
+            nx += s[j];
+            if (dp(x - stoi(nx)) == 0) DP[x] = min(DP[x], stoi(nx));
+        }
+    }
+
+    if (DP[x] == inf) DP[x] = 0;
+    return DP[x];
+}
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+
+    DP[0] = 1;
+    for (int i = 10; i <= N; i++) {
+        DP[i] = -1;
+    }
+    
+    int ret = dp(N);
+    if (ret == 0) cout << -1;
+    else cout << ret;
+
+
+
+}
+    
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
