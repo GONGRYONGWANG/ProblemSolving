34c42104f194d13d3e973f4ea4c103e349eb0540
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-06-17 15:21:08 +0900

    [Gold V] Title: 래환이의 택시 타기 대작전, Time: 32 ms, Memory: 2804 KB -BaekjoonHub

---
+++ 백준/Gold/32530. 래환이의 택시 타기 대작전/래환이의 택시 타기 대작전.cc
@@ -0,0 +1,107 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(4e18)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+    vector<int> arr(N);
+    for (int i = 0; i < N; i++) {
+        int h, m; char tmp;
+        cin >> h >> tmp >> m;
+        arr[i] = h * 60 + m;
+    }
+
+    sort(arr.begin(), arr.end());
+
+    int ret = 1;
+    int cnt = 0;
+    int s = inf;
+    for (int x : arr) {
+        if (cnt == 3 || s + 20 < x) {
+            ret += 1;
+            cnt = 0;
+            s = inf;
+        }
+
+        if (cnt == 0) s = x;
+
+        cnt += 1;
+    }
+
+    cout << ret;
+
+
+
+
+
+}
+    
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
