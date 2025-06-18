b095bb58a6c42f1038f598651315ae0cb1f8ad43
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-06-18 14:40:25 +0900

    [Gold III] Title: 보석, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2492. 보석/보석.cc
@@ -0,0 +1,109 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(4e18)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+
+void solve(int tc) {
+
+    int N, M, T, K;
+    cin >> N >> M >> T >> K;
+
+    vector<pii> arr(T);
+    for (int i = 0; i < T; i++) cin >> arr[i].first >> arr[i].second;
+
+    int mx = 0;
+    pii ret;
+
+    for (int i = 0; i < T; i++) {
+        for (int j = 0; j < T; j++) {
+            int x = arr[i].first;
+            int y = arr[j].second;
+
+            int cnt = 0;
+            for (auto [a, b] : arr) {
+                if (a<x || a>x + K || b<y || b > y + K) continue;
+                cnt += 1;
+            }
+
+            if (cnt >= mx) {
+                mx = cnt;
+                ret = { x,y };
+            }
+        }
+    }
+
+    cout << min(ret.first, N - K) << " " << min(ret.second, M - K) + K << endl << mx;
+
+
+
+
+
+
+}
+    
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
