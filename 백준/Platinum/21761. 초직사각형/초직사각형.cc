6f3c031a3d92dcd5fde73875189b3d3dfdd08180
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-06-24 15:14:57 +0900

    [Platinum V] Title: 초직사각형, Time: 80 ms, Memory: 5872 KB, Score: 70 point -BaekjoonHub

---
+++ 백준/Platinum/21761. 초직사각형/초직사각형.cc
@@ -0,0 +1,117 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(4e18)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+pq<ll> q[4]; 
+ll arr[4]; 
+
+void solve(int tc) {
+    
+    int N, K;
+    cin >> N >> K;
+    
+    for (int i = 0; i < 4; i++) {
+        cin >> arr[i];
+        q[i].push(0);
+    }
+
+    while (N--) {
+        char t; ll u;
+        cin >> t >> u;
+        q[t - 'A'].push(u);
+    }
+
+    while (K--) {
+
+        __int128 total = 1; //__int128
+        for (int i = 0; i < 4; i++) total *= arr[i];
+
+        int idx = 0;
+
+        for (int i = 1; i < 4; i++) {
+            if (total / arr[i] * (arr[i] + q[i].top()) > total / arr[idx] * (arr[idx] + q[idx].top())) {
+                idx = i;
+            }
+        }
+
+        cout << char('A' + idx) << " " << q[idx].top() << endl;
+
+        arr[idx] += q[idx].top();
+        q[idx].pop();
+
+    }
+
+
+
+
+
+
+
+
+}
+    
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
