03571a9ee2a3bc3230f27acb88d4a73b7950efbc
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-06-26 20:17:23 +0900

    [Gold II] Title: 걸어가요, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/34036. 걸어가요/걸어가요.cc
@@ -0,0 +1,112 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(4e18)
+const int inf = 1e9 + 7;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+void solve(int tc) {
+    
+    int N;
+    cin >> N;
+
+    ll X, S;
+    cin >> X >> S;
+
+    vector<pll> arr(N - 1);
+    for (int i = 0; i < N - 1; i++) {
+        cin >> arr[i].first >> arr[i].second;
+    }
+
+    for (auto [x, s] : arr) {
+        if (X < x) X += (x - X + S - 1) / S * S;
+
+        ll NX = -1;
+        for (ll j = X; j < X + S * s; j += S) {
+            if (j % s == x % s) {
+                NX = j;
+                break;
+            }
+        }
+
+        if (NX == -1) {
+            cout << -1;
+            return;
+        }
+
+        X = NX; S = lcm(S, s);
+
+    }
+
+    cout << X;
+
+
+
+
+
+
+}
+    
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
