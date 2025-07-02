cfeaab3e1c323b058b04a5b0ba96c86fe081508a
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-07-02 15:29:58 +0900

    [Gold IV] Title: 여우의 꿈, Time: 76 ms, Memory: 13744 KB -BaekjoonHub

---
+++ 백준/Gold/30190. 여우의 꿈/여우의 꿈.cc
@@ -0,0 +1,103 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(4e18)
+const int inf = 1000000007;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+ll DP[1000001];
+ll mod = 1e9 + 7;
+
+void solve(int tc) {
+
+    int N, K;
+    cin >> N >> K;
+
+    for (int i = 1; i <= N; i++) {
+        DP[i] = DP[i - 1] * 2 + 1;
+        DP[i] %= mod;
+    }
+
+    vector<int> arr(N + 1);
+    for (int i = 1; i <= N; i++) cin >> arr[i];
+
+    ll ans = 0;
+
+    for (int i = N; i >= 1; i--) {
+        int x = arr[i];
+        if (x == K) continue;
+        ans += DP[i - 1] + 1;
+        K = 6 - K - x;
+    }
+
+    cout << ans % mod;
+
+
+
+
+}
+    
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
