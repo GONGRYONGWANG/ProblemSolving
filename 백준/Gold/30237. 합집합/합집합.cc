df1e43d12b383fea066edab67bc55ddcd55e4fad
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-07-09 02:02:38 +0900

    [Gold V] Title: 합집합, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/30237. 합집합/합집합.cc
@@ -0,0 +1,108 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+bitset<51> st[50];
+bitset<51> total;
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+
+    total.reset();
+
+    for (int i = 0; i < N; i++) {
+        st[i].reset();
+        int k; cin >> k;
+        while (k--) {
+            int s;
+            cin >> s;
+            st[i].set(s);
+            total.set(s);
+        }
+    }
+
+    int ans = 0;
+    for (int j = 1; j <= 50; j++) {
+        if (!total[j]) continue;
+        bitset<51> ret;
+        for (int i = 0; i < N; i++) {
+            if (st[i][j]) continue;
+            ret |= st[i];
+        }
+        ans = max(ans, int(ret.count()));
+    }
+
+    cout << ans << endl;
+
+
+
+}
+    
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
