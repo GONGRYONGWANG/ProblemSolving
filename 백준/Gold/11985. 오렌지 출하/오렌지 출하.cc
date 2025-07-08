0fdfc2171b290fffd675ce20f19ac912f73c9662
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-07-08 13:58:55 +0900

    [Gold IV] Title: 오렌지 출하, Time: 32 ms, Memory: 2336 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Gold/11985. 오렌지 출하/오렌지 출하.cc
@@ -0,0 +1,100 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+ll arr[20000];
+ll DP[20001];
+
+void solve(int tc) {
+
+    int N, M, K;
+    cin >> N >> M >> K;
+
+    for (int i = 0; i < N; i++) cin >> arr[i];
+
+    DP[N] = 0;
+    for (int i = N - 1; i >= 0; i--) {
+        ll mn = INF;
+        ll mx = 0;
+        DP[i] = INF;
+        for (int j = i; j < min(N, i + M); j++) {
+            mn = min(mn, arr[j]);
+            mx = max(mx, arr[j]);
+            DP[i] = min(DP[i], K + DP[j + 1] + (mx - mn) * (j - i + 1));
+        }
+    }
+
+    cout << DP[0];
+
+
+
+
+
+}
+    
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
