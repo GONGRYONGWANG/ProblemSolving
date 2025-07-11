5b99123ea4167fe1c8236b8a19b3010641750f1c
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-07-11 19:00:40 +0900

    [Platinum III] Title: 양손 정렬, Time: 24 ms, Memory: 2948 KB -BaekjoonHub

---
+++ 백준/Platinum/30465. 양손 정렬/양손 정렬.cc
@@ -0,0 +1,121 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+int arr[200001];
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+
+    for (int i = 1; i <= N; i++) cin >> arr[i];
+
+    if (N % 2 && arr[(N + 1) / 2] != (N + 1) / 2) {
+        cout << -1;
+        return;
+    }
+
+    vector<bool> visited(N + 1, false);
+    vector<int> cnt(4, 0);
+    
+
+    int ans = 0;
+    for (int i = 1; i <= N; i++) {
+        if (arr[i] == i || visited[i]) continue;
+        visited[i] = true;
+        int sz = 1;
+        int type = (1 << (i <= N / 2));
+        int x = arr[i];
+        while (x != i) {
+            visited[x] = true;
+            type |= (1 << (x <= N / 2));
+            sz += 1;
+            x = arr[x];
+        }
+
+        ans += sz - 1;
+        cnt[type] += 1;
+    }
+
+    ans += 2 * min(cnt[1], cnt[2]) + 2 * abs(cnt[1] - cnt[2]);
+
+    cout << ans;
+
+
+
+
+
+
+
+
+
+
+}
+    
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
