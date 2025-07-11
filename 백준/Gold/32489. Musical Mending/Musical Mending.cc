2b11b5066ae222a76f3ba123682869926a66719f
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-07-11 16:12:39 +0900

    [Gold V] Title: Musical Mending, Time: 20 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/32489. Musical Mending/Musical Mending.cc
@@ -0,0 +1,107 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+int N;
+int arr[100000];
+
+ll cal(int x) {
+
+    ll ret = 0;
+    for (int i = 0; i < N; i++) {
+        ret += abs(x + i - arr[i]);
+    }
+    return ret;
+}
+
+void solve(int tc) {
+
+    cin >> N;
+    for (int i = 0; i < N; i++) cin >> arr[i];
+
+    int l = -4e5;
+    int r = 2e5;
+
+    while (r - l >= 3) {
+        int m1 = (l * 2 + r) / 3;
+        int m2 = (l + 2 * r) / 3;
+        if (cal(m1) <= cal(m2)) r = m2;
+        else l = m1;
+    }
+
+    ll ans = INF;
+    for (int i = l; i <= r; i++) {
+        ans = min(ans, cal(i));
+    }
+
+    cout << ans;
+
+
+}
+    
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
