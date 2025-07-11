5dcde0cb05018921972bef5cf5106c5a3f822c4c
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-07-11 16:15:07 +0900

    [Gold V] Title: Musical Mending, Time: 16 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/32489. Musical Mending/Musical Mending.cc
+++ 백준/Gold/32489. Musical Mending/Musical Mending.cc
@@ -55,39 +55,25 @@ ifstream fin; ofstream fout;
 ///////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////
 
-int N;
-int arr[100000];
-
-ll cal(int x) {
-
-    ll ret = 0;
-    for (int i = 0; i < N; i++) {
-        ret += abs(x + i - arr[i]);
-    }
-    return ret;
-}
 
 void solve(int tc) {
 
+    int N;
     cin >> N;
+    vector<int> arr(N);
     for (int i = 0; i < N; i++) cin >> arr[i];
+    for (int i = 0; i < N; i++) arr[i] -= i;
+    sort(arr.begin(), arr.end());
 
-    int l = -4e5;
-    int r = 2e5;
-
-    while (r - l >= 3) {
-        int m1 = (l * 2 + r) / 3;
-        int m2 = (l + 2 * r) / 3;
-        if (cal(m1) <= cal(m2)) r = m2;
-        else l = m1;
-    }
-
-    ll ans = INF;
-    for (int i = l; i <= r; i++) {
-        ans = min(ans, cal(i));
-    }
-
+    ll ans = 0;
+    for (int i = 0; i < N; i++) ans += abs(arr[i] - arr[N / 2]);
+    
     cout << ans;
+
+
+
+
+
 
 
 }
