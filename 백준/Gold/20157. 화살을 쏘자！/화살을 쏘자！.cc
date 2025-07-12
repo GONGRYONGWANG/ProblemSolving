efa977f7d26e996d481a8ad1ba1e30b66438624a
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-07-12 23:20:24 +0900

    [Gold V] Title: 화살을 쏘자!, Time: 60 ms, Memory: 8116 KB -BaekjoonHub

---
+++ 백준/Gold/20157. 화살을 쏘자！/화살을 쏘자！.cc
@@ -0,0 +1,118 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+
+//int dx[4] = { -1,0,1,0 }; 
+//int dy[4] = { 0,-1,0,1 }; 
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+struct PiiHasher { // unordered_set<pii, PiiHasher>
+    size_t operator()(const pii& x) const {
+        return hash<long long>()(((long long)x.first) ^ (((long long)x.second) << 32));
+    }
+};
+///////////////////////////////////////////////////////////////
+
+unordered_map<pii, int, PiiHasher> m[4];
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+
+    int ans = 0;
+    vector<int> axiscnt(4, 0);
+    while (N--) {
+        int x, y;
+        cin >> x >> y;
+        if (x == 0 ) {
+            if (y < 0) axiscnt[0] += 1;
+            else axiscnt[1] += 1;
+            continue;
+        }
+        if (y == 0) {
+            if (x < 0) axiscnt[2] += 1;
+            else axiscnt[3] += 1;
+            continue;
+        }
+
+        int b = 0;
+        if (x < 0) b += 1;
+        if (y < 0) b += 2;
+        x = abs(x); y = abs(y);
+        int g = gcd(x, y);
+        m[b][{x / g, y / g}] += 1;
+        ans = max(ans, m[b][{x / g, y / g}]);
+    }
+
+    for (int i = 0; i < 4; i++) {
+        ans = max(ans, axiscnt[i]);
+    }
+    
+    cout << ans;
+
+
+
+
+}
+    
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
