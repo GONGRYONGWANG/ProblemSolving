8fef5ddcf3bd8640ae6bc421caaa3d1e6801fb62
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-07-16 01:43:00 +0900

    [Gold V] Title: Crazy tea party, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/3790. Crazy tea party/Crazy tea party.cc
@@ -0,0 +1,82 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,-1,0,1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+void solve(int tc) {
+
+    ll N;
+    cin >> N;
+    if (N <= 2) cout << 0;
+    else if (N % 2) cout << (N / 2 + 1) * (N / 2) - N / 2;
+    else cout << (N / 2) * (N / 2 - 1);
+    cout << endl;
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
---
+++ 백준/Gold/3790. Crazy tea party/README.md
@@ -0,0 +1,28 @@
+# [Gold V] Crazy tea party - 3790 
+
+[문제 링크](https://www.acmicpc.net/problem/3790) 
+
+### 성능 요약
+
+메모리: 2020 KB, 시간: 0 ms
+
+### 분류
+
+애드 혹, 수학, 정렬
+
+### 제출 일자
+
+2025년 7월 16일 01:42:51
+
+### 문제 설명
+
+<p>n participants of «crazy tea party» sit around the table. Each minute one pair of neighbors can change their places. Find the minimum time (in minutes) required for all participants to sit in reverse order (so that left neighbors would become right, and right – left).</p>
+
+### 입력 
+
+ <p>The first line is the amount of tests. Each next line contains one integer n (1 ≤ n ≤ 32767) – the amount of crazy tea participants.</p>
+
+### 출력 
+
+ <p>For each number n of participants to crazy tea party print on the standard output, on a separate line, the minimum time required for all participants to sit in reverse order.</p>
+
