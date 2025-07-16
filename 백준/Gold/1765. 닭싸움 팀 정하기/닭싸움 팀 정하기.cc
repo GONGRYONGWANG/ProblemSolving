c340defe5242266af80f8daae5939e23ae007e52
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-07-16 19:02:26 +0900

    [Gold II] Title: 닭싸움 팀 정하기, Time: 0 ms, Memory: 2188 KB -BaekjoonHub

--- 백준/Gold/1765. 닭싸움 팀 정하기/닭싸움 팀 정하기.cc
+++ 백준/Gold/1765. 닭싸움 팀 정하기/닭싸움 팀 정하기.cc
@@ -3,12 +3,15 @@
 #include<iomanip>
 #include<cstdio>
 #include<string>
+#include<cctype>
 #include<vector>
+#include<array>
 #include<utility>
 #include<list>
 #include<queue>
 #include<stack>
 #include<deque>
+#include<tuple>
 #include<set>
 #include<unordered_set>
 #include<map>
@@ -24,424 +27,34 @@
 #include<numeric>
 #include<cassert>
 #include <climits>
-#include<fstream>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
 using namespace std;
 typedef long long ll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 typedef unsigned long long ull;
 typedef unsigned int uint;
-typedef long double ld;
 typedef complex<double> cpx;
+typedef long double ld;
 #define pq priority_queue
 #define endl "\n"
-const ll INF = 1e18 + 7;
-const int inf = 1e9 + 7;
+#define INF ll(2e18)
+const int inf = 1000000007;
 const long double pi = 3.14159265358979323846;
 const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
 ifstream fin; ofstream fout;
 
 
-struct VectorHasher { // unordered_set<vector<int>,VectorHasher>
-    size_t operator()(const vector<int>& V) const {
-        size_t hash = V.size();
-        for (auto& i : V) {
-            hash ^= i + 0x9e3779b9 + (hash << 6) + (hash >> 2);
-        }
-        return hash;
-    }
-};
-struct PiiHasher { // unordered_set<pii, PiiHasher>
-    size_t operator()(const pii& x) const {
-        return hash<long long>()(((long long)x.first) ^ (((long long)x.second) << 32));
-    }
-};
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,-1,0,1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
 
-ll gcd(ll a, ll b) {
-    if (a < b) swap(a, b);
-    while (b != 0) {
-        ll  n = a % b;
-        a = b;
-        b = n;
-    }
-    return a;
-}
-ll lcm(ll a, ll b) {
-    ll g = gcd(a, b);
-    return a / g * b;
-}
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
 
-ll powmod(ll x, ll n, ll mod) {
-    if (n == 0) return 1;
-    if (n % 2) return x * powmod(x, n - 1, mod) % mod;
-    ll half = powmod(x, n / 2, mod);
-    return half * half % mod;
-}
-ll modinv(ll x, ll mod) { // when mod is primenum
-    return powmod(x, mod - 2, mod);
-}
-
-
-struct MF { // Dinic O(V^2*E)
-    struct Edge {
-        int to, rev;
-        long long flow, cap;
-        Edge(int to, int rev, long long cap) : to(to), rev(rev), flow(0), cap(cap) {}
-    };
-
-    int n;
-    vector<vector<Edge>> adj; // get이후 edge vector를 이용해서 trace, cap<=flow인 간선은 continue하며 bfs.
-    vector<int> level, ptr;
-
-    MF(int n) : n(n), adj(n), level(n), ptr(n) {}
-
-    void addEdge(int u, int v, long long cap, bool directed = true) {
-        if (cap == 0) return;
-        adj[u].emplace_back(v, adj[v].size(), cap);
-        if (!directed) adj[v].emplace_back(u, adj[u].size() - 1, cap);
-        else adj[v].emplace_back(u, adj[u].size() - 1, 0);
-    }
-
-    bool bfs(int source, int sink) {
-        fill(level.begin(), level.end(), -1);
-        level[source] = 0;
-        queue<int> q;
-        q.push(source);
-
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-            for (Edge& e : adj[node]) {
-                if (level[e.to] == -1 && e.flow < e.cap) { // 잔여 용량이 있는 간선을 찾음
-                    level[e.to] = level[node] + 1;
-                    q.push(e.to);
-                }
-            }
-        }
-        return level[sink] != -1;
-    }
-
-    long long dfs(int node, int sink, long long pushed) {
-        if (pushed == 0) return 0;
-        if (node == sink) return pushed;
-
-        for (int& cid = ptr[node]; cid < adj[node].size(); cid++) {
-            Edge& e = adj[node][cid];
-            if (level[e.to] != level[node] + 1 || e.flow == e.cap) continue;
-
-            long long tr = dfs(e.to, sink, min(pushed, e.cap - e.flow));
-            if (tr == 0) continue;
-
-            e.flow += tr;
-            adj[e.to][e.rev].flow -= tr;
-            return tr;
-        }
-        return 0;
-    }
-    long long get(int source, int sink) {
-        long long flow = 0;
-        while (bfs(source, sink)) {
-            fill(ptr.begin(), ptr.end(), 0);
-            while (long long pushed = dfs(source, sink, LLONG_MAX)) {
-                flow += pushed;
-            }
-        }
-        return flow;
-    }
-    void trace(int src) {
-        queue<int> q;
-        q.push(src);
-        vector<bool> visited(n, false);
-        visited[src] = true;
-        vector<int> ret;
-        while (!q.empty()) {
-            int cur = q.front();
-            q.pop();
-            for (Edge& e : adj[cur]) {
-                if (e.cap <= e.flow) continue;
-                if (visited[e.to]) continue;
-                q.push(e.to);
-                ret.push_back(e.to);
-                visited[e.to] = true;
-            }
-        }
-        cout << ret.size() << " ";
-        for (int x : ret) {
-            cout << x << " ";
-        }
-        cout << endl;
-    }
-};
-struct MCMF { // SPFA O(VEf) - Practically Ef
-    struct Edge {
-        int to, rev;
-        long long cap, flow, cost;
-        Edge(int to, int rev, long long cap, long long cost) : to(to), rev(rev), cap(cap), flow(0), cost(cost) {}
-    };
-
-    int n;
-    vector<vector<Edge>> adj;
-    vector<long long> dist;
-    vector<int> parent, parentEdge;
-    vector<bool> inQueue;
-    int source, sink;
-
-    MCMF(int n) : n(n), adj(n), dist(n), parent(n), parentEdge(n), inQueue(n), source(-1), sink(-1) {}
-
-    void setSource(int s) {
-        source = s;
-    }
-
-    void setSink(int t) {
-        sink = t;
-    }
-
-    void addEdge(int u, int v, long long cap, long long cost, bool directed = true) {
-        if (cap == 0) return;
-        adj[u].emplace_back(v, adj[v].size(), cap, cost);
-        adj[v].emplace_back(u, adj[u].size() - 1, 0, -cost); // 역방향 간선
-        if (!directed) {
-            adj[v].emplace_back(u, adj[u].size(), cap, cost);
-            adj[u].emplace_back(v, adj[v].size() - 1, 0, -cost);
-        }
-    }
-
-    bool spfa() {
-        fill(dist.begin(), dist.end(), LLONG_MAX);
-        fill(inQueue.begin(), inQueue.end(), false);
-        queue<int> q;
-        dist[source] = 0;
-        inQueue[source] = true;
-        q.push(source);
-
-        while (!q.empty()) {
-            int u = q.front();
-            q.pop();
-            inQueue[u] = false;
-
-            for (int i = 0; i < adj[u].size(); ++i) {
-                Edge& e = adj[u][i];
-                if (e.flow < e.cap && dist[u] + e.cost < dist[e.to]) {
-                    dist[e.to] = dist[u] + e.cost;
-                    parent[e.to] = u;
-                    parentEdge[e.to] = i;
-
-                    if (!inQueue[e.to]) {
-                        inQueue[e.to] = true;
-                        q.push(e.to);
-                    }
-                }
-            }
-        }
-
-        return dist[sink] != LLONG_MAX;
-    }
-    pair<long long, long long> get(int _source = -1, int _sink = -1) {
-        if (_source != -1) {
-            setSource(_source);
-            setSink(_sink);
-        }
-        long long maxFlow = 0, minCost = 0;
-
-        while (spfa()) {
-            long long flow = LLONG_MAX;
-
-            for (int u = sink; u != source; u = parent[u]) {
-                Edge& e = adj[parent[u]][parentEdge[u]];
-                flow = min(flow, e.cap - e.flow);
-            }
-
-            for (int u = sink; u != source; u = parent[u]) {
-                Edge& e = adj[parent[u]][parentEdge[u]];
-                e.flow += flow;
-                adj[u][e.rev].flow -= flow;
-                minCost += flow * e.cost;
-            }
-
-            maxFlow += flow;
-        }
-        return { maxFlow, minCost };
-    }
-};
-struct BiMatch { // Hopcroft-Karp O(E*sqrtV)
-    int n, m; // n: left side vertices, m: right side vertices
-    vector<vector<int>> adj; // adjacency list
-    vector<int> pairU, pairV, dist;
-    // pairU 또는 pairV를 출력해서 매칭 결과를 print 가능.
-
-    BiMatch(int n, int m) : n(n), m(m), adj(n + 1), pairU(n + 1), pairV(m + 1), dist(n + 1) {}
-
-    void addEdge(int u, int v) {
-        adj[u].push_back(v);
-    }
-
-    bool bfs() {
-        queue<int> q;
-        for (int u = 1; u <= n; u++) {
-            if (pairU[u] == 0) { // If u is free (not matched)
-                dist[u] = 0;
-                q.push(u);
-            }
-            else {
-                dist[u] = INT_MAX;
-            }
-        }
-
-        dist[0] = INT_MAX;
-        while (!q.empty()) {
-            int u = q.front();
-            q.pop();
-
-            if (dist[u] < dist[0]) {
-                for (int v : adj[u]) {
-                    if (dist[pairV[v]] == INT_MAX) { // If pairV[v] is not yet visited
-                        dist[pairV[v]] = dist[u] + 1;
-                        q.push(pairV[v]);
-                    }
-                }
-            }
-        }
-
-        return dist[0] != INT_MAX; // If there's an augmenting path
-    }
-
-    bool dfs(int u) {
-        if (u != 0) {
-            for (int v : adj[u]) {
-                if (dist[pairV[v]] == dist[u] + 1 && dfs(pairV[v])) {
-                    pairV[v] = u;
-                    pairU[u] = v;
-                    return true;
-                }
-            }
-            dist[u] = INT_MAX;
-            return false;
-        }
-        return true;
-    }
-
-    int get() {
-        fill(pairU.begin(), pairU.end(), 0);
-        fill(pairV.begin(), pairV.end(), 0);
-        int matching = 0;
-
-        while (bfs()) {
-            for (int u = 1; u <= n; u++) {
-                if (pairU[u] == 0 && dfs(u)) {
-                    matching++;
-                }
-            }
-        }
-
-        return matching;
-    }
-
-};
-
-struct TwoSat { // for SCC and TwoSat.
-    int n;
-    vector<vector<int>> g, gr; // gr is the reversed graph
-    vector<int> scc_id, topological_order, answer; // scc_id[v]: ID of the SCC containing node v
-    vector<bool> visited;
-
-    TwoSat() {}
-
-    TwoSat(int _n) { init(_n); }
-
-    void init(int _n) {
-        n = _n;
-        g.assign(2 * n, vector<int>());
-        gr.assign(2 * n, vector<int>());
-        scc_id.resize(2 * n);
-        visited.resize(2 * n);
-        answer.resize(2 * n);
-    }
-
-    // Can be used conveniently for SCC tasks by using the edge function only. The drawback is double memory usage.
-    void add_edge(int u, int v) {
-        g[u].push_back(v);
-        gr[v].push_back(u);
-    }
-
-    // For the following three functions
-    // int x, bool val: if 'val' is true, we take the variable to be x. Otherwise we take it to be x's complement.
-
-    // At least one of them is true
-    void add_clause_or(int i, int j, bool f = true, bool g = true) {
-        if (i < 0) f = !f;
-        if (j < 0) g = !g;
-        i = abs(i); j = abs(j);
-        add_edge(i + (f ? n : 0), j + (g ? 0 : n));
-        add_edge(j + (g ? n : 0), i + (f ? 0 : n));
-    }
-
-    // Only one of them is true
-    void add_clause_xor(int i, int j, bool f = true, bool g = true) {
-        if (i < 0) f = !f;
-        if (j < 0) g = !g;
-        i = abs(i); j = abs(j);
-        add_clause_or(i, f, j, g);
-        add_clause_or(i, !f, j, !g);
-    }
-
-    // Both of them have the same value
-    void add_clause_and(int i, int j, bool f = true, bool g = true) {
-        if (i < 0) f = !f;
-        if (j < 0) g = !g;
-        i = abs(i); j = abs(j);
-        add_clause_xor(i, !f, j, g);
-    }
-
-    // Topological sort
-    void dfs(int u) {
-        visited[u] = true;
-
-        for (const auto& v : g[u])
-            if (!visited[v]) dfs(v);
-
-        topological_order.push_back(u);
-    }
-
-    // Extracting strongly connected components
-    void scc(int u, int id) {
-        visited[u] = true;
-        scc_id[u] = id;
-
-        for (const auto& v : gr[u]) {
-            if (!visited[v]) scc(v, id);
-        }
-    }
-
-    // Returns true if the given proposition is satisfiable and constructs a valid assignment
-    bool satisfiable() {
-        fill(visited.begin(), visited.end(), false);
-
-        for (int i = 0; i < 2 * n; i++)
-            if (!visited[i]) dfs(i);
-
-        fill(visited.begin(), visited.end(), false);
-        reverse(topological_order.begin(), topological_order.end());
-
-        int id = 0;
-        for (const auto& v : topological_order)
-            if (!visited[v]) scc(v, id++);
-
-        // Constructing the answer
-        for (int i = 0; i < n; i++) {
-            if (scc_id[i] == scc_id[i + n]) return false;
-            answer[i] = (scc_id[i] > scc_id[i + n] ? 1 : 0);
-        }
-
-        return true;
-    }
-};
-
-//int dx[4] = { 0,1,0,-1 }; 
-//int dy[4] = { 1,0,-1,0 }; 
-//int dx[8] = { -1,-1,-1,0,0,1,1,1 };
-//int dy[8] = { -1,0,1,-1,1,-1,0,1 };
-
-vector<int> F[1001];
 vector<int> E[1001];
 int parent[1001];
 int findp(int x) {
@@ -449,47 +62,65 @@ int findp(int x) {
     return parent[x] = findp(parent[x]);
 }
 
-void solve() {
+void solve(int tc) {
 
     int N, M;
     cin >> N >> M;
-    for (int i = 0; i < M; i++) {
-        char t; int u, v;
-        cin >> t >> u >> v;
-        if (t == 'F') F[u].push_back(v);
+
+    vector<pii> F;
+
+    while (M--) {
+        char x; int p, q;
+        cin >> x >> p >> q;
+        if (x == 'F') F.push_back({ p,q });
         else {
-            E[u].push_back(v);
-            E[v].push_back(u);
-        }
-    }
-
-    for (int i = 1; i <= N; i++) parent[i] = i;
-
-    for (int i = 1; i <= N; i++) {
-        for (int f : F[i]) {
-            parent[findp(f)] = findp(i);
-        }
-        if (!E[i].empty()) {
-            for (int e : E[i]) {
-                parent[findp(e)] = findp(E[i].front());
-            }
+            E[p].push_back(q);
+            E[q].push_back(p);
         }
     }
 
     int ans = 0;
     for (int i = 1; i <= N; i++) {
-        if (findp(i) == i) ans += 1;
+        if (parent[i]) continue;
+        ans += 1;
+        parent[i] = i;
+        if (E[i].empty()) continue;
+        ans += 1;
+        int f = i; int e = E[i].front();
+        queue<int> q;
+        q.push(i);
+        while (!q.empty()) {
+            int x = q.front();
+            q.pop();
+            for (int nx : E[x]) {
+                if (parent[nx]) continue;
+                if (parent[x] == f) parent[nx] = e;
+                else parent[nx] = f;
+                q.push(nx);
+            }
+        }
     }
+
+    for (auto [u, v] : F) {
+        u = findp(u); v = findp(v);
+        if (u == v) continue;
+        parent[u] = v;
+        ans -= 1;
+    }
+
     cout << ans;
 
+
 }
+
 
 int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL);
     int T = 1;
     //cin >> T;
-    while (T--) {
-        solve();
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
     }
 
     return 0;
