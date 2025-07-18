76212754230582082deb3ae60b6cf1993af23034
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-07-18 17:00:45 +0900

    [Gold III] Title: 줄 서기, Time: 28 ms, Memory: 2152 KB -BaekjoonHub

---
+++ 백준/Gold/2273. 줄 서기/줄 서기.cc
@@ -0,0 +1,119 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,-1,0,1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+    
+
+
+bool A[257][257];
+bool B[257][257];
+
+void solve(int tc) {
+
+    int N, M;
+    cin >> N >> M;
+
+    while (M--) {
+        int u, v;
+        cin >> u >> v;
+        A[v][u] = B[u][v] = true;
+    }
+
+    for (int k = 1; k <= N; k++) {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                A[i][j] |= (A[i][k] & A[k][j]);
+                B[i][j] |= (B[i][k] & B[k][j]);
+            }
+        }
+    }
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            if (A[i][j] && B[i][j]) {
+                cout << -1;
+                return;
+            }
+        }
+    }
+    
+    for (int i = 1; i <= N; i++) {
+        int a = 0, b = 0;
+        for (int j = 1; j <= N; j++) {
+            if (A[i][j]) a += 1;
+            if (B[i][j]) b += 1;
+        }
+        cout << a + 1 << " " << N - b << endl;
+    }
+
+
+
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
