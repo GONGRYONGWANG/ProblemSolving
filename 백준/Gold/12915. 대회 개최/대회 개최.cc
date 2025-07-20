c68908b91b762fdc8b8f8f0dfb471fed6aa67e8e
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-07-20 14:41:52 +0900

    [Gold V] Title: 대회 개최, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/12915. 대회 개최/대회 개최.cc
@@ -0,0 +1,88 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,-1,0,1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+    
+
+void solve(int tc) {
+
+    int a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+    int x = inf;
+    x = min(x, a + b);
+    x = min(x, b + c + d);
+    x = min(x, d + e);
+    x = min(x, (a + b + c + d) / 2);
+    x = min(x, (b + c + d + e) / 2);
+    x = min(x, (a + b + c + d + e) / 3);
+
+    cout << x;
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
