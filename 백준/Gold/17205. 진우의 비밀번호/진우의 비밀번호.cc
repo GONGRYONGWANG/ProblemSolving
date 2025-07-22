8d82059c3960a24354de1b438aea4212a8d5d0d6
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-07-22 14:17:26 +0900

    [Gold IV] Title: 진우의 비밀번호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/17205. 진우의 비밀번호/진우의 비밀번호.cc
@@ -0,0 +1,100 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,-1,0,1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+ll DP[11];
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+
+    DP[1] = 1;
+    for (int i = 2; i <= N; i++) {
+        DP[i] = 1 + DP[i - 1] * 26;
+    }
+
+    string s;
+    cin >> s;
+
+    ll ans = s.length();
+    for (int i = 0; i < s.length(); i++) {
+        ans += DP[N - i] * (s[i] - 'a');
+    }
+
+    cout << ans;
+
+
+
+
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
