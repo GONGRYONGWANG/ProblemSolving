78ee278a4590245d590e13ded4dcb2a302cb9465
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-07-30 18:35:54 +0900

    [Gold I] Title: 인경호수공원, Time: 244 ms, Memory: 17652 KB -BaekjoonHub

---
+++ 백준/Gold/31856. 인경호수공원/인경호수공원.cc
@@ -0,0 +1,110 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+
+//int dx[4] = { 1,0,-1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+
+    vector<ll> A(2 * N), B(2 * N);
+    for (int i = 0; i < N; i++) cin >> A[i];
+    for (int i = 0; i < N; i++) cin >> B[i];
+    for (int i = 0; i < N; i++) {
+        A[i + N] = A[i]; B[i + N] = B[i];
+    }
+    
+    for (int i = 1; i < 2 * N; i++) A[i] += A[i - 1];
+
+    ll ans = 0;
+
+    multiset<ll> st;
+    for (int i = 1; i < N; i++) {
+        st.insert(A[i - 1] + B[i]);
+    }
+
+    ans = max(ans, B[0] + *st.rbegin());
+
+    for (int i = 1; i < N; i++) {
+        st.erase(A[i - 1] + B[i]);
+        st.insert(A[i + N - 2] + B[i + N - 1]);
+        ans = max(ans, B[i] + *st.rbegin() - A[i - 1]);
+    }
+
+    cout << ans;
+
+    
+
+
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
