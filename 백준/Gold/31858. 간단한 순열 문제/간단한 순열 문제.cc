a7253bcc107271630c385dd37b3887ff56b01285
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-07-31 19:04:42 +0900

    [Gold III] Title: 간단한 순열 문제, Time: 20 ms, Memory: 4372 KB -BaekjoonHub

---
+++ 백준/Gold/31858. 간단한 순열 문제/간단한 순열 문제.cc
@@ -0,0 +1,93 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+
+//int dx[4] = { 1,0,-1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+    vector<int> arr(N);
+    for (int i = 0; i < N; i++) cin >> arr[i];
+
+    vector<int> mx1(N), mx2(N);
+    mx1[0] = arr[0];
+    for (int i = 1; i < N; i++) mx1[i] = max(mx1[i - 1], arr[i]);
+    mx2[N - 1] = arr[N - 1];
+    for (int i = N - 2; i >= 0; i--) mx2[i] = max(mx2[i + 1], arr[i]);
+
+    int ans = N - 1;
+    for (int i = 1; i < N - 1; i++) {
+        if (mx1[i - 1] > arr[i] && mx2[i + 1] > arr[i]) ans += 1;
+    }
+
+    cout << ans;
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
