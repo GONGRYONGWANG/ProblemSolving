8e77a119d00131427b757c9a93a89864a4036f07
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-08-03 23:13:18 +0900

    [Platinum V] Title: 전투기 출격, Time: 284 ms, Memory: 49220 KB -BaekjoonHub

---
+++ 백준/Platinum/27116. 전투기 출격/전투기 출격.cc
@@ -0,0 +1,146 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+
+//int dx[4] = { 1,0,-1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+ll d[101][101];
+ll F[101];
+int arr[100001];
+ll psum[100001];
+queue<int> q[101][101];
+
+ll N, M, R;
+
+ll cal(int x, int y) {
+    if (y == N + 1) {
+        return F[arr[x]] + psum[x];
+    }
+    return F[arr[x]] + psum[N] - psum[y] + psum[x] + d[arr[x]][arr[y]];
+}
+
+void solve(int tc) {
+    
+    cin >> N >> M >> R;
+
+    for (int i = 1; i <= M; i++) {
+        for (int j = 1; j <= M; j++) {
+            cin >> d[i][j];
+        }
+    }
+
+    for (int k = 1; k <= M; k++) {
+        for (int i = 1; i <= M; i++) {
+            for (int j = 1; j <= M; j++) {
+                d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+            }
+        }
+    }
+
+    for (int i = 1; i <= M; i++) cin >> F[i];
+
+    arr[0] = 1;
+    for (int i = 1; i <= N; i++) cin >> arr[i];
+
+    for (int i = 1; i <= N; i++) psum[i] = psum[i - 1] + d[arr[i - 1]][arr[i]];
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= M; j++) {
+            q[j][arr[i]].push(i);
+        }
+    }
+
+    for (int i = 1; i <= M; i++) {
+        for (int j = 2; j <= M; j++) {
+            q[i][j].push(N + 1);
+        }
+    }
+
+    int L = inf;
+    ll val = INF;
+    for (int i = 1; i <= N; i++) {
+        int a = arr[i];
+        for (int j = 1; j <= M; j++) {
+            while (!q[a][j].empty() && (q[a][j].front() <= i || cal(i, q[a][j].front()) > R )) {
+                q[a][j].pop();
+            }
+
+            if (!q[a][j].empty()) {
+                if (q[a][j].front() - i - 1 < L) val = INF;
+                L = min(L, q[a][j].front() - i - 1);
+                if (q[a][j].front() - i - 1 == L) val = min(val, cal(i, q[a][j].front()));
+            }
+        }
+
+    }
+
+    if (L == inf) cout << -1;
+    else cout << L << " " << val;
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
