53e3895221c3f8c895c6adda8589bb7ced7ddfbb
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-08-03 14:00:14 +0900

    [Gold II] Title: 메이플스토리, Time: 124 ms, Memory: 2960 KB -BaekjoonHub

---
+++ 백준/Gold/20925. 메이플스토리/메이플스토리.cc
@@ -0,0 +1,116 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+
+//int dx[4] = { 1,0,-1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+int DP[200][1001];
+int c[200], e[200];
+int t[200][200];
+
+void solve(int tc) {
+
+    int N, T;
+    cin >> N >> T;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j <= T; j++) {
+            DP[i][j] = -1;
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        cin >> c[i] >> e[i];
+        if (c[i] == 0) DP[i][0] = 0;
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> t[i][j];
+        }
+    }
+
+    for (int j = 0; j < T; j++) {
+        for (int i = 0; i < N; i++) {
+            if (DP[i][j] == -1) continue;
+            DP[i][j + 1] = max(DP[i][j + 1], DP[i][j] + e[i]);
+            for (int k = 0; k < N; k++) {
+                if (t[i][k] + j > T || c[k] > DP[i][j]) continue;
+                DP[k][t[i][k] + j] = max(DP[k][t[i][k] + j], DP[i][j]);
+            }
+        }
+    }
+
+    int ans = 0;
+    for (int i = 0; i < N; i++) ans = max(ans, DP[i][T]);
+
+    cout << ans;
+
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
