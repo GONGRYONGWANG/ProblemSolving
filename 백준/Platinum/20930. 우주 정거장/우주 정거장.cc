47eed7d0a12a02c261aa1c6ab38cc3d9fd581f1d
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-08-05 16:21:28 +0900

    [Platinum V] Title: 우주 정거장, Time: 296 ms, Memory: 24728 KB -BaekjoonHub

---
+++ 백준/Platinum/20930. 우주 정거장/우주 정거장.cc
@@ -0,0 +1,124 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = 3.14159265358979323846;
+const string debug = "output: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+
+//int dx[4] = { 1,0,-1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+int parent[200001];
+int findp(int x) {
+    if (parent[x] == x) return x;
+    return parent[x] = findp(parent[x]);
+}
+
+void solve(int tc) {
+    
+    int N, Q;
+    cin >> N >> Q;
+
+    vector<vector<int>> X(N), Y(N);
+
+    for (int i = 0; i < N; i++) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        if (x1 > x2) swap(x1, x2);
+        if (y1 > y2) swap(y1, y2);
+        X[i] = { x1,x2,i + 1 };
+        Y[i] = { y1,y2,i + 1 };
+    }
+    sort(X.begin(), X.end()); sort(Y.begin(), Y.end());
+
+    for (int i = 1; i <= N; i++) parent[i] = i;
+
+    int e = -inf;
+    for (int i = 0; i < N; i++) {
+        int l = X[i][0]; int r = X[i][1];
+        if (l <= e)  parent[findp(X[i][2])] = findp(X[i - 1][2]);
+        e = max(e, r);
+    }
+
+    e = -inf;
+    for (int i = 0; i < N; i++) {
+        int l = Y[i][0]; int r = Y[i][1];
+        if (l <= e)  parent[findp(Y[i][2])] = findp(Y[i - 1][2]);
+        e = max(e, r);
+    }
+
+    while (Q--) {
+        int u, v;
+        cin >> u >> v;
+        u = findp(u); v = findp(v);
+        cout << (u == v) << endl;
+    }
+
+
+
+
+
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
