f9f1c882726e0fdfe57c72ab56c7f3603180e046
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-08-11 14:38:53 +0900

    [Platinum V] Title: Cryptography, Time: 396 ms, Memory: 20844 KB -BaekjoonHub

---
+++ 백준/Platinum/19666. Cryptography/Cryptography.cc
@@ -0,0 +1,142 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/gcctemplate_for_pbds&rope
+#include<bits/stdc++.h>
+#include<ext/rope>
+/*
+string s;
+s.c_str() -> 스트링을 char arr로
+rope<char> rp(s.c_str());
+rp.insert(idx, s.c_str());
+rp.insert(idx, some_char);'
+cout<<rp.substr(idx, length);
+*/
+typedef long long ll;
+using namespace std;
+using namespace __gnu_cxx;
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+
+
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+typedef tree<ll,null_type,less_equal<ll>,rb_tree_tag,tree_order_statistics_node_update> ordered_multiset;
+
+# pragma GCC optimize ("O3")
+# pragma GCC optimize ("Ofast")
+# pragma GCC optimize ("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,avx,avx2") //SIMD
+
+
+
+#define pq priority_queue
+#define endl "\n"
+
+const ll INF = 1e18;
+const int inf = 1e9;
+const double pi = 3.14159265358979323846;
+
+const string debug = "output: ";
+
+ll gcd(ll a, ll b) {
+    if (a < b) swap(a, b);
+    while (b != 0) {
+        ll  n = a % b;
+        a = b;
+        b = n;
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    ll g = gcd(a, b);
+    return a / g * b;
+}
+
+ll powmod(ll x, ll n, ll mod) {
+    if (n == 0) return 1;
+    if (n % 2) return x * powmod(x, n - 1, mod) % mod;
+    ll half = powmod(x, n / 2, mod);
+    return half * half % mod;
+}
+
+ll modinv(ll x, ll mod) {
+    return powmod(x, mod - 2, mod);
+}
+
+ll factorial(ll x) {
+    ll ret = 1;
+    // ret = F[x];
+    return ret;
+}
+
+ll combination(ll n, ll r, ll mod) {
+    return factorial(n) * modinv(factorial(n - r), mod) % mod * modinv(factorial(r), mod) % mod;
+}
+
+ifstream fin; ofstream fout;
+//int dx[4] = { 0,1,0,-1 };
+//int dy[4] = { 1,0,-1,0 };
+//int dx[8] = { -1,-1,-1,0,0,1,1,1 };
+//int dy[8] = { -1,0,1,-1,1,-1,0,1 };
+
+
+string deb = "debug: ";
+
+
+ll F[300001];
+
+void solve(int tc) {
+
+
+    int N;
+    cin >> N;
+    ll mod = 1e9 + 7;
+
+    F[0]=1;
+    for(int i=1; i<=N;i++){
+        F[i] = F[i-1]*i%mod;
+    }
+
+    ordered_set os;
+    vector<ll> arr(N);
+    for(int i=0; i<N;i++){
+        cin>>arr[i];
+        os.insert(arr[i]);
+    }
+
+    ll ret = F[N];
+
+    for(int i =0; i<N;i++ ){
+        ll x  =arr[i];
+        ll cnt =  os.size() - os.order_of_key(x+1);
+
+        ret -= cnt*F[N-1-i]%mod;
+        ret %=mod;
+
+        os.erase(x);
+    }
+
+
+
+    cout<< (ret+mod)%mod;
+
+
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    //fin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
---
+++ 백준/Platinum/19666. Cryptography/README.md
@@ -0,0 +1,51 @@
+# [Platinum V] Cryptography - 19666 
+
+[문제 링크](https://www.acmicpc.net/problem/19666) 
+
+### 성능 요약
+
+메모리: 20844 KB, 시간: 396 ms
+
+### 분류
+
+수학, 자료 구조, 세그먼트 트리, 조합론, 값 / 좌표 압축
+
+### 제출 일자
+
+2025년 8월 11일 14:38:05
+
+### 문제 설명
+
+<p>Charles the Cryptographer has been researching novel methods of generating random numbers. In particular, by combining multiple sources of random numbers, he hopes to create a <em>cryptographically secure pseudorandom number generator (CSPRNG)</em>.</p>
+
+<p>One algorithm that he has recently invented is as follows:</p>
+
+<ol>
+	<li>Randomly generate a sequence S of N distinct positive integers S<sub>1</sub>, . . . , S<sub>N</sub></li>
+	<li>Randomly shuffle S to obtain a permutation<sup>1</sup> P of N elements P<sub>1</sub>, . . . , P<sub>N</sub></li>
+	<li>Find the lexicographical order of P</li>
+	<li>As the answer can be very large, output the value modulo<sup>2</sup> 1 000 000 007</li>
+</ol>
+
+<p>The lexicographical order of P is defined as the number of permutations of S that are lexicographically smaller than<sup>3</sup> or equal to P.</p>
+
+<p>Unfortunately, Charles is a Cryptographer and not a Coder. Given the resultant permutation P, help Charles to find its lexicographical order, modulo 1 000 000 007.</p>
+
+<p><sup>1</sup>A permutation P of a sequence S is a rearrangement of the elements of S</p>
+
+<p><sup>2</sup>The remainder when the value is divided by 1 000 000 007</p>
+
+<p><sup>3</sup>A permutation P<sub>1</sub>, . . . , P<sub>N</sub> is considered lexicographically smaller than another permutation P'<sub>1</sub> , . . . , P'<sub>N</sub> if there exists 1 ≤ k ≤ N such that P<sub>k</sub> < P'<sub>k</sub> and P<sub>i</sub> = P'<sub>i</sub> for i = 1, . . . , k − 1.</p>
+
+### 입력 
+
+ <p>Your program must read from standard input.</p>
+
+<p>The first line contains a single integer N.</p>
+
+<p>The second line contains N space-separated integers, P<sub>1</sub>, . . . , P<sub>N</sub>.</p>
+
+### 출력 
+
+ <p>Your program must print to standard output. The output should contain a single integer on a single line, the lexicographical order of P, modulo 1 000 000 007.</p>
+
