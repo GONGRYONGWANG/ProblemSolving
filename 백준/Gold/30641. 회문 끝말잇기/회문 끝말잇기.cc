e957513a0f33fd7353a2c418560decc544124338
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-08-13 03:01:58 +0900

    [Gold V] Title: 회문 끝말잇기, Time: 12 ms, Memory: 9836 KB -BaekjoonHub

---
+++ 백준/Gold/30641. 회문 끝말잇기/회문 끝말잇기.cc
@@ -0,0 +1,96 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = 3.14159265358979323846;
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+
+//int dx[4] = { 1,0,-1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////
+
+ll pow26[1000001];
+
+void solve(int tc) {
+
+    int L, U;
+    cin >> L >> U;
+
+    ll mod = 1e9 + 7;
+    pow26[0] = 1;
+    for (int i = 1; i <= U; i++) {
+        pow26[i] = pow26[i - 1] * 26 % mod;
+    }
+
+    ll ret = 0;
+    for (int i = L; i <= U; i++) {
+        ret += pow26[(i + 1) / 2 - 1];
+        ret %= mod;
+    }
+
+    if (U == 1 || L == 2) cout << "H";
+    else cout << "A";
+    cout << endl;
+    cout << ret;
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
