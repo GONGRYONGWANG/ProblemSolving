0ebe91d40a5dcf1abd538fac137937f9adc111ed
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-08-18 20:04:09 +0900

    [Bronze I] Title: ISBN, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14626. ISBN/ISBN.cc
@@ -0,0 +1,115 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = 3.14159265358979323846;
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+
+//int dx[4] = { 1,0,-1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+void solve(int tc) {
+
+    string s;
+    cin >> s;
+    int isbn = s.back() - '0';
+
+    bool b = false; // *3
+
+    int sum = 0;
+
+    for (int i = 0; i < 12; i++) {
+        if (s[i] == '*') {
+            b = i % 2;
+            continue;
+        }
+        if (i % 2) sum += 3 * (s[i] - '0');
+        else sum += (s[i] - '0');
+    }
+
+    sum %= 10;
+
+    for (int i = 0; i < 10; i++) {
+
+        int ret = sum;
+        if (b) ret += 3 * i;
+        else ret += i;
+
+        if (ret % 10 == 0 && isbn == 0) {
+            cout << i;
+            return;
+        }
+        if (ret % 10 + isbn == 10) {
+            cout << i;
+            return;
+        }
+
+    }
+
+
+
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
