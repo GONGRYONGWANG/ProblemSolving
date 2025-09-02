fb4f0761eeb8458108f0233eab447ffd680b9057
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-09-02 14:25:09 +0900

    [Gold II] Title: 멀지만 가까운 사이, Time: 488 ms, Memory: 56820 KB -BaekjoonHub

---
+++ 백준/Gold/34224. 멀지만 가까운 사이/멀지만 가까운 사이.cc
@@ -0,0 +1,112 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = 3.14159265358979323846;
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+
+vector<pii> E[500001];
+int dist[500001];
+
+void solve(int tc) {
+    
+    int N;
+    cin >> N;
+    for (int i = 0; i < N - 1; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        E[u].push_back({ v,w });
+        E[v].push_back({ u,w });
+    }
+    
+    queue<pii> q;
+    q.push({ 1,0 });
+    while (!q.empty()) {
+        auto [x, p] = q.front();
+        q.pop();
+        for (pii& e : E[x]) {
+            if (e.first == p) continue;
+            dist[e.first] = dist[x] ^ e.second;
+            q.push({ e.first,x });
+        }
+    }
+
+    ll ans = 0;
+    unordered_map<int, int> cnt;
+    for (int i = 1; i <= N; i++) {
+        ans += cnt[dist[i]];
+        cnt[dist[i]] += 1;
+    }
+
+    cout << ans;
+
+
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
