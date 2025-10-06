87597348d358d520921fd087f53b5e97875a9cd5
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-10-06 21:35:27 +0900

    [Platinum III] Title: 주유소, Time: 156 ms, Memory: 44912 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Platinum/28219. 주유소/주유소.cc
@@ -0,0 +1,121 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = 3.14159265358979323846;
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+int N, K;
+vector<int> E[200001];
+int DP[200001];
+
+void dfs(int x, int p) {
+    vector<int> v = { 0,0 };
+    for (int nx : E[x]) {
+        if (nx == p) continue;
+        dfs(nx, x);
+        v.push_back(DP[nx]);
+    }
+
+    sort(v.rbegin(), v.rend());
+    if (v[0] + v[1] + 1 >= K + 1) {
+        DP[x] = 0;
+    }
+    else {
+        DP[x] = v.front() + 1;
+    }
+}
+
+void solve(int tc) {
+
+    cin >> N >> K;
+
+    for (int i = 0; i < N - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        E[u].push_back(v);
+        E[v].push_back(u);
+    }
+
+    dfs(1, 0);
+
+    int ret = 0;
+    for (int i = 1; i <= N; i++) {
+        ret += (DP[i] == 0);
+    }
+
+    cout << ret;
+
+
+
+
+
+
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
