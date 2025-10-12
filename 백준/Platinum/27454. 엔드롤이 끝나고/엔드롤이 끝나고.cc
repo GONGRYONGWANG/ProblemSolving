b3de4b5565ec6912f2869d3b5f878039cf889bc0
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-10-12 19:01:11 +0900

    [Platinum V] Title: 엔드롤이 끝나고, Time: 264 ms, Memory: 9840 KB -BaekjoonHub

---
+++ 백준/Platinum/27454. 엔드롤이 끝나고/엔드롤이 끝나고.cc
@@ -0,0 +1,115 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = 3.14159265358979323846;
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+
+
+void solve(int tc) {
+
+    ll N, K;
+    cin >> N >> K;
+
+    ll Y; cin >> Y;
+
+    vector<ll> arr(N);
+    for (int i = 0; i < N; i++) cin >> arr[i];
+    sort(arr.begin(), arr.end());
+
+    ll ret = 0;
+    ll l = 0; ll r = K - 2;
+    for (int i = l; i <= r; i++) ret += arr[i] - arr[0];
+
+    ll ans = ret + abs(Y - arr[0]);
+    
+    for (int i = 1; i < N; i++) {
+        ret += (arr[i] - arr[i - 1]) * (i - l);
+        ret -= (arr[i] - arr[i - 1]) * (r - i + 1);
+
+        while (r != N - 1 && arr[i] - arr[l] > arr[r + 1] - arr[i]) {
+            ret -= arr[i] - arr[l];
+            ret += arr[r + 1] - arr[i];
+            l += 1; r += 1;
+        }
+
+        ans = min(ans, ret + abs(Y - arr[i]));
+    }
+
+    ret = 0;
+    for (ll& x : arr) x = abs(x - Y);
+    sort(arr.begin(), arr.end());
+
+    for (int i = 0; i < K - 1; i++) ret += arr[i];
+    ans = min(ans, ret);
+
+    cout << ans;
+
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
