402de074ee9d4b22474d3ec6618abf006c8b525f
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-10-17 21:56:18 +0900

    [Gold II] Title: 원숭이 땅을 옮기다, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1425. 원숭이 땅을 옮기다/원숭이 땅을 옮기다.cc
@@ -0,0 +1,113 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = 3.14159265358979323846;
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+
+#include<fstream>
+ifstream fin; ofstream fout;
+//#define cin fin 
+//#define cout fout
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+int N;
+pll arr[50];
+
+ll get(ll k) {
+    ll ret = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = i + 1; j < N; j++) {
+            ret = max(ret, abs(arr[i].first - arr[j].first) + abs(arr[i].second - k) + abs(arr[j].second - k));
+        }
+    }
+    return ret;
+}
+
+void solve(int tc) {
+
+    cin >> N;
+    for (int i = 0; i < N; i++) cin >> arr[i].first >> arr[i].second;
+    
+    ll l = -1e9;
+    ll r = 1e9;
+    while (r - l > 2) {
+        ll p = (l * 2 + r) / 3;
+        ll q = (l + r * 2) / 3;
+
+        if (get(p) <= get(q)) r = q;
+        else l = p;
+    }
+
+    ll ans = INF;
+    for (ll i = l; i <= r; i++) {
+        ans = min(ans, get(i));
+    }
+
+    cout << ans;
+
+
+
+    //cout << "Case #" << tc << ": " << debug << endl;
+}
+
+
+int main() {
+    //fin.open("input.txt"); fout.open("output.txt");
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
