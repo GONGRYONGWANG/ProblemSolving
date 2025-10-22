f90af977fe45f7cc1c23b688e7a8918974e329aa
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-10-22 21:08:26 +0900

    [Platinum V] Title: Tournament Wins, Time: 188 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/13983. Tournament Wins/README.md
@@ -0,0 +1,32 @@
+# [Platinum V] Tournament Wins - 13983 
+
+[문제 링크](https://www.acmicpc.net/problem/13983) 
+
+### 성능 요약
+
+메모리: 2020 KB, 시간: 188 ms
+
+### 분류
+
+수학, 확률론
+
+### 제출 일자
+
+2025년 10월 22일 21:03:39
+
+### 문제 설명
+
+<p>You are one of 2<sup>k</sup> competitors invited to enter a single elimination tournament. You are ranked rth in the published rankings. Furthermore, you know that in any match between two players, the one ranked higher will always win.</p>
+
+<p>The only source of uncertainty is the bracket. If every possible tournament bracket is equally likely, determine your expected number of wins in the tournament. Your expected number of wins is the average number of your wins over all possible tournament bracket orderings.</p>
+
+### 입력 
+
+ <p>The input consists of a single line containing the two space-separated integers k (1 ≤ k ≤ 20) and r (1 ≤ r ≤ 2<sup>k</sup>).</p>
+
+### 출력 
+
+ <p>Print, on a single line, your expected number of wins in the tournament, rounded and displayed to exactly five decimal places. The sixth digit after the decimal point of the exact answer will never be 4 or 5 (eliminating complex rounding considerations).</p>
+
+<p>Be careful about very small or very large numbers during intermediate steps.</p>
+
---
+++ 백준/Platinum/13983. Tournament Wins/Tournament Wins.cc
@@ -0,0 +1,92 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = 3.14159265358979323846;
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+#include<fstream>
+ifstream fin; ofstream fout;
+
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+
+void solve(int tc) {
+   
+    int k,r;
+    cin>>k>>r;
+    int N = (1<<k);
+    
+    ld ret = ld(N-r)/(N-1);
+    ld ans =ret;
+    for(int i=2; N-r-(1<<i) +2 >0 ; i++){
+        for(int j= (1<<(i-1)); j<(1<<i); j++){
+            ret = ret * (N-r-j+1)/ (N-j);
+        }
+        ans += ret;
+    }
+    
+    cout<<fixed;
+    cout.precision(5);
+    cout<<ans;
+
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    //fin.open("input.txt"); fout.open("output.txt");
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
