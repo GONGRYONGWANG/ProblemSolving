b5cd408b8cda6dce201dcc9e92666facbc457e36
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-11-06 20:03:57 +0900

    [Platinum IV] Title: 가희와 신칸센 2, Time: 1528 ms, Memory: 132804 KB -BaekjoonHub

---
+++ 백준/Platinum/34676. 가희와 신칸센 2/가희와 신칸센 2.cc
@@ -0,0 +1,270 @@
+//구현 2. mmap/write 이용
+#pragma GCC optimize("O3")
+#pragma GCC target("avx2")
+#include <bits/stdc++.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
+using namespace std;
+
+/////////////////////////////////////////////////////////////////////////////////////////////
+/*
+ * Author : jinhan814
+ * Date : 2021-05-06
+ * Source : https://blog.naver.com/jinhan814/222266396476
+ * Description : FastIO implementation for cin, cout. (mmap ver.)
+ */
+constexpr int SZ = 1 << 20;
+
+class INPUT {
+private:
+    char* p;
+    bool __END_FLAG__, __GETLINE_FLAG__;
+public:
+    explicit operator bool() { return !__END_FLAG__; }
+    INPUT() {
+        struct stat st; fstat(0, &st);
+        p = (char*)mmap(0, st.st_size, PROT_READ, MAP_SHARED, 0, 0);
+    }
+    bool IsBlank(char c) { return c == ' ' || c == '\n'; }
+    bool IsEnd(char c) { return c == '\0'; }
+    char _ReadChar() { return *p++; }
+    char ReadChar() {
+        char ret = _ReadChar();
+        for (; IsBlank(ret); ret = _ReadChar());
+        return ret;
+    }
+    template<typename T> T ReadInt() {
+        T ret = 0; char cur = _ReadChar(); bool flag = 0;
+        for (; IsBlank(cur); cur = _ReadChar());
+        if (cur == '-') flag = 1, cur = _ReadChar();
+        for (; !IsBlank(cur) && !IsEnd(cur); cur = _ReadChar()) ret = 10 * ret + (cur & 15);
+        if (IsEnd(cur)) __END_FLAG__ = 1;
+        return flag ? -ret : ret;
+    }
+    string ReadString() {
+        string ret; char cur = _ReadChar();
+        for (; IsBlank(cur); cur = _ReadChar());
+        for (; !IsBlank(cur) && !IsEnd(cur); cur = _ReadChar()) ret.push_back(cur);
+        if (IsEnd(cur)) __END_FLAG__ = 1;
+        return ret;
+    }
+    double ReadDouble() {
+        string ret = ReadString();
+        return stod(ret);
+    }
+    string getline() {
+        string ret; char cur = _ReadChar();
+        for (; cur != '\n' && !IsEnd(cur); cur = _ReadChar()) ret.push_back(cur);
+        if (__GETLINE_FLAG__) __END_FLAG__ = 1;
+        if (IsEnd(cur)) __GETLINE_FLAG__ = 1;
+        return ret;
+    }
+    friend INPUT& getline(INPUT& in, string& s) { s = in.getline(); return in; }
+} _in;
+
+class OUTPUT {
+private:
+    char write_buf[SZ];
+    int write_idx;
+public:
+    ~OUTPUT() { Flush(); }
+    explicit operator bool() { return 1; }
+    void Flush() {
+        write(1, write_buf, write_idx);
+        write_idx = 0;
+    }
+    void WriteChar(char c) {
+        if (write_idx == SZ) Flush();
+        write_buf[write_idx++] = c;
+    }
+    template<typename T> int GetSize(T n) {
+        int ret = 1;
+        for (n = n >= 0 ? n : -n; n >= 10; n /= 10) ret++;
+        return ret;
+    }
+    template<typename T> void WriteInt(T n) {
+        int sz = GetSize(n);
+        if (write_idx + sz >= SZ) Flush();
+        if (n < 0) write_buf[write_idx++] = '-', n = -n;
+        for (int i = sz; i --> 0; n /= 10) write_buf[write_idx + i] = n % 10 | 48;
+        write_idx += sz;
+    }
+    void WriteString(string s) { for (auto& c : s) WriteChar(c); }
+    void WriteDouble(double d) { WriteString(to_string(d)); }
+} _out;
+
+/* operators */
+INPUT& operator>> (INPUT& in, char& i) { i = in.ReadChar(); return in; }
+INPUT& operator>> (INPUT& in, string& i) { i = in.ReadString(); return in; }
+template<typename T, typename std::enable_if_t<is_arithmetic_v<T>>* = nullptr>
+INPUT& operator>> (INPUT& in, T& i) {
+    if constexpr (is_floating_point_v<T>) i = in.ReadDouble();
+    else if constexpr (is_integral_v<T>) i = in.ReadInt<T>(); return in; }
+
+OUTPUT& operator<< (OUTPUT& out, char i) { out.WriteChar(i); return out; }
+OUTPUT& operator<< (OUTPUT& out, string i) { out.WriteString(i); return out; }
+template<typename T, typename std::enable_if_t<is_arithmetic_v<T>>* = nullptr>
+OUTPUT& operator<< (OUTPUT& out, T i) {
+    if constexpr (is_floating_point_v<T>) out.WriteDouble(i);
+    else if constexpr (is_integral_v<T>) out.WriteInt<T>(i); return out; }
+
+/* macros */
+#define fastio 1
+#define cin _in
+#define cout _out
+#define istream INPUT
+#define ostream OUTPUT
+/////////////////////////////////////////////////////////////////////////////////////////////
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+// AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = acosl(-1);
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//#define cin fin 
+//#define cout fout
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+bool arr[2000002];
+set<int> land;
+
+int root[2000002];
+int sz[2000002];
+set<pii> st;
+int findp(int x){
+    if(root[x]==x) return x;
+    return root[x] = findp(root[x]);
+}
+
+void update(int x){
+    arr[x] = true;
+    sz[x] = 1;
+    root[x] = x;
+    if(arr[x+1]){
+        sz[x]+=sz[x+1];
+        st.erase({sz[x+1],x+1});
+        root[x+1]=x;
+    }
+    if(arr[x-1]){
+        int r = findp(x-1);
+        st.erase({sz[r],r});
+        sz[r] += sz[x];
+        root[x] = r;
+    }
+    st.insert({sz[findp(x)], findp(x)});
+}
+
+void solve(int tc) {
+    
+    int N; cin>>N;
+    
+    for(int i=1; i<=N;i++){
+        char c; cin>>c;
+        int x = c-'0';
+        if(x==0){
+            land.insert(i);
+        }
+        else if(x==1) update(i);
+    }
+
+    int Q; cin>>Q;
+    while(Q--){
+        int t; cin>>t;
+        if(t==1){
+            
+            int s,e;
+            cin>>s>>e;
+            e-=1;
+            
+            auto it = land.lower_bound(s);
+            while(it!=land.end() && *it <= e){
+                update(*it);
+                it++;
+                land.erase(prev(it));
+            }
+            
+        }
+        else{
+            if(st.empty()){
+                cout<<-1<<endl;
+                continue;
+            }
+            else{
+                cout<<st.size()<<" ";
+                int mx = st.rbegin()->first;
+                auto it = st.lower_bound({mx,0});
+                cout<<it->second<<" "<<it->second+it->first<<" ";
+                cout<<st.begin()->second<<" "<<st.begin()->second + st.begin()->first<<endl;
+            }
+        }
+        
+    }
+    
+
+
+    // cout << "Case #" << tc << ": " << ret<< endl;
+}
+
+int main() {
+    //fin.open("input.txt"); fout.open("output.txt");
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
