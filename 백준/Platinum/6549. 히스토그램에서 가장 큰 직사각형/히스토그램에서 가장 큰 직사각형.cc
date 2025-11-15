3f402d44152202881da120713ef4ab5347168faf
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-11-15 11:09:34 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 56 ms, Memory: 4948 KB -BaekjoonHub

--- 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.cc
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.cc
@@ -1,46 +1,125 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
 #include<iostream>
-using namespace std;
-#define endl "\n"
-
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
 #include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = acosl(-1);
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
 
-int n;                     //      i
-long long height[100001];  //0 2 2 1 2 2
-						   //0 1 
-long long HG() {
-	long long h;
-	height[0] = 0;
-	for (int i = 1; i <= n; i++) {
-		cin >> h;
-		height[i] = h;
-	}
-	stack<int> S;
-	S.push(0);
-	long long weight;
-	long long result = 0;
-	for (int i = 1; i <= n; i++) {
-		while (height[i] < height[S.top()]) {
-			h = height[S.top()];
-			S.pop();
-			weight = h * (i - S.top() - 1);
-			if (weight > result) { result = weight; }
-		}
-		S.push(i);
-	}
-	while (S.top() != 0) {
-		h = height[S.top()];
-		S.pop();
-		weight = h * (n - S.top());
-		if (weight > result) { result = weight; }
-	}
+#include<fstream>
+ifstream fin; ofstream fout;
 
-	return result;
+//#define cin fin 
+//#define cout fout
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+
+int parent[100002];
+int findp(int x) {
+    if (parent[x] == x) return x;
+    return parent[x] = findp(parent[x]);
 }
+ll sz[100001];
+
+void solve(int tc){
+
+    while(1) {
+        int N;
+        cin >> N;
+        if (N == 0) break;
+        vector<pll> arr(N);
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i].first;
+            arr[i].second = i + 1;
+            sz[i + 1] = 1;
+            parent[i + 1] = 0;
+        }
+        parent[0] = parent[N + 1] = 0;
+
+        sort(arr.rbegin(), arr.rend());
+
+        ll ret = 0;
+        for (auto& [h, i] : arr) {
+            parent[i] = i;
+            if (parent[i + 1]) {
+                parent[i + 1] = i;
+                sz[i] += sz[i + 1];
+            }
+            if (parent[i - 1]) {
+                parent[i] = findp(i - 1);
+                sz[findp(i - 1)] += sz[i];
+            }
+
+            ret = max(ret, h * sz[findp(i)]);
+        }
+
+        cout << ret << endl;
+    }
+
+
+
+
+
+
+
+    // cout << "Case #" << tc << ": " << ret<< endl;
+}
+
 int main() {
-	while (1) {
-		cin >> n;
-		if (n == 0) { return 0; }
-		cout << HG() << endl;
-	}
-	return 0;
+    //fin.open("input.txt"); fout.open("output.txt");
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
 }
