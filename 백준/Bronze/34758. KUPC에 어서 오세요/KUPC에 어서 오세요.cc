5b473329b85e6d7f65aaa360b122b76b99ddc033
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-11-26 18:42:24 +0900

    [Bronze III] Title: KUPC에 어서 오세요, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34758. KUPC에 어서 오세요/KUPC에 어서 오세요.cc
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cin.tie(NULL); 
+    ios_base::sync_with_stdio(false);
+    int X,Y,N;
+    cin>>X>>Y>>N;
+    while(N--){
+        int x,y;
+        cin>>x>>y;
+        cout<<(x!=X&&y!=Y)<<"\n";
+      
+}
+  
+
+
+    return 0;
+}
