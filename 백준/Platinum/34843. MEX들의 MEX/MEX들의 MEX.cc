45ef629746f44ee3a04f1118dc46bc5fe3572db9
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-12-01 23:23:16 +0900

    [Platinum III] Title: MEX들의 MEX, Time: 240 ms, Memory: 18428 KB -BaekjoonHub

---
+++ 백준/Platinum/34843. MEX들의 MEX/MEX들의 MEX.cc
@@ -0,0 +1,127 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = acosl(-1);
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//#define cin fin 
+//#define cout fout
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+int arr[251];
+int DP[251][23];
+int ret[(1 << 22)];
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++) cin >> arr[i];
+
+    for (int i = 1; i <= N; i++) {
+        for (int k = 0; k <= 22; k++) DP[i][k] = inf;
+        vector<bool> v(23, false);
+        for (int j = i; j <= N; j++) {
+            if (arr[j] <= 22) v[arr[j]] = true;
+            for (int k = 0; k <= 22; k++) {
+                if (!v[k]) {
+                    DP[i][k] = min(DP[i][k], j);
+                    break;
+                }
+            }
+        }
+    }
+
+    for (int i = N-1; i >= 1; i--) {
+        for (int k = 0; k <= 22; k++) {
+            DP[i][k] = min(DP[i][k], DP[i + 1][k]);
+        }
+    }
+
+    for (int mask = 0; mask < (1 << 22); mask++) ret[mask] = inf;
+    ret[0] = 0;
+
+    for (int mask = 0; mask < (1 << 22); mask++) {
+        if (ret[mask] == inf || ret[mask] == N) continue;
+        for (int i = 0; i < 22; i++) {
+            ret[mask | (1 << i)] = min(ret[mask | (1 << i)], DP[ret[mask] + 1][i]);
+        }
+    }
+
+    int x = 0;
+    for (int i = 0; i < 22; i++) {
+        x += (1 << i);
+        if (ret[x] == inf) {
+            cout << i;
+            return;
+        }
+    }
+    cout << 22;
+
+    // cout << "Case #" << tc << ": " << ret << endl;
+}
+
+
+int main() {
+    // fin.open("input.txt"); fout.open("output.txt");
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    // cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
