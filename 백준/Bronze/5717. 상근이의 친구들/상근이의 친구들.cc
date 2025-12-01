04989036a8da90bcef5cbc4b7aeeec6e9a59df6a
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-12-01 12:49:45 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    cin.tie(NULL); ios_base::sync_with_stdio(false);
+    while(1){
+        int a,b;
+        cin>>a>>b;
+        if(a==0 && b==0) break;
+        cout<<a+b<<endl;
+    }
+    
+    
+    
+    
+}
