d1bb5cf0a33bc72835ee183a619e6b319f03fe49
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-12-03 22:12:54 +0900

    [Gold I] Title: A+를 향하여, Time: 4 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Gold/34806. A＋를 향하여/A＋를 향하여.cc
@@ -0,0 +1,118 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = acosl(-1);
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//#define cin fin 
+//#define cout fout
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+
+
+void solve(int tc) {
+
+    int N, T, W;
+    cin >> N >> T >> W;
+
+    vector<pii> arr(N);
+    for (auto& [t, w] : arr) cin >> t >> w;
+
+    int l = 0; int r = T+1;
+    while (l < r) {
+        int m = (l + r) / 2;
+        ll ret = 0;
+        vector<ll> DP(T - m + 1, 0);
+        for (auto& [t, w] : arr) {
+            if (t <= m) {
+                ret += w;
+                continue;
+            }
+
+            for (int i = T - m; i >= t-m; i--) {
+                DP[i] = max(DP[i], DP[i - (t - m)] + w);
+            }
+        }
+
+        ret += DP[T - m];
+
+        if (ret >= W) r = m;
+        else l = m + 1;
+    }
+
+    if (l == T + 1) cout << -1;
+    else cout << l;
+
+
+
+
+
+
+
+    // cout << "Case #" << tc << ": " << ret << endl;
+}
+
+
+int main() {
+    // fin.open("input.txt"); fout.open("output.txt");
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
