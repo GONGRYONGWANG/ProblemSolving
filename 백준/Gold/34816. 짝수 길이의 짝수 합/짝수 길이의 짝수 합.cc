1c4c39c210c2fa322e45ccd07f14b06ca09ac300
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-12-08 00:37:37 +0900

    [Gold V] Title: 짝수 길이의 짝수 합, Time: 84 ms, Memory: 2316 KB -BaekjoonHub

---
+++ 백준/Gold/34816. 짝수 길이의 짝수 합/짝수 길이의 짝수 합.cc
@@ -0,0 +1,121 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = acosl(-1);
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//#define cin fin 
+//#define cout fout
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+
+bool arr[300001];
+
+void solve(int tc) {
+
+    int N, Q;
+    cin >> N >> Q;
+    for (int i = 1; i <= N; i++) {
+        char x; cin >> x;
+        arr[i] = (x == '1');
+    }
+
+    while (Q--) {
+        int t;
+        cin >> t;
+        if (t == 1) {
+            int i; cin >> i;
+            arr[i] = !arr[i];
+        }
+        else {
+            int x, y;
+            cin >> x >> y;
+            if (x == y) {
+                cout << "NO" << endl;
+                continue;
+            }
+            if (x + 1 == y && arr[x] != arr[y]) {
+                cout << "NO" << endl;
+                continue;
+            }
+            if (x + 2 == y && arr[x] != arr[x + 1] && arr[x] == arr[x + 2]) {
+                cout << "NO" << endl;
+                continue;
+            }
+            cout << "YES" << endl;
+        }
+    }
+
+
+
+
+
+
+
+    // cout << "Case #" << tc << ": " << ret << endl;
+}
+
+
+int main() {
+    // fin.open("input.txt"); fout.open("output.txt");
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
