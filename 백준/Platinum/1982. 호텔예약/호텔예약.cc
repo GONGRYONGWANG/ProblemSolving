05fff70478296582a534daa94c645a86db2f2ae4
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-12-14 00:12:19 +0900

    [Platinum IV] Title: 호텔예약, Time: 16 ms, Memory: 6048 KB -BaekjoonHub

--- 백준/Platinum/1982. 호텔예약/호텔예약.cc
+++ 백준/Platinum/1982. 호텔예약/호텔예약.cc
@@ -68,6 +68,7 @@ void solve(int tc) {
     int m, f, r, c;
     cin >> m >> f >> r >> c;
 
+    c = min(c, 1);
     for (int i = 0; i <= m; i++) {
         for (int j = 0; j <= f; j++) {
             for (int k = 0; k <= c; k++) {
