20bd31c47fcaebed8f7d280fd9f72066990bbcd5
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2025-12-14 02:25:21 +0900

    [Gold I] Title: 팰린드롬 만들기, Time: 28 ms, Memory: 99736 KB -BaekjoonHub

---
+++ 백준/Gold/16473. 팰린드롬 만들기/팰린드롬 만들기.cc
@@ -0,0 +1,105 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = acosl(-1);
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//#define cin fin 
+//#define cout fout
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+
+
+int DP[5001][5001];
+int arr[5001];
+
+void solve(int tc) {
+
+
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; i++) cin >> arr[i];
+
+    for (int i = 2; i <= N; i++) {
+        for (int j = N - 1; j >= i; j--) {
+            DP[i][j] = max(DP[i - 1][j], DP[i][j + 1]);
+            if (arr[i - 1] == arr[j + 1]) DP[i][j] = max(DP[i][j], 1 + DP[i - 1][j + 1]);
+        }
+    }
+
+    int T;
+    cin >> T;
+    while (T--) {
+        int M; cin >> M;
+        cout << N - (2 * DP[M][M] + 1) << endl;
+    }
+
+
+
+    // cout << "Case #" << tc << ": " << ret << endl;
+}
+
+
+int main() {
+    //fin.open("input.txt"); fout.open("output.txt");
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
