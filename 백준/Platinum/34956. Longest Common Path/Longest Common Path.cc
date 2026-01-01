bb4b88c5ebc85fb6f34bc74c5ef1a76c10954e06
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-01-01 23:50:55 +0900

    [Platinum V] Title: Longest Common Path, Time: 128 ms, Memory: 14096 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Platinum/34956. Longest Common Path/Longest Common Path.cc
@@ -0,0 +1,155 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = acosl(-1);
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//#define cin fin 
+//#define cout fout
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+vector<int> E[100001];
+bool visited[100001];
+int dist[100001];
+int root[100001];
+
+void diameter(int r) {
+
+    queue<int> q;
+    q.push(r);
+    int x = r;
+    while (!q.empty()) {
+        x = q.front();
+        q.pop();
+        visited[x] = true;
+        for (int nx : E[x]) {
+            if (visited[nx]) continue;
+            q.push(nx);
+        }
+    }
+
+    int s = x;
+    q.push(s);
+    while (!q.empty()) {
+        x = q.front();
+        q.pop();
+        root[x] = s;
+        for (int nx : E[x]) {
+            if (root[nx]) continue;
+            dist[nx] = dist[x] + 1;
+            q.push(nx);
+        }
+    }
+
+}
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+    
+    set<pii> st;
+    for (int i = 0; i < N - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        if (u > v) swap(u, v);
+        st.insert({ u,v });
+    }
+
+    for (int i = 0; i < N - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        if (u > v) swap(u, v);
+        if (!st.count({ u,v })) continue;
+        E[u].push_back(v);
+        E[v].push_back(u);
+    }
+
+
+    for (int i = 1; i <= N; i++) {
+        if (visited[i]) continue;
+        diameter(i);
+    }
+
+    int idx = 0;
+    for (int i = 1; i <= N; i++) {
+        if (dist[i] > dist[idx]) idx = i;
+    }
+
+    if (idx == 0) {
+        cout << -1;
+        return;
+    }
+
+    cout << idx << " " << root[idx];
+
+
+
+
+    // cout << "Case #" << tc << ": " << ret << endl;
+}
+
+
+int main() {
+    //fin.open("input.txt"); fout.open("output.txt");
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    // cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
