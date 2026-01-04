00c7e0e37debc50e55d23cbfead71a5df3e3e62b
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-01-04 20:04:16 +0900

    [Gold III] Title: 낚시 (Easy), Time: 76 ms, Memory: 7560 KB, Score: 16 point -BaekjoonHub

---
+++ 백준/Gold/34954. 낚시 （Easy）/낚시 （Easy）.cc
@@ -0,0 +1,120 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = acosl(-1);
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//#define cin fin 
+//#define cout fout
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+
+
+void solve(int tc) {
+
+    int N,M;
+    cin >> N >> M;
+    vector<ll> arr(N);
+    for (ll& x : arr) cin >> x;
+    sort(arr.begin(), arr.end());
+
+    multiset<ll> pool;
+    while (M--) {
+        ll x; cin >> x;
+        pool.insert(x);
+    }
+    
+
+    multiset<ll> fish;
+    for (ll x : arr) {
+        auto it = pool.upper_bound(x);
+        if (it == pool.end()) continue;
+        fish.insert(*it);
+        pool.erase(it);
+    }
+
+    if (fish.empty()) {
+        cout << 0 << endl << 0;
+        return;
+    }
+
+    while (!pool.empty() && !fish.empty() && *fish.begin() < *pool.rbegin()) {
+        ll x = *fish.begin();
+        fish.erase(fish.begin());
+        auto it = pool.upper_bound(x);
+        fish.insert(*it);
+        pool.erase(it);
+    }
+
+    ll ans = 0;
+    for (ll x : fish) ans += x;
+    cout << ans;
+
+
+    // cout << "Case #" << tc << ": " << ret << endl;
+}
+
+
+int main() {
+    //fin.open("input.txt"); fout.open("output.txt");
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    // cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
