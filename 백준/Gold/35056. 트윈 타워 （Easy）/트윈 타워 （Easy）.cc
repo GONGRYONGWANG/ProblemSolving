4fbf849ab230b3439e6a1e99f1f2ad357477f615
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-01-07 22:47:22 +0900

    [Gold III] Title: 트윈 타워 (Easy), Time: 200 ms, Memory: 2356 KB -BaekjoonHub

---
+++ 백준/Gold/35056. 트윈 타워 （Easy）/트윈 타워 （Easy）.cc
@@ -0,0 +1,113 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = acosl(-1);
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//#define cin fin 
+//#define cout fout
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+ll A[2001];
+ll B[2001];
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+
+    for (int i = 1; i <= N; i++) cin >> A[i];
+    for (int i = 1; i < N; i++) cin >> B[i];
+
+    vector<vector<ll>> DP(N + 1, vector<ll>(3, -INF));
+    DP[0][0] = 0;
+    for (int i = 1; i <= N; i++) {
+        vector<vector<ll>> nx(N + 1, vector<ll>(3, -INF));
+        for (int j = 0; j <= N; j++) {
+            for (int k = 0; k < 3; k++) {
+                nx[j][0] = max(nx[j][0], DP[j][k]);
+                if (j != N) nx[j + 1][1] = max(nx[j + 1][1], DP[j][k] + A[i]);
+                if (j != N && k == 1) nx[j + 1][2] = max(nx[j + 1][2], DP[j][k] + A[i] + B[i - 1]);
+            }
+        }
+        DP = nx;
+    }
+
+    ll ret = -INF;
+    for (int j = 1; j <= N; j++) {
+        for (int k = 0; k < 3; k++) {
+            ret = max(ret, DP[j][k]);
+        }
+        cout << ret << endl;
+    }
+
+    
+
+
+    // cout << "Case #" << tc << ": " << ret << endl;
+}
+
+
+int main() {
+    //fin.open("input.txt"); fout.open("output.txt");
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    // cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
