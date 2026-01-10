ef7ca551e08ecb839de99220c6362c80a2330e4c
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-01-11 01:36:10 +0900

    [Platinum II] Title: 트리 초기화, Time: 48 ms, Memory: 15228 KB -BaekjoonHub

---
+++ 백준/Platinum/34715. 트리 초기화/트리 초기화.cc
@@ -0,0 +1,131 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = acosl(-1);
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//#define cin fin 
+//#define cout fout
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+
+ll DP[100001];
+vector<int> E[100001];
+int parent[100001];
+bool down[100001];
+bool up[100001];
+
+int ret = 0;
+void dfs(int x) {
+
+    int upcnt = 0;
+    for (int nx : E[x]) {
+        if (nx == parent[x]) continue;
+        parent[nx] = x;
+        dfs(nx);
+        DP[x] += DP[nx];
+        up[x] |= up[nx];
+        upcnt += up[nx];
+    }
+    for (int nx : E[x]) {
+        if (nx == parent[x]) continue;
+        down[x] |= (down[nx] && !(upcnt - up[nx]));
+    }
+
+    if (DP[x] > 0 && !up[x]) {
+        ret += 1;
+        up[x] = true;
+        down[x] = false;
+    }
+    down[x] |= (DP[x] < 0);
+}
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+
+    for (int i = 1; i <= N; i++) cin >> DP[i];
+
+    for (int i = 0; i < N - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        E[u].push_back(v);
+        E[v].push_back(u);
+    }
+
+    dfs(1);
+    if (DP[1] != 0) {
+        cout << -1;
+        return;
+    }
+    cout << ret + down[1];
+
+
+    // cout << "Case #" << tc << ": " << ret << endl;
+}
+
+
+int main() {
+    //fin.open("input.txt"); fout.open("output.txt");
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    // cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
