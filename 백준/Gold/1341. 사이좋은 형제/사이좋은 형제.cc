ee9d068fabcaa2cf49c48275128182550c17a906
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-01-18 21:53:39 +0900

    [Gold III] Title: 사이좋은 형제, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1341. 사이좋은 형제/사이좋은 형제.cc
@@ -0,0 +1,116 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = acosl(-1);
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//#define cin fin 
+//#define cout fout
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+
+void solve(int tc) {
+
+    ull a, b;
+    cin >> a >> b;
+
+
+    for (int i = 1; i <= 60; i++) {
+        ull total = (1ull << i) - 1;
+        if (b > total) continue;
+        if (total % b) continue;
+
+        ull x = total / b * a;
+        ull y = total / b * (b - a);
+
+        string ret;
+        for (int j = 0; j < i; j++) {
+            if ((x & (1ull << j)) ^ (y & (1ull << j))) {
+                if (x & (1ull << j)) ret.push_back('*');
+                else ret.push_back('-');
+            }
+            else {
+                ret.clear();
+                break;
+            }
+        }
+
+        if (ret.empty()) continue;
+        reverse(ret.begin(), ret.end());
+        cout << ret;
+        return;
+    }
+
+    cout << -1;
+
+
+
+
+
+    // cout << "Case #" << tc << ": " << ret << endl;
+}
+
+
+int main() {
+    //fin.open("input.txt"); fout.open("output.txt");
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    // cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
