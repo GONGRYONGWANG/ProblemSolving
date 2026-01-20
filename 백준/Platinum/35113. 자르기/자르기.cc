eb77250f101a2b155d9fa5f0ba5abac7165fa10c
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-01-20 21:37:22 +0900

    [Platinum V] Title: 자르기, Time: 128 ms, Memory: 13740 KB -BaekjoonHub

---
+++ 백준/Platinum/35113. 자르기/자르기.cc
@@ -0,0 +1,105 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = acosl(-1);
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//#define cin fin 
+//#define cout fout
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+int arr[1000000];
+int premx[1000000];
+int sufmx[1000000];
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) cin >> arr[i];
+
+    premx[0] = arr[0];
+    for (int i = 1; i < N; i++) {
+        premx[i] = max(premx[i - 1], arr[i]);
+    }
+
+    sufmx[N - 1] = arr[N - 1];
+    for (int i = N - 2; i >= 0; i--) {
+        sufmx[i] = max(sufmx[i+1], arr[i]);
+    }
+    
+    int ret = inf;
+    for (int i = 1; i < N - 2; i++) {
+        ret = min(ret, premx[i] + sufmx[i + 1] + min(arr[0], arr[i]) + min(arr[i + 1], arr[N - 1]));
+    }
+
+    cout << ret;
+
+    // cout << "Case #" << tc << ": " << ret << endl;
+}
+
+
+int main() {
+    //fin.open("input.txt"); fout.open("output.txt");
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
