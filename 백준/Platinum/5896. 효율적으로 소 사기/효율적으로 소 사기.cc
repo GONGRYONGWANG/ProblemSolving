bd24f3a29c579d9673e550f9c8f0ad723a5ef3b3
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-01-23 00:08:23 +0900

    [Platinum II] Title: 효율적으로 소 사기, Time: 12 ms, Memory: 3076 KB -BaekjoonHub

---
+++ 백준/Platinum/5896. 효율적으로 소 사기/효율적으로 소 사기.cc
@@ -0,0 +1,134 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<random> // rand
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = acosl(-1);
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//#define cin fin 
+//#define cout fout
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+
+struct cmp {
+    bool operator()(pll& a, pll& b) {
+        return a.second > b.second;
+    }
+};
+
+void solve(int tc) {
+
+    ll N, K, M;
+    cin >> N >> K >> M;
+
+    vector<pll> arr(N);
+    for (auto& [c, p] : arr) cin >> p >> c;
+
+    sort(arr.begin(), arr.end());
+
+    pq<pll, vector<pll>, cmp > q;
+
+    int ret = 0;
+    for (int i = 0; i < K; i++) {
+        if (arr[i].first <= M) {
+            M -= arr[i].first;
+            q.push(arr[i]);
+            ret += 1;
+        }
+    }
+
+    if (q.empty()) {
+        cout << 0;
+        return;
+    }
+
+    for (int i = K; i < N; i++) {
+        auto& [c, p] = arr[i];
+        if (min(p, q.top().second - q.top().first + c) > M) continue;
+        ret += 1;
+        if (p <= q.top().second - q.top().first + c) {
+            M -= p;
+        }
+        else {
+            M -= q.top().second - q.top().first + c;
+            q.pop();
+            q.push({ c,p });
+        }
+    }
+
+    cout << ret;
+
+
+
+
+
+
+
+
+
+    // cout << "Case #" << tc << ": " << ret << endl;
+}
+
+
+int main() {
+    //fin.open("input.txt"); fout.open("output.txt");
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
