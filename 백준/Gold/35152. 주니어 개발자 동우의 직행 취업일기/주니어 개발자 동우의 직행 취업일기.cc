067f0dc2683e8e47625d7cf30485c061984f2f20
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-01-27 23:33:21 +0900

    [Gold V] Title: 주니어 개발자 동우의 직행 취업일기, Time: 64 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Gold/35152. 주니어 개발자 동우의 직행 취업일기/주니어 개발자 동우의 직행 취업일기.cc
@@ -0,0 +1,113 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = acosl(-1);
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+
+#include<random> // rand
+std::mt19937 gen;
+void rng_init() {
+    std::random_device rd;
+    gen.seed(rd());  // main에서 1번
+}
+int randint(int l, int r) {
+    std::uniform_int_distribution<int> dist(l, r);
+    return dist(gen);
+}
+
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//#define cin fin 
+//#define cout fout
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+
+    if (N % 2) {
+        cout << 6 + N / 2 << endl;
+        cout << "1 2" << endl << "2 5" << endl << "5 6" << endl << "4 5" << endl << "3 4" << endl;
+        for (int i = 0; i < N / 2; i++) {
+            cout << 6 << " " << 7 + i << endl;
+        }
+        return;
+    }
+
+    cout << 4 + N / 2 << endl;
+    cout << "1 2" << endl << "2 3" << endl << "2 4" << endl;
+    for (int i = 0; i < N / 2; i++) {
+        cout << 4 << " " << 5 + i << endl;
+    }
+
+
+
+
+
+    // cout << "Case #" << tc << ": " << ret << endl;
+}
+
+
+int main() {
+    //fin.open("input.txt"); fout.open("output.txt");
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    rng_init();
+    int T = 1;
+    // cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
