418fe8693f8fda35b6995e888dfeb7a6d34dc706
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-01-31 21:20:32 +0900

    [Gold IV] Title: 누가 이름 안 적고 나갔어, Time: 648 ms, Memory: 81316 KB -BaekjoonHub

---
+++ 백준/Gold/33578. 누가 이름 안 적고 나갔어/누가 이름 안 적고 나갔어.cc
@@ -0,0 +1,158 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = acosl(-1);
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+
+#include<random> // rand
+std::mt19937 gen;
+void rng_init() {
+    std::random_device rd;
+    gen.seed(rd());  // main에서 1번
+}
+int randint(int l, int r) {
+    std::uniform_int_distribution<int> dist(l, r);
+    return dist(gen);
+}
+
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//#define cin fin 
+//#define cout fout
+
+int dx[4] = { -1,0,1,0 };
+int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+char board[3001][3001];
+int distJ[3001][3001];
+int distS[3001][3001];
+
+void solve(int tc) {
+
+    int N, M;
+    cin >> N >> M;
+
+    pii S, J;
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= M; j++) {
+            char x; cin >> x;
+            board[i][j] = x;
+            if (x == 'S') S = { i,j };
+            else if (x == 'J') J = { i,j };
+        }
+    }
+
+    queue<pii> q;
+
+    q.push(J);
+    distJ[J.first][J.second] = 1;
+    while (!q.empty()) {
+        auto [x, y] = q.front();
+        q.pop();
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+            if (nx <= 0 || nx > N || ny <= 0 || ny > M) continue;
+            if (distJ[nx][ny]) continue;
+            if (board[nx][ny] == '#') continue;
+            distJ[nx][ny] = distJ[x][y] + 1;
+            q.push({ nx,ny });
+        }
+    }
+
+    q.push(S);
+    distS[S.first][S.second] = 1;
+    while (!q.empty()) {
+        auto [x, y] = q.front();
+        q.pop();
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+            if (nx <= 0 || nx > N || ny <= 0 || ny > M) continue;
+            if (distS[nx][ny]) continue;
+            if (board[nx][ny] == '#') continue;
+            distS[nx][ny] = distS[x][y] + 1;
+            q.push({ nx,ny });
+        }
+    }
+
+    if (!distJ[S.first][S.second]) {
+        cout << -1;
+        return;
+    }
+
+    int ret = (distJ[S.first][S.second]-1) * 2;
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= M; j++) {
+            if (!distJ[i][j]) continue;
+            if (board[i][j] != 'T') continue;
+            ret = min(ret, (distJ[i][j] - 1) * 2 + distS[i][j] - 1);
+        }
+    }
+
+    cout << ret;
+
+    // cout << "Case #" << tc << ": " << ret << endl;
+}
+
+
+int main() {
+    //fin.open("input.txt"); fout.open("output.txt");
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    rng_init();
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
