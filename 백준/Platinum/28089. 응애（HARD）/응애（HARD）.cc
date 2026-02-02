5e104d4a0ed8ee77b8354de82584e8158e533fce
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-02-03 00:21:42 +0900

    [Platinum II] Title: 응애(HARD), Time: 1016 ms, Memory: 4840 KB -BaekjoonHub

---
+++ 백준/Platinum/28089. 응애（HARD）/응애（HARD）.cc
@@ -0,0 +1,118 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = acosl(-1);
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+
+#include<random> // rand
+std::mt19937 gen;
+void rng_init() {
+    std::random_device rd;
+    gen.seed(rd());  // main에서 1번
+}
+int randint(int l, int r) {
+    std::uniform_int_distribution<int> dist(l, r);
+    return dist(gen);
+}
+
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//#define cin fin 
+//#define cout fout
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+
+bitset<6000000> arr;
+
+void solve(int tc) {
+
+    int N, M; ll K;
+    cin >> N >> M >> K;
+
+    while (M--) {
+        int x; cin >> x;
+        arr[x] = arr[x + N] = arr[x + 2 * N] = true;
+    }
+
+    for (int i = 0; i < 60; i++) {
+        if (K & (1ll << i)) {
+            ll sh = (1ll << i) % N;
+            arr = (arr << sh) ^ (arr >> sh);
+            for (int j = 0; j < N; j++) {
+                arr[j] = arr[j + 2 * N] = arr[j + N];
+            }
+        }
+    }
+
+    int ret = 0;
+    for (int i = N; i < 2 * N; i++) ret += arr[i];
+
+    cout << ret;
+
+
+
+    // cout << "Case #" << tc << ": " << ret << endl;
+}
+
+
+int main() {
+    //fin.open("input.txt"); fout.open("output.txt");
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    rng_init();
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
