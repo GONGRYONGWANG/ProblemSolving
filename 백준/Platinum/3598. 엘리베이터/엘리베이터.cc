a289c16a5c95c0e45b89b8be1e5e21381c4bd5d6
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-02-06 00:17:15 +0900

    [Platinum III] Title: 엘리베이터, Time: 8 ms, Memory: 2944 KB -BaekjoonHub

---
+++ 백준/Platinum/3598. 엘리베이터/엘리베이터.cc
@@ -0,0 +1,139 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = acosl(-1);
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+
+#include<random> // rand
+std::mt19937 gen;
+void rng_init() {
+    std::random_device rd;
+    gen.seed(rd());  // main에서 1번
+}
+int randint(int l, int r) {
+    std::uniform_int_distribution<int> dist(l, r);
+    return dist(gen);
+}
+
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//#define cin fin 
+//#define cout fout
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+
+
+ll visited[100000];
+
+void solve(int tc) {
+
+    ll h, a, b, c;
+    cin >> h >> a >> b >> c;
+
+    for (int i = 0; i < c; i++) {
+        visited[i] = INF;
+    }
+
+    queue<ll> A, B;
+    A.push(1);
+
+    while (!A.empty() || !B.empty()) {
+        ll x;
+        if (A.empty()) {
+            x = B.front(); B.pop();
+        }
+        else if (B.empty()) {
+            x = A.front(); A.pop();
+        }
+        else if (A.front() <= B.front()) {
+            x = A.front(); A.pop();
+        }
+        else {
+            x = B.front(); B.pop();
+        }
+
+        if (x > h) break;
+        if (visited[x % c] != INF) continue;
+        visited[x % c] = x;
+
+        if (visited[(x + a) % c] == INF) A.push(x + a);
+        if (visited[(x + b) % c] == INF) B.push(x + b);
+    }
+
+    ll ret = 0;
+    for (int i = 0; i < c; i++) {
+        if (visited[i] > h) continue;
+        ret += (h - visited[i]) / c + 1;
+    }
+
+    cout << ret;
+
+
+
+
+
+    // cout << "Case #" << tc << ": " << ret << endl;
+}
+
+
+int main() {
+    //fin.open("input.txt"); fout.open("output.txt");
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    rng_init();
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
