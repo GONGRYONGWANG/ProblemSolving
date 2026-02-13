1f9e7cf0157b19159cc4a91c2cb88f7500facaf0
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-02-13 21:37:25 +0900

    [Gold IV] Title: 사각형 개수 세기, Time: 36 ms, Memory: 2620 KB -BaekjoonHub

---
+++ 백준/Gold/34316. 사각형 개수 세기/사각형 개수 세기.cc
@@ -0,0 +1,133 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = acosl(-1);
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+
+#include<random> // rand
+std::mt19937 gen;
+void rng_init() {
+    std::random_device rd;
+    gen.seed(rd());  // main에서 1번
+}
+int randint(int l, int r) {
+    std::uniform_int_distribution<int> dist(l, r);
+    return dist(gen);
+}
+
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//#define cin fin 
+//#define cout fout
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+
+
+void solve(int tc) {
+    int N, M;
+    cin >> N >> M;
+
+    bool b = false;
+    if (N > M) {
+        b = true;
+        swap(N, M);
+    }
+
+    vector<vector<int>> arr(N, vector<int>(M));
+
+    if (!b) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                cin >> arr[i][j];
+            }
+        }
+    }
+    else {
+        for (int j = 0; j < M; j++) {
+            for (int i = 0; i < N; i++) {
+                cin >> arr[i][j];
+            }
+        }
+    }
+
+    ll ret = 0;
+    for (int a = 0; a < N; a++) {
+        for (int b = a + 1; b < N; b++) {
+            vector<int> cnt(19, 0);
+            for (int j = 0; j < M; j++) {
+                int x = arr[a][j] + arr[b][j];
+                ret += cnt[20 - x];
+                cnt[x] += 1;
+            }
+        }
+    }
+
+    cout << ret;
+
+
+
+    // cout << "Case #" << tc << ": " << ret << endl;
+}
+
+
+int main() {
+    //fin.open("input.txt"); fout.open("output.txt");
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    rng_init();
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
