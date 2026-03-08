25469f03cfc9b2d54eddf8873a1d0786b8f70738
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-03-09 00:33:34 +0900

    [Gold II] Title: unique(str)=KCPC, Time: 16 ms, Memory: 3688 KB -BaekjoonHub

---
+++ 백준/Gold/35159. unique（str）＝KCPC/unique（str）＝KCPC.cc
@@ -0,0 +1,132 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = acosl(-1);
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+
+#include<random> // rand
+std::mt19937 gen;
+void rng_init() {
+    std::random_device rd;
+    gen.seed(rd());  // main에서 1번
+}
+int randint(int l, int r) {
+    std::uniform_int_distribution<int> dist(l, r);
+    return dist(gen);
+}
+
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//#define cin fin 
+//#define cout fout
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+
+void solve(int tc) {
+
+    string s;
+    cin >> s;
+    int N = s.length();
+
+    ll ret = 0;
+
+    int totalp = 0;
+    int totalc = 0;
+
+    int kcnt = 0;
+    for (int i = 0; i < N; i++) {
+        if (s[i] == 'P') totalp += 1;
+        else if (s[i] == 'C') totalc += 1;
+        if (s[i] != 'K') continue;
+        ret += i - kcnt;
+        kcnt += 1;
+    }
+
+    int pcnt = 0; int ccnt = 0;
+    for (int i = 0; i < N; i++) {
+        char x = s[i];
+        if (s[i] == 'K') continue;
+        if (s[i] == 'P') {
+            pcnt += 1;
+            continue;
+        }
+        ccnt += 1;
+        if (ccnt == 1) {
+            ret += pcnt;
+        }
+        else if (ccnt == totalc) {
+            ret += totalp - pcnt;
+        }
+        else {
+            ret += min(pcnt, totalp - pcnt);
+        }
+    }
+
+    cout << ret;
+
+
+    // cout << "Case #" << tc << ": " << ret << endl;
+}
+
+
+int main() {
+    //fin.open("input.txt"); fout.open("output.txt");
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    rng_init();
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
