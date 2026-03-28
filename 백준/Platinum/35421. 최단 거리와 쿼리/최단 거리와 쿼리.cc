c28ee4296ba913d3545d5c61238eb3858de10f86
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-03-29 00:56:40 +0900

    [Platinum II] Title: 최단 거리와 쿼리, Time: 1520 ms, Memory: 124128 KB -BaekjoonHub

---
+++ 백준/Platinum/35421. 최단 거리와 쿼리/최단 거리와 쿼리.cc
@@ -0,0 +1,197 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = acosl(-1);
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+
+#include<random> // rand
+std::mt19937 gen;
+void rng_init() {
+    std::random_device rd;
+    gen.seed(rd());  // main에서 1번
+}
+int randint(int l, int r) {
+    std::uniform_int_distribution<int> dist(l, r);
+    return dist(gen);
+}
+
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//#define cin fin 
+//#define cout fout
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+
+int depth[100001];
+pll parent[100001][20];
+vector<pll> E[100001];
+ll distance(int x, int y) {
+    if (depth[x] < depth[y]) swap(x, y);
+    ll ret = 0;
+    int d = depth[x] - depth[y];
+    for (int j = 0; j < 20; j++) {
+        if (d & (1 << j)) {
+            ret += parent[x][j].second;
+            x = parent[x][j].first;
+        }
+    }
+    if (x == y) return ret;
+    for (int j = 19; j >= 0; j--) {
+        if (parent[x][j].first != parent[y][j].first) {
+            ret += parent[x][j].second + parent[y][j].second;
+            x = parent[x][j].first; y = parent[y][j].first;
+        }
+    }
+    return ret + parent[x][0].second + parent[y][0].second;
+}
+
+vector<ll> dist[100001];
+
+struct cmp {
+    bool operator()(pll& a, pll& b) {
+        return a.second > b.second;
+    }
+};
+
+void solve(int tc) {
+
+    int N, M;
+    cin >> N >> M;
+
+    while (M--) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        E[u].push_back({ v,w });
+        E[v].push_back({ u,w });
+    }
+    
+    queue<int> q;
+    q.push(1); depth[1] = 1; parent[1][0] = { 1,0 };
+    while (!q.empty()) {
+        int x = q.front();
+        q.pop();
+        for (auto& [nx, w] : E[x]) {
+            if (depth[nx]) continue;
+            depth[nx] = depth[x] + 1;
+            parent[nx][0] = { x,w };
+            q.push(nx);
+        }
+    }
+
+    for (int j = 1; j < 20; j++) {
+        for (int i = 1; i <= N; i++) {
+            parent[i][j].first = parent[parent[i][j - 1].first][j - 1].first;
+            parent[i][j].second = parent[i][j - 1].second + parent[parent[i][j - 1].first][j - 1].second;
+        }
+    }
+
+    vector<bool> isext(N + 1, false);
+    vector<int> ext;
+    for (int x = 1; x <= N; x++) {
+        for (auto& [nx, w] : E[x]) {
+            if (x == parent[nx][0].first || nx == parent[x][0].first) continue;
+            isext[x] = true;
+        }
+        if (isext[x]) ext.push_back(x);
+    }
+
+    for (int s : ext) {
+        dist[s].resize(N + 1, 0);
+        vector<bool> visited(N + 1, false);
+        pq<pll, vector<pll>, cmp> q;
+        q.push({ s,0 });
+        while (!q.empty()) {
+            auto [x, d] = q.top();
+            q.pop();
+            if (visited[x]) continue;
+            visited[x] = true;
+            dist[s][x] = d;
+            for (auto& [nx, w] : E[x]) {
+                if (visited[nx]) continue;
+                q.push({ nx,d + w });
+            }
+        }
+    }
+
+
+    int Q;
+    cin >> Q;
+    while (Q--) {
+        int s, e;
+        cin >> s >> e;
+        ll ret = distance(s, e);
+
+        for (int x : ext) {
+            ret = min(ret, dist[x][s] + dist[x][e]);
+        }
+
+        cout << ret << endl;
+
+    }
+
+
+    // cout << "Case #" << tc << ": " << ret << endl;
+}   
+
+
+int main() {
+    //fin.open("input.txt"); fout.open("output.txt");
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    rng_init();
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
