c992bcf597bad95f147519601505550d99693d48
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-04-03 21:03:28 +0900

    [Silver III] Title: 두 수의 합, Time: 20 ms, Memory: 7220 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.cc
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.cc
@@ -1,53 +1,109 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
 #include<iostream>
-using namespace std;
-#define endl "\n"
-//ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
-
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
 #include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
 #include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = acosl(-1);
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+
+#include<random> // rand
+std::mt19937 gen;
+void rng_init() {
+    std::random_device rd;
+    gen.seed(rd());  // main에서 1번
+}
+int randint(int l, int r) {
+    std::uniform_int_distribution<int> dist(l, r);
+    return dist(gen);
+}
+
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//#define cin fin 
+//#define cout fout
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+unordered_set<int> st;
+int arr[100000];
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; i++) cin >> arr[i];
+
+    int X; cin >> X;
+
+    int ret = 0;
+    for (int i = 0; i < N; i++) {
+        int x = arr[i];
+        ret += st.count(X - x);
+        st.insert(x);
+    }
+
+    cout << ret;
 
 
-int n;
-int main() {
-	ios_base::sync_with_stdio(false); cout.tie(NULL); cin.tie(NULL);
-	cin >> n;
-	vector<int> v;
-	int tmp;
-	for (int i = 0; i < n; i++) {
-		cin >> tmp;
-		v.push_back(tmp);
-	}
-	sort(v.begin(), v.end());
-	int p1 = 0;
-	int p2 = n - 1;
-	
-	int x;
-	cin >> x;
-	int count = 0;
-	int sum;
-	while (p1 < p2) {
-		sum = v[p1] + v[p2];
-		if (sum == x) {
-			count++; p1++; p2--; 
-		}
-		else if (sum > x) {
-			p2--;
-		}
-		else {
-			p1++;
-		}
-	}
-	cout << count;
-
-
-
-	return 0;
+    // cout << "Case #" << tc << ": " << ret << endl;
 }
 
 
+int main() {
+    //fin.open("input.txt"); fout.open("output.txt");
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    rng_init();
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
 
-
-
-
-
-
+    return 0;
+}
