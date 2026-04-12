39291de21185df53338c2ed94a6bb4738ff1e122
Jaemin Lee <63445382+GONGRYONGWANG@users.noreply.github.com>
2026-04-13 01:10:52 +0900

    [Platinum II] Title: Tri-Tree XOR, Time: 252 ms, Memory: 43340 KB -BaekjoonHub

---
+++ 백준/Platinum/34188. Tri－Tree XOR/Tri－Tree XOR.cc
@@ -0,0 +1,129 @@
+// template: https://github.com/GONGRYONGWANG/ProblemSolving/blob/main/template.txt
+#include<iostream>
+#include<iomanip>
+#include<cstdio>
+#include<string>
+#include<cctype>
+#include<vector>
+#include<array>
+#include<utility>
+#include<list>
+#include<queue>
+#include<stack>
+#include<deque>
+#include<tuple>
+#include<set>
+#include<unordered_set>
+#include<map>
+#include<unordered_map>
+#include<cmath>
+#include<algorithm>
+#include<bitset>
+#include<cstdlib>
+#include<ctime> // srand(time(0))
+#include<regex> // 정규표현식
+#include<complex> // complex number
+#include<numeric>
+#include<cassert>
+#include <climits>
+#include <immintrin.h> // AVX, AVX2, AVX-512 // #pragma GCC optimize ("O3,unroll-loops") //#pragma GCC target ("avx,avx2,fma")
+#include<chrono>
+using namespace std;
+using ll = long long;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef unsigned long long ull;
+typedef unsigned int uint;
+typedef complex<double> cpx;
+typedef long double ld;
+#define pq priority_queue
+#define endl "\n"
+#define INF ll(2e18)
+const int inf = 1000000007;
+const long double pi = acosl(-1);
+const string debug = "debug: ";
+#define all(x) (x).begin(), (x).end()
+
+#include<random> // rand
+std::mt19937 gen;
+void rng_init() {
+    std::random_device rd;
+    gen.seed(rd());  // main에서 1번
+}
+int randint(int l, int r) {
+    std::uniform_int_distribution<int> dist(l, r);
+    return dist(gen);
+}
+
+#include<fstream>
+ifstream fin; ofstream fout;
+
+//#define cin fin 
+//#define cout fout
+
+//int dx[4] = { -1,0,1,0 };
+//int dy[4] = { 0,1,0,-1 };
+//int dx8[8] = { -1,-1,-1,0,1,1,1,0 };
+//int dy8[8] = { -1,0,1,1,1,0,-1,-1 };
+
+///////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////
+
+
+vector<int> E[300001];
+
+bool dfs(int x, int p) {
+    vector<int> v;
+    for (int nx : E[x]) {
+        if (nx == p) continue;
+        if (dfs(nx, x)) v.push_back(nx);
+    }
+    for (int i = 0; i + 1 < v.size(); i += 2) {
+        cout << x << " " << v[i] << endl;
+        cout << v[i] << " " << v[i + 1] << endl;
+    }
+    if (v.size() % 2) {
+        cout << p << " " << x << endl;
+        cout << p << " " << v.back() << endl;
+        return false;
+    }
+    return true;
+}
+
+void solve(int tc) {
+
+    int N;
+    cin >> N;
+    for (int i = 0; i < N - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        E[u].push_back(v);
+        E[v].push_back(u);
+    }
+
+    if (N % 2 == 0) {
+        cout << "NO";
+        return;
+    }
+
+    cout << "YES" << endl;
+    dfs(1, 0);
+
+
+    // cout << "Case #" << tc << ": " << ret << endl;
+}
+
+
+int main() {
+    //fin.open("input.txt"); fout.open("output.txt");
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    rng_init();
+    int T = 1;
+    //cin >> T;
+    for (int tc = 1; tc <= T; tc++) {
+        solve(tc);
+    }
+
+    return 0;
+}
